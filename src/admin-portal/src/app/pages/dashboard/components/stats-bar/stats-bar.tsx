13f64117f280560c56fdbb676cabe32e63cf5042
Tiaan <info@bythehour.co.za>
2022-04-12 16:28:40 +0200

    fix(cleanup): update commit message

--- src/admin-portal/src/app/pages/dashboard/components/stats-bar/stats-bar.tsx
+++ src/admin-portal/src/app/pages/dashboard/components/stats-bar/stats-bar.tsx
@@ -30,10 +30,12 @@ export default function StatsBar({
               <div className={`absolute rounded-md p-3 ${item.color}`}>
                 <item.icon className="h-6 w-6 text-white" aria-hidden="true" />
               </div>
-              <p className="ml-16 text-sm font-medium text-gray-500 truncate" title={item.name}>
+              <p
+                className="ml-16 text-sm font-medium text-gray-500 truncate"
+                title={item.name}
+              >
                 {item.name}
               </p>
-
               {/* {item.miniText && (
                 <p className="ml-16 text-sm font-medium text-gray-500 truncate">{item.miniText}</p>
               )} */}
