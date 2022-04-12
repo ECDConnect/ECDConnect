ba4d1901b6fa4bbc1d38b1e7e1939c258277c61b
Tiaan <info@bythehour.co.za>
2022-04-12 14:37:32 +0200

    initial commit

--- src/admin-portal/src/app/pages/dashboard/dashboard.tsx
+++ src/admin-portal/src/app/pages/dashboard/dashboard.tsx
@@ -37,7 +37,7 @@ export default function Dashboard() {
                 currentTab?.title === tab.title
                   ? 'border-primary text-primary'
                   : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200 cursor-pointer',
-                'whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm'
+                'whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm '
               )}
               aria-current={
                 currentTab?.title === tab.title ? 'page' : undefined
