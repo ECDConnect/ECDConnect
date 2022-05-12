601549eba12aed24b912bc7b11415cadeb9bc7cf
Tiaan <info@bythehour.co.za>
2022-05-12 05:50:36 +0200

    fix(server): change embedded code to config read

--- src/api/ECDLink.Tenancy/EntityFramework/Extensions/DbContextOptionsBuilderExtensions.cs
+++ src/api/ECDLink.Tenancy/EntityFramework/Extensions/DbContextOptionsBuilderExtensions.cs
@@ -8,16 +8,23 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.Configuration;
+using ECDLink.Core.Extensions;
 
 namespace ECDLink.Tenancy.EntityFramework.Extensions
 {
+
     public static class DbContextOptionsBuilderExtensions
     {
-        public static void UseNpgsqlTenancy(this DbContextOptionsBuilder builder, IServiceProvider provider, string MigrationAssembly)
+
+        public static void UseNpgsqlTenancy(this DbContextOptionsBuilder builder, IServiceProvider provider, string MigrationAssembly, IConfiguration config)
         {
+            IConfiguration _config = config;
+
+            var conf = _config.GetSection<FranchisorConfiguration>(TenancyConstants.Configuration.TenantSettings);
             builder.AddInterceptors(new PostgresTenantDbConnectionInterceptor());
 
-            builder.UseNpgsql("Server=localhost;Port=5432;Database=ecdconnect;User Id=postgres;Password=admin");
+            builder.UseNpgsql(conf.ConnectionString);
         }
     }
 }
