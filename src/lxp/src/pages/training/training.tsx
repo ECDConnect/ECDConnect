e09b92c0a7e715c5529774fa2693fffc0d0a4088
birarr <birarr@gmail.com>
2022-07-06 08:52:42 -0300

    fix(training): remove second post request

--- src/lxp/src/pages/training/training.tsx
+++ src/lxp/src/pages/training/training.tsx
@@ -10,84 +10,47 @@ export const Training: React.FC = () => {
   const { isOnline } = useOnlineStatus();
   const history = useHistory();
   const [token, setToken] = useState<any>('');
-  const [token2, setToken2] = useState<any>(false);
 
-  const handleFetch = async () => {
-    // const payload = {
-    //   username: 'Reeffaard',
-    //   password: 'M4%c%1h$38',
-    //   loginToken: 'yKYGWNdj7ZBLQwPg5dZ4tcssJgAsdgu5',
-    // };
-    const res = await fetch(
-      'https://ecdconnect.appysites.co.za/login/token.php?service=moodle_mobile_app&username=Reeffaard&password=M4%c%1h$38',
-      {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-type': 'application/json',
-          Connection: 'keep-alive',
-        },
-        // body: JSON.stringify(payload),
-      }
-    );
-    const data = await res.json();
-    await setToken(data.token);
-    await console.log(data.token);
-    return data;
-  };
+  // const handleFetch = async () => {
+  //   // const payload = {
+  //   //   username: 'Reeffaard',
+  //   //   password: 'M4%c%1h$38',
+  //   //   loginToken: 'yKYGWNdj7ZBLQwPg5dZ4tcssJgAsdgu5',
+  //   // };
+  //   const res = await fetch(
+  //     'https://ecdconnect.appysites.co.za/login/token.php?service=moodle_mobile_app&username=Reeffaard&password=M4%c%1h$38',
+  //     {
+  //       method: 'POST',
+  //       headers: {
+  //         Accept: 'application/json',
+  //         'Content-type': 'application/json',
+  //         Connection: 'keep-alive',
+  //       },
+  //       // body: JSON.stringify(payload),
+  //     }
+  //   );
+  //   const data = await res.json();
+  //   await setToken(data.token);
+  //   await console.log(data.token);
+  //   return data;
+  // };
 
-  const handleFetch2 = async () => {
-    const payload = {
-      username: 'Reeffaard',
-      password: 'M4%c%1h$38',
-      loginToken: 'yKYGWNdj7ZBLQwPg5dZ4tcssJgAsdgu5',
-    };
-    const res = await fetch(
-      `https://ecdconnect.appysites.co.za/login/token.php?service=moodle_mobile_app&username=Reeffaard&password=M4%c%1h$38&logintoken=${token}`,
-      {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-type': 'application/json',
-          Connection: 'keep-alive',
-          // username: 'Reeffaard',
-          // password: 'M4%c%1h$38',
-          // loginToken: 'yKYGWNdj7ZBLQwPg5dZ4tcssJgAsdgu5',
-        },
-        // body: JSON.stringify(payload),
-      }
-    );
-    const data = await res.json();
-    await setToken(data.token);
-    setToken2(true);
-    return data;
-  };
+  // const renderIframe = useMemo(() => {
+  //   return (
+  //     <iframe
+  //       src={`https://ecdconnect.appysites.co.za?service=moodle_mobile_app`}
+  //       title="ECD Moodle"
+  //       height="800px"
+  //       width="90%"
+  //       className="divide-y-2 divide-uiLight divide-dashed mx-auto"
+  //     ></iframe>
+  //   );
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [token]);
 
-  useEffect(() => {
-    if (token) {
-      handleFetch2();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [token]);
-
-  const renderIframe = useMemo(() => {
-    if (token2) {
-      return (
-        <iframe
-          src={`https://ecdconnect.appysites.co.za?service=moodle_mobile_app`}
-          title="ECD Moodle"
-          height="800px"
-          width="90%"
-          className="divide-y-2 divide-uiLight divide-dashed mx-auto"
-        ></iframe>
-      );
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [token, token2]);
-
-  useEffect(() => {
-    handleFetch();
-  }, []);
+  // useEffect(() => {
+  //   handleFetch();
+  // }, []);
 
   return (
     <BannerWrapper
@@ -99,7 +62,13 @@ export const Training: React.FC = () => {
       displayOffline={!isOnline}
     >
       <div className="divide-y-2 divide-uiLight divide-dashed">
-        {renderIframe}
+        <iframe
+          src={`https://ecdconnect.appysites.co.za`}
+          title="ECD Moodle"
+          height="800px"
+          width="90%"
+          className="divide-y-2 divide-uiLight divide-dashed mx-auto"
+        ></iframe>
       </div>
     </BannerWrapper>
   );
