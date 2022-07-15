6d4ee98a18f00ce476462aac7788ceba3ae451ee
bryce__ <bryce@appy.co.za>
2022-07-15 07:29:05 +0300

    feat(ui): dropdown style update

--- packages/ui/src/components/dropdown/dropdown.tsx
+++ packages/ui/src/components/dropdown/dropdown.tsx
@@ -6,6 +6,7 @@ import { DropDownFillType, DropDownOption } from './models/DropDownOption';
 import { renderIcon } from '../../utils';
 import { Colours, ComponentBaseProps } from '../../models';
 import { classNames } from '../../utils/style-class.utils';
+import { CheckCircleIcon } from '@heroicons/react/solid';
 
 export interface DropdownProps<T> extends ComponentBaseProps {
   placeholder?: string;
@@ -30,7 +31,7 @@ export function Dropdown<T>({
   disabled = false,
   fillType = 'filled',
   fullWidth = false,
-  fillColor = 'white',
+  fillColor = 'uiBg',
   textColor = 'primary',
   className,
 }: DropdownProps<T>) {
@@ -70,7 +71,7 @@ export function Dropdown<T>({
               disabled={disabled}
             >
               <Typography
-                type={'dropText'}
+                type={'body'}
                 color={touched ? 'textDark' : 'textLight'}
                 weight={touched ? 'normal' : 'skinny'}
                 text={selectedLabel ? selectedLabel : placeholder}
@@ -110,7 +111,16 @@ export function Dropdown<T>({
                             }
                             onClick={() => handler(item)}
                           >
-                            {item.label}
+                            <div className="flex flex-row gap-2.5">
+                              <CheckCircleIcon
+                                className={`cursor-pointer h-22 w-22 text-${
+                                  item.label === selectedLabel
+                                    ? 'blue-accent3'
+                                    : 'primaryAccent2'
+                                }`}
+                              />
+                              {item.label}
+                            </div>
                           </div>
                         </Menu.Item>
                       </div>
