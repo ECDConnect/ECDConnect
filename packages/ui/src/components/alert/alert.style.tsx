7051a2586b303e0b9fce09594bd83eb1228577a3
bryce__ <bryce@appy.co.za>
2022-07-15 07:27:55 +0300

    feat(ui): alert design update

--- packages/ui/src/components/alert/alert.style.tsx
+++ packages/ui/src/components/alert/alert.style.tsx
@@ -1,16 +1,15 @@
 import { renderIcon } from '../../utils';
 import { AlertProps, AlertVariant } from './alert.types';
-export const wrapper = (style: string) =>
-  ` ${style !== 'flat' && 'rounded-md border'} p-4`;
-export const innerWrapper = 'flex flex-row';
-export const extendedContentWrapper = 'pl-3 mt-2';
+export const wrapper = (style: string) => `rounded-10 p-4 `;
+export const innerWrapper = 'flex flex-row ';
+export const extendedContentWrapper = 'pl-3 mt-2 ';
 export const iconWrapper = 'flex-shrink-0';
-export const contentWrapper = 'flex flex-col justify-start items-start';
-export const icon = ' h-5 w-5';
-export const messageWrapper = 'ml-3';
-export const message = (hasTitle: boolean) => `text-sm ${hasTitle && 'mt-2'}`;
-export const title = ' text-sm font-thin';
-export const list = ' list-disc pl-4';
+export const contentWrapper = 'flex flex-col justify-start items-start ';
+export const icon = 'h-5 w-5 ';
+export const messageWrapper = 'ml-3 ';
+export const message = (hasTitle: boolean) => `text-sm ${hasTitle && 'mt-2 '}`;
+export const title = 'text-sm font-thin ';
+export const list = 'list-disc pl-4 mt-2 ';
 
 export const alertColor = (type: string, style: AlertVariant) => {
   switch (type) {
@@ -51,7 +50,22 @@ export const alertTextColor = (type: string) => {
 };
 
 const getIcon = (iconName: string, colorType: string) => {
-  return renderIcon(iconName, icon + alertColor(colorType, 'outlined'));
+  return renderIcon(iconName, icon + alertIconColor(colorType));
+};
+
+export const alertIconColor = (type: string) => {
+  switch (type) {
+    case 'info':
+      return ` text-infoMain`;
+    case 'success':
+      return `text-successMain `;
+    case 'warning':
+      return `text-alertMain `;
+    case 'error':
+      return `text-errorMain `;
+    default:
+      return `text-infoMain `;
+  }
 };
 
 export const alertIcon = (type: string): JSX.Element => {
