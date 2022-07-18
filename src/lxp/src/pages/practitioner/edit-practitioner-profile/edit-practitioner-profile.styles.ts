25e0116fc9d040e72e356a39d762afd2c78d3aa2
bryce__ <bryce@appy.co.za>
2022-07-18 09:54:24 +0300

    feat(ui): update practitioner edit form styling

--- src/lxp/src/pages/practitioner/edit-practitioner-profile/edit-practitioner-profile.styles.ts
+++ src/lxp/src/pages/practitioner/edit-practitioner-profile/edit-practitioner-profile.styles.ts
@@ -1,4 +1,5 @@
-export const label = 'block text-base font-body text-textDark';
+export const label =
+  'block text-base leading-snug font-body font-semibold text-textMid';
 export const input =
   'appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm';
 export const inputGroup = 'ml-3 mr-3 mt-1';
