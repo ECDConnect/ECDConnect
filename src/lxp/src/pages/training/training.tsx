8d8c21e2602bf66d47edc345332c37f968b4a834
birarr <birarr@gmail.com>
2022-07-19 16:33:33 -0300

    fix(training): fix moodle get request

--- src/lxp/src/pages/training/training.tsx
+++ src/lxp/src/pages/training/training.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from 'react';
-import { BannerWrapper } from '@ecdlink/ui';
+import { BannerWrapper, LoadingSpinner } from '@ecdlink/ui';
 // import { IconInformationIndicator } from '../classroom/programme-planning/components/icon-information-indicator/icon-information-indicator';
 import { useHistory } from 'react-router';
 import { useOnlineStatus } from '@hooks/useOnlineStatus';
@@ -10,6 +10,7 @@ export const Training: React.FC = () => {
   const { isOnline } = useOnlineStatus();
   const history = useHistory();
   const [token, setToken] = useState<any>('');
+  const [loading, setLoading] = useState(false);
 
   // const handleFetch = async () => {
   //   // const payload = {
@@ -35,48 +36,60 @@ export const Training: React.FC = () => {
   //   return data;
   // };
 
-  const handleFetch = async () => {
-    // const payload = {
-    //   username: 'Reeffaard',
-    //   password: 'M4%c%1h$38',
-    //   loginToken: 'yKYGWNdj7ZBLQwPg5dZ4tcssJgAsdgu5',
-    // };
-    const res = await fetch(
-      'https://ecdconnect.appysites.co.za/login/token.php?service=moodle_mobile_app&username=9907045800080&password=Pass1234!',
-      {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-          'Content-type': 'application/json',
-          Connection: 'keep-alive',
-        },
-        // body: JSON.stringify(payload),
-      }
-    );
-    const data = await res.json();
-    await setToken(data.token);
-    await console.log(data.token);
-    return data;
-  };
-
-  console.log({ token });
+  // const handleFetch = async () => {
+  //   // const payload = {
+  //   //   username: 'Reeffaard',
+  //   //   password: 'M4%c%1h$38',
+  //   //   loginToken: 'yKYGWNdj7ZBLQwPg5dZ4tcssJgAsdgu5',
+  //   // };
+  //   const res = await fetch(
+  //     'https://ecdconnect.appysites.co.za/login/token.php?service=moodle_mobile_app&username=9907045800080&password=Pass1234!',
+  //     {
+  //       method: 'GET',
+  //       headers: {
+  //         Accept: 'application/json',
+  //         'Content-type': 'application/json',
+  //         Connection: 'keep-alive',
+  //       },
+  //       // body: JSON.stringify(payload),
+  //     }
+  //   );
+  //   const data = await res.json();
+  //   await setToken(data.token);
+  //   await console.log(data.token);
+  //   await setLoading(false);
+  //   return data;
+  // };
+  // console.log({ loading });
+  // console.log({ token });
 
   const renderIframe = useMemo(() => {
-    return (
-      <iframe
-        src={`https://ecdconnect.appysites.co.za/login/index.php?service=moodle_mobile_app`}
-        title="ECD Moodle"
-        height="800px"
-        width="90%"
-        className="divide-y-2 divide-uiLight divide-dashed mx-auto"
-      ></iframe>
-    );
+    if (loading) {
+      return (
+        <LoadingSpinner
+          className="mt-6"
+          size={'medium'}
+          spinnerColor={'primary'}
+          backgroundColor={'uiLight'}
+        />
+      );
+    } else {
+      return (
+        <iframe
+          src={`https://ecdconnect.appysites.co.za/login/index.php?service=moodle_mobile_app`}
+          title="ECD Moodle"
+          height="800px"
+          width="90%"
+          className="divide-y-2 divide-uiLight divide-dashed mx-auto"
+        ></iframe>
+      );
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [token]);
+  }, [token, loading]);
 
-  useEffect(() => {
-    handleFetch();
-  }, []);
+  // useEffect(() => {
+  //   handleFetch();
+  // }, []);
 
   // https://ecdconnect.appysites.co.za/login/token.php?username=9907045800080&password=Pass1234!&service=moodle_mobile_app
   console.log({ renderIframe });
