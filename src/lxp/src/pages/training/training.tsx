cc393a95a946c02d6ca8b6a7cc7e3d3b79a35975
birarr <birarr@gmail.com>
2022-08-12 08:36:19 -0300

    fix(training): change url address

--- src/lxp/src/pages/training/training.tsx
+++ src/lxp/src/pages/training/training.tsx
@@ -27,7 +27,7 @@ export const Training: React.FC = () => {
     } else {
       return (
         <iframe
-          src={`https://ecdconnect.appysites.co.za/login/index.php?service=moodle_mobile_app`}
+          src={`http://apy-ecd-d-vm-moodle-01.northeurope.cloudapp.azure.com?service=moodle_mobile_app`}
           title="ECD Moodle"
           height="800px"
           width="90%"
