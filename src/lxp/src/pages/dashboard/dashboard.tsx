0dbc52a676fb33482a1c363c0ed074461e8d2c87
birarr <birarr@gmail.com>
2022-08-25 15:26:32 -0300

    fix(coach): fix navigation bar profile route

--- src/lxp/src/pages/dashboard/dashboard.tsx
+++ src/lxp/src/pages/dashboard/dashboard.tsx
@@ -53,6 +53,7 @@ export const Dashboard: React.FC = () => {
   const history = useHistory();
   const { theme } = useTheme();
   const dialog = useDialog();
+  const isCoach = userData?.roles?.some((role) => role.name === 'Coach');
 
   const newNotificationCount = useSelector(
     notificationsSelectors.getNewNotificationCount
@@ -82,7 +83,7 @@ export const Dashboard: React.FC = () => {
    * 2. Children of Practitioners
    */
   useEffect(() => {
-    if (userData?.roles?.some((role) => role.name === 'Coach')) {
+    if (isCoach) {
       (async () =>
         await appDispatch(
           practitionerForCoachThunkActions.getPractitionersForCoach({})
@@ -136,7 +137,9 @@ export const Dashboard: React.FC = () => {
     },
     {
       name: NavigationTypes.Profile,
-      href: ROUTES.PRACTITIONER.PROFILE.ROOT,
+      href: isCoach
+        ? ROUTES.COACH.PROFILE.ROOT
+        : ROUTES.PRACTITIONER.PROFILE.ROOT,
       icon: 'UserIcon',
       current: false,
       showDivider: true,
@@ -162,7 +165,7 @@ export const Dashboard: React.FC = () => {
 
   const dashboardItems: StackedListItemType[] = [];
 
-  if (userData?.roles?.some((role) => role.name === 'Coach')) {
+  if (isCoach) {
     dashboardItems.push(
       {
         title: 'Smartstarters',
