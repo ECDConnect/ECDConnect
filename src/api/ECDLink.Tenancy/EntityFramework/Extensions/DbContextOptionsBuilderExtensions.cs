e894a843d83ead7a3b1327da9c79c66b5d0730f4
Tiaan <tiaan@appy.co.za>
2022-08-31 09:41:29 +0200

    feat(hierarchy): redo hierachy accumlation for admin roles

--- src/api/ECDLink.Tenancy/EntityFramework/Extensions/DbContextOptionsBuilderExtensions.cs
+++ src/api/ECDLink.Tenancy/EntityFramework/Extensions/DbContextOptionsBuilderExtensions.cs
@@ -16,8 +16,7 @@ namespace ECDLink.Tenancy.EntityFramework.Extensions
         public static void UseNpgsqlTenancy(this DbContextOptionsBuilder builder, IServiceProvider provider, string MigrationAssembly)
         {
             builder.AddInterceptors(new PostgresTenantDbConnectionInterceptor());            
-            builder.UseNpgsql("Server=localhost;Port=5432;Database=public;User Id=postgres;Password=admin");
-            //builder.UseNpgsql("Server=ecdlink-pgsql-dev.postgres.database.azure.com;Port=5432;Database=SmartStartDev; User Id=ecdlinkadmin@ecdlink-pgsql-dev;Password=!@Mr3sp0ns1v3;SslMode=Require");
+            builder.UseNpgsql("Server=localhost;Port=5432;Database=ecdconnect;User Id=postgres;Password=admin");            
         }
     }
 }
