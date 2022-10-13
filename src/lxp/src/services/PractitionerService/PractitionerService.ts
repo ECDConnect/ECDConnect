7ba62f1ed9ddf141625cbcc667f3dcb55ac5500d
Ubiratan <ubiratan@appy.co.za>
2022-10-13 14:44:28 -0300

    fix(practitioner): add insertedDate in the getClassroomDetailsForPractitioner

--- src/lxp/src/services/PractitionerService/PractitionerService.ts
+++ src/lxp/src/services/PractitionerService/PractitionerService.ts
@@ -298,6 +298,8 @@ class PractitionerService {
             principalName
             classroomName
             classroomGroupName
+            classroomId
+            insertedDate
           }
         }
       `,
