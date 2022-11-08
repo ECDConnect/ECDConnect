ea76777d4d35832c90ad5836ecabd2caa560457d
Tiaan <tiaan@appy.co.za>
2022-11-08 20:48:22 +0200

    fix(reassignment): reassignment absenteeism metrics and statusses changes

--- src/api/core-api/GraphApi/Mutations/AbsenteeMutationExtension.cs
+++ src/api/core-api/GraphApi/Mutations/AbsenteeMutationExtension.cs
@@ -18,6 +18,7 @@ using System.Collections.Generic;
 using ECDLink.DataAccessLayer.Hierarchy.Entities;
 using ECDLink.DataAccessLayer.Hierarchy;
 using Microsoft.Azure.Documents;
+using System.Drawing;
 
 namespace EcdLink.Api.CoreApi.GraphApi.Mutations
 {
@@ -28,74 +29,36 @@ namespace EcdLink.Api.CoreApi.GraphApi.Mutations
 
         public Absentees AddAbsenteeForPractitioner([Service] IHttpContextAccessor contextAccessor,
             [Service] UserManager<ApplicationUser> userManager,
-            [Service] IDbContextFactory<AuthenticationDbContext> dbFactory,
             [Service] IGenericRepositoryFactory repoFactory,
             [Service] HierarchyEngine engine,
-            string practitionerId,
+            string fromUserId,
+            string toUserId,
             string reason,
             DateTime absentDate,
             string loggedByUser,
-            string classProgram,
-            string reassignedToPractitioner)
+            string classProgram = null)
         {
-            using var scope = dbFactory.CreateDbContext();
-            using var dbContextTransaction = scope.Database.BeginTransaction();
             var uId = contextAccessor.HttpContext.GetUser().Id;
-            var absenteeRepo = repoFactory.CreateRepository<Absentees>(userContext: uId);
-            var historyRepo = repoFactory.CreateRepository<ClassReassignmentHistory>(userContext: uId);
+            var absenteeRepo = repoFactory.CreateRepository<Absentees>(userContext: uId);            
             var updated = new Absentees();
-            var hierarchy = engine.GetUserHierarchy((reassignedToPractitioner != null ? reassignedToPractitioner : uId));
             if (classProgram != null)
             {
 
                 var absent = new Absentees
                 {
-                    UserId = practitionerId,
+                    UserId = fromUserId,
                     Reason = reason,
                     AbsentDate = absentDate,
                     LoggedBy = loggedByUser,
                     ReassignedClass = classProgram,
-                    ReassignedToPractitioner = reassignedToPractitioner
+                    ReassignedToPractitioner = toUserId,
                     
                 };
                 updated = absenteeRepo.Insert(absent);
 
-                //Log to the history table
-                var history = new ClassReassignmentHistory
-                {
-                    UserId = practitionerId,
-                    Reason = reason,
-                    ReassignedDate = DateTime.Now,
-                    LoggedBy = loggedByUser,
-                    ReassignedClass = classProgram,
-                    ReassignedToUser = reassignedToPractitioner,
-                    HierarchyToUser = hierarchy,
-                    
-                };
-                var historySaved = historyRepo.Insert(history);
-
-                //reassign classroomGroup
-                var classroomGroupRepo = repoFactory.CreateRepository<ClassroomGroup>(userContext: uId);
-                var classroom = classroomGroupRepo.GetById(Guid.Parse(classProgram));//classroomGroupRepo.GetByUserId(practitionerId);
-                if (classroom != null && reassignedToPractitioner != null)
-                {
-                    //get userhierarchy and assign that here
-                    //string newHierarchy = "";
-                    //var userHierarchyRepo = repoFactory.CreateGenericRepository<UserHierarchyEntity>(userContext: uId);
-                    //UserHierarchyEntity userHierarchy = userHierarchyRepo.GetAll().Where(x => x.UserId.Equals(reassignedToPractitioner)).FirstOrDefault();
-                    if (hierarchy != null)
-                    {
-                        classroom.Hierarchy = hierarchy;
-                    }
-
-                    classroom.UserId = Guid.Parse(reassignedToPractitioner);
-                    classroomGroupRepo.Update(classroom);
-
-                    //update classProgramme
-                    this.UpdateClassProgrammeForPractitioner(contextAccessor, dbFactory, repoFactory, Guid.Parse(classProgram), hierarchy);
-
-
-                }
+                //Log to the history table for reassignment back to owner user
+                ReassignmentMutationExtension reassignment = new ReassignmentMutationExtension();
+                reassignment.AddReassignmentForPractitioner(contextAccessor, repoFactory, engine, fromUserId, toUserId, reason, absentDate, loggedByUser, classProgram);
             }
 
             //Save the history so it can be reassigned
@@ -103,65 +66,21 @@ namespace EcdLink.Api.CoreApi.GraphApi.Mutations
             return updated;            
         }
 
-        public bool ReassignClassroomsFromHistory([Service] IHttpContextAccessor contextAccessor,
+        public bool ReassignAbsenteeFromHistory([Service] IHttpContextAccessor contextAccessor,
             [Service] UserManager<ApplicationUser> userManager,
             [Service] IDbContextFactory<AuthenticationDbContext> dbFactory,
             [Service] IGenericRepositoryFactory repoFactory,
             [Service] HierarchyEngine engine,
             string userId)
         {
-            using var scope = dbFactory.CreateDbContext();
-            using var dbContextTransaction = scope.Database.BeginTransaction();
             var uId = contextAccessor.HttpContext.GetUser().Id;
             var historyRepo = repoFactory.CreateRepository<ClassReassignmentHistory>(userContext: uId);
             bool reAssigned = false;
-            List<ClassReassignmentHistory> history = historyRepo.GetListByUserId(userId);
-            foreach (var historyItem in history)
-            {
-                if (historyItem.ReassignedDate == DateTime.Now.AddDays(-1))
-                {
-
-                    //Log to the history table the reassignment back to original user
-                    var updateHistory = new ClassReassignmentHistory
-                    {                        
-                        Reason = "Reassigning to original",
-                        ReassignedBackDate = DateTime.Now,
-                        ReassignedBackToUserId = historyItem.UserId
-                    };
-
-                    var historySaved = historyRepo.Update(updateHistory);
-
-                    //reassign classroom
-                    var classroomGroupRepo = repoFactory.CreateRepository<ClassroomGroup>(userContext: uId);
-                    ClassroomGroup classroom = classroomGroupRepo.GetAll().Where(x => x.Id.Equals(Guid.Parse(historyItem.ReassignedClass))).FirstOrDefault();   //GetByUserId(historyItem.ReassignedToUser);
-                    if (classroom != null)
-                    {
-                        //string newHierarchy = "";
-                        //var userHierarchyRepo = repoFactory.CreateGenericRepository<UserHierarchyEntity>(userContext: uId);
-                        //UserHierarchyEntity userHierarchy = userHierarchyRepo.GetAll().Where(x => x.UserId.Equals(historyItem.UserId)).FirstOrDefault();
-                        var hierarchy = engine.GetUserHierarchy((userId != null ? userId : uId));
-                        if (hierarchy != null)
-                        {
-                            classroom.Hierarchy = hierarchy;
-                        }
-                        classroom.UserId = Guid.Parse(historyItem.UserId);
-                        classroomGroupRepo.Update(classroom);
-                        if (historyItem.ReassignedClass != null)
-                        {
-                            this.UpdateClassProgrammeForPractitioner(contextAccessor, dbFactory, repoFactory, Guid.Parse(historyItem.ReassignedClass), hierarchy);
-                        }
-                    }
-                }
-                reAssigned = true;
-            }
-
-            //Save the history so it can be reassigned
-
-            return reAssigned;
+            ReassignmentMutationExtension reassignment = new ReassignmentMutationExtension();
+            return reassignment.ReassignClassroomsFromHistory(contextAccessor, repoFactory, userId);
         }
 
         private bool UpdateClassProgrammeForPractitioner([Service] IHttpContextAccessor contextAccessor,
-[Service] IDbContextFactory<AuthenticationDbContext> dbFactory,
 [Service] IGenericRepositoryFactory repoFactory, Guid classroomId, string newHierarchy)
         {
             bool updated = false;
@@ -176,6 +95,22 @@ namespace EcdLink.Api.CoreApi.GraphApi.Mutations
             }
             return updated;
         }
+
+//        private bool UpdateLearnerForPractitioner([Service] IHttpContextAccessor contextAccessor,
+//[Service] IGenericRepositoryFactory repoFactory, Guid classroomGroupId, string hierarchy)
+//        {
+//            bool updated = false;
+//            var uId = contextAccessor.HttpContext.GetUser().Id;
+
+//            var learnerRepo = repoFactory.CreateGenericRepository<Learner>(userContext: uId);
+//            Learner learners = (ClassProgramme)learnerRepo.GetAll().Where(x => x.ClassroomGroupId.Equals(classroomId)).FirstOrDefault();
+//            if (classProgramme != null && !string.IsNullOrWhiteSpace(newHierarchy))
+//            {
+//                classProgramme.Hierarchy = newHierarchy;
+//                classProgrammeRepo.Update(classProgramme);
+//            }
+//            return updated;
+//        }
 
     }
 }
