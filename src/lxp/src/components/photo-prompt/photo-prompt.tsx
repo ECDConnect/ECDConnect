8f199ce69aae6c86fb4ddb311f2d9fb796c97a77
Divvie Esterhuizen <divvie@appy.co.za>
2022-11-23 18:19:35 +0200

    fix(camera): xpp and GG camera flip fix

--- src/lxp/src/components/photo-prompt/photo-prompt.tsx
+++ src/lxp/src/components/photo-prompt/photo-prompt.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { ActionSelect } from '@ecdlink/ui';
 import { ActionSelectItem, ComponentBaseProps } from '@ecdlink/ui';
 import {
@@ -94,6 +95,19 @@ export const PhotoPrompt: React.FC<PhotoPromptProps> = ({
     switch (value) {
       case 'camera':
         openCamera();
+        // added logic to flip camera using JS and CSS
+        // some logic from: https://github.com/ionic-team/pwa-elements/issues/11
+        // some logic from CSS knowledge
+        setTimeout(() => {
+          const video = document
+            .querySelector('pwa-camera-modal-instance')
+            .shadowRoot.querySelector('pwa-camera')
+            .shadowRoot.querySelector('video');
+          if (video !== null) {
+            video.style.transform = 'none';
+            video.style.transform = 'scaleX(-1)';
+          }
+        }, 100);
         break;
       case 'delete':
         deletePhoto();
