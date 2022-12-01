cf992650de8e4e3b24b99cbedff65f60c5585d9a
karel <karel@swing.dev>
2022-12-01 08:02:26 +0200

    feat(teamlead): created team lead and clinic

---
+++ src/api/core-api/GraphApi/Models/TeamLeadModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ECDLink.DataAccessLayer.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using ECDLink.DataAccessLayer.Entities.Users;
+
+namespace EcdLink.Api.CoreApi.GraphApi.Models
+{
+    public class TeamLeadModel
+    {
+        public virtual ApplicationUser User { get; set; }
+        public string UserId { get; set; }
+
+        public virtual Clinic Clinic { get; set; }
+        public Guid? ClinicId { get; set; }
+
+        public string JobTitle { get; set; }
+    }
+}
+
