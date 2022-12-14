4a8895dae7c765379a7a62ca550c9ae770f8b78a
lubezerra <lucilene@appy.co.za>
2022-12-14 14:15:03 -0300

    refactor(gg/app): remove redundant const

--- src/growgreat/src/pages/auth/password-reset/password-reset.tsx
+++ src/growgreat/src/pages/auth/password-reset/password-reset.tsx
@@ -144,7 +144,7 @@ export const PasswordReset: React.FC = () => {
         renderBorder={true}
         onBack={goBack}
       >
-        <div className={styles.passwordResetContainer}>
+        <div className={'p-4'}>
           {!displaySuccess && (
             <div>
               <Typography
