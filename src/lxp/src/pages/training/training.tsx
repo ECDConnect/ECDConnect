bcae074b6098fcdb604cd9d85aeb4deffe848cb8
Ubiratan <ubiratan@appy.co.za>
2022-12-30 12:15:39 -0300

    feat(moodle): add user session id cookie

--- src/lxp/src/pages/training/training.tsx
+++ src/lxp/src/pages/training/training.tsx
@@ -1,9 +1,14 @@
-import { useMemo, useState } from 'react';
+import { useMemo, useState, useEffect } from 'react';
 import { BannerWrapper, LoadingSpinner } from '@ecdlink/ui';
 import { useHistory } from 'react-router';
 import { useOnlineStatus } from '@hooks/useOnlineStatus';
 
 import React from 'react';
+import { AuthService } from '@/services/AuthService';
+import { useSelector } from 'react-redux';
+import { userSelectors } from '@store/user';
+import { authSelectors } from '@/store/auth';
+import { PractitionerService } from '@/services/PractitionerService';
 
 export const Training: React.FC = () => {
   const { isOnline } = useOnlineStatus();
@@ -12,6 +17,25 @@ export const Training: React.FC = () => {
   const [token, setToken] = useState<any>('');
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [loading, setLoading] = useState(false);
+  const userData = useSelector(userSelectors.getUser);
+  const userAuth = useSelector(authSelectors.getAuthUser);
+  const [userSessionId, setUserSessionId] = useState('');
+
+  const getUserSessionId = async () => {
+    if (userData?.id) {
+      const data = await new PractitionerService(
+        userAuth?.auth_token!
+      ).getMoodleSessionForUserId(userData?.id);
+      setUserSessionId(data);
+    }
+  };
+
+  useEffect(() => {
+    if (userData?.id) {
+      getUserSessionId();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userData?.id]);
 
   const renderIframe = useMemo(() => {
     if (loading) {
@@ -24,6 +48,9 @@ export const Training: React.FC = () => {
         />
       );
     } else {
+      document.cookie = `MoodleSession=${userSessionId};path=/;domain=moodle.ecdlink.co.za`;
+      document.cookie =
+        'teste=1; Max-Age=2600000; Path=/; SameSite=None; Secure';
       return (
         <iframe
           src={`https://moodle.ecdlink.co.za/?service=moodle_mobile_app`}
