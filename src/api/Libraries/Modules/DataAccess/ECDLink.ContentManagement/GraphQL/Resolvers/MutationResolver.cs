7235f7b9734f050396c66160eb3bba41ae1a6a97
Ruald de Bruyn <ruald@appy.co.za>
2023-01-26 16:44:29 +0200

    fix(dotnet-upgrade): fix mutation builder and resolver

--- src/api/Libraries/Modules/DataAccess/ECDLink.ContentManagement/GraphQL/Resolvers/MutationResolver.cs
+++ src/api/Libraries/Modules/DataAccess/ECDLink.ContentManagement/GraphQL/Resolvers/MutationResolver.cs
@@ -23,12 +23,11 @@ namespace ECDLink.ContentManagement.GraphQL.Resolvers
             return new ValueTask<object?>(newId);
         }
 
-        public ValueTask<object?> UpdateMutationResolver(IResolverContext context)
+        public ValueTask<object?> UpdateMutationResolver(IResolverContext context, int contentId)
         {
             var service = context.Services.GetService<ContentManagementRepository>();
-
             var locale = base.GetLanguageArgument(context);
-            var contentId = context.ArgumentValue<int>(ArgumentConstants.Id);
+
             var values = context.ArgumentValue<Dictionary<string, object>>(ArgumentConstants.Input);
 
             var dynamicObject = service.Update(contentId, locale, values);
@@ -36,10 +35,8 @@ namespace ECDLink.ContentManagement.GraphQL.Resolvers
             return new ValueTask<object?>(dynamicObject);
         }
 
-        public ValueTask<object?> DeleteMutationResolver(IResolverContext context)
+        public ValueTask<object?> DeleteMutationResolver(IResolverContext context, int contentId)
         {
-            var contentId = context.ArgumentValue<int>(ArgumentConstants.Id);
-
             var service = context.Services.GetService<ContentManagementRepository>();
 
             var isDeleted = service.Delete(contentId);
