381f162126764ae50fe199fd6d82ef179d02a379
Tiaan <tiaan@appy.co.za>
2023-02-08 19:41:08 +0200

    fix(statements): update balance sheet and add balancesheet model for FE easy display EC-93

--- src/api/Libraries/Modules/ECDLink.DataAccessLayer/Entities/IncomeStatements/StatementsBalanceSheet.cs
+++ src/api/Libraries/Modules/ECDLink.DataAccessLayer/Entities/IncomeStatements/StatementsBalanceSheet.cs
@@ -2,13 +2,14 @@
 
 namespace ECDLink.DataAccessLayer.Entities.IncomeStatements
 {
-    public class StatementsSubmit
+    public class StatementsBalanceSheet
     {
-        public string Period { get; set; }
         public int? Month { get; set; }
         public int Year { get; set; }
         public string UserId { get; set; }
-        //public DateTime? StartDate { get; set; }
-        //public DateTime? EndDate { get; set; }
+
+        public double IncomeTotal { get; set; }
+        public double ExpenseTotal { get; set; }
+        public double Balance { get; set; }
     } 
 }
