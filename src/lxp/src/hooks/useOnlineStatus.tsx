ae3f6c85a923270073c4a35ec8c475f2b3c54840
Ruald de Bruyn <ruald@appy.co.za>
2023-02-24 00:32:24 +0200

    fix(ec171): reduce poll frequency

--- src/lxp/src/hooks/useOnlineStatus.tsx
+++ src/lxp/src/hooks/useOnlineStatus.tsx
@@ -19,7 +19,6 @@ export const OnlineStatusProvider: React.FC = ({ children }) => {
           {
             interval: 3000,
             timeout: 2000,
-            url: 'https://localhost:5001/api/authentication/online-check',
           } as PollingConfig
         }
         render={({ online }) => {
