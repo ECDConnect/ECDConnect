9380994bfca44e18bbd0c61544193b50d2408da7
Tiaan <tiaan@appy.co.za>
2023-03-08 03:08:22 +0200

    fix(languagefiltering): EC-20 extract locales of cms content cleanup on isle 7

--- src/api/core-api/GraphApi/Queries/GenericQueryTypeExtension.cs
+++ src/api/core-api/GraphApi/Queries/GenericQueryTypeExtension.cs
@@ -37,7 +37,6 @@ namespace EcdLink.Api.CoreApi.GraphApi.Queries
             Guid tenantId = TenantExecutionContext.Tenant.Id;
             var dynamicContentList = new List<Language>();
 
-
             var cTypes = _context.ContentTypes
               .Include(i => i.Content)
               .ThenInclude(ti => ti.ContentValues)
@@ -45,39 +44,12 @@ namespace EcdLink.Api.CoreApi.GraphApi.Queries
               .Where(x => x.Name == contentType)
               .FirstOrDefault();
 
-
-            //var contentTypeFields = _context.ContentTypes
-            //    .Where(x => x.Name == contentType)
-            //    .GroupJoin(_context.ContentTypeFields, ct => ct.Id, ctf => ctf.ContentTypeId, (ct, ctf) => ctf.Select(x => new { contentTypes = ct, ContentTypeField = ctf }).DefaultIfEmpty())
-            //    //.GroupJoin(_context.ContentTypesFieldValues, ctff => ctff , ctfv => ctfv.ContentTypeFieldId, (ctf, ctfv) => ctfv.Select(y => new { ContentTypeFields = ctf, ContentTypesFieldValues = ctfv }).DefaultIfEmpty())
-            //    .FirstOrDefault();
-
-            //var contentTypeFieldss = _context.ContentTypes.Where(x => x.Name == contentType);
-
-
-
-
-
-            //fr FromSqlRaw("SELECT distinct(L.Id),L.Locale FROM ContentValue CV LEFT JOIN Language L ON CV.LocaleId = L.Id LEFT JOIN ContentTypeField CTF on CTF.Id = CV.ContentTypeFieldId  WHERE CTF.DataLinkName = \"\" + type.Name + \"\"  AND \"TenantId\" = '" + tenantId + "'").AsQueryable();
-            /*.ContentTypes      
-            .fr
-            //
-           //.LeftJoin(_context.ContentTypesFieldValues,x => x.Id, y => y.co
-          .Include(i => i.Content)//.Select(y => y.ContentTypeId))              
-          //.Include(ti => ti.ContentValues.Select(y => y.ContentTypeId))
-          .ThenInclude(ti => ti.ContentValues.Select(y => y.ContentTypeFieldId))
-          .ThenInclude(ti2 => ti2)              
-          .ThenInclude(i => i)
-          .Where(x => x.Name == contentType)
-          .FirstOrDefault();*/
-
             if (cTypes != null)
             {
                 foreach (var item in cTypes.Content.Where(x => x.IsActive))
                 {
                     foreach (var locale in item.ContentValues)
                     {
-                        //var localeId = item.ContentValues.Select(y => y.LocaleId);
                         var lang = _context.Languages
                             .Where(x => x.IsActive && x.Id.Equals(locale.LocaleId)).FirstOrDefault();
                         if (!dynamicContentList.Contains(lang))
