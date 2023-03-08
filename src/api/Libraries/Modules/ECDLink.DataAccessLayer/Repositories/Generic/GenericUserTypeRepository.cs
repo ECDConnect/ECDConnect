00fbd5ff6220154972abb52768312aa5c3ef4add
Tiaan <tiaan@appy.co.za>
2023-03-08 09:03:39 +0200

    fix(statements): ec-166 cleanup on isle 2

--- src/api/Libraries/Modules/ECDLink.DataAccessLayer/Repositories/Generic/GenericUserTypeRepository.cs
+++ src/api/Libraries/Modules/ECDLink.DataAccessLayer/Repositories/Generic/GenericUserTypeRepository.cs
@@ -8,7 +8,6 @@ using ECDLink.DataAccessLayer.Repositories.Generic.Base;
 using ECDLink.Security;
 using ECDLink.Tenancy.Context;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
