7128cda0566882cbc52a128a84b3fed9b922f4c4
Ubiratan <ubiratan@appy.co.za>
2023-03-28 21:24:02 -0300

    feat(programmes): update summaries section

--- packages/core/src/utils/date.utils.ts
+++ packages/core/src/utils/date.utils.ts
@@ -49,6 +49,21 @@ export function getPreviousAndNextMonths(
   return { previousDate, nextDate };
 }
 
+export const monthsList = [
+  { label: 'January', value: '1' },
+  { label: 'February', value: '2' },
+  { label: 'March', value: '3' },
+  { label: 'April', value: '4' },
+  { label: 'May', value: '5' },
+  { label: 'June', value: '6' },
+  { label: 'July', value: '7' },
+  { label: 'August', value: '8' },
+  { label: 'Septembre', value: '9' },
+  { label: 'Octubre', value: '10' },
+  { label: 'November', value: '11' },
+  { label: 'December', value: '12' },
+];
+
 export const getNextDateByDay = (daysLater: number, currentDate?: Date) => {
   const today = new Date();
   const date = new Date(currentDate || today);
