facd98eef08f8c111768e7c4513bef1c9ad2f67f
Elisha <elisha@ecdconnect.co.za>
2023-04-03 21:34:48 +0200

    ec-163 classroom group id bug fix

--- src/lxp/src/pages/classroom/attendance/components/attendance-list/attendance-list.tsx
+++ src/lxp/src/pages/classroom/attendance/components/attendance-list/attendance-list.tsx
@@ -375,7 +375,9 @@ export const AttendanceList: React.FC<AttendanceListProps> = ({
                 attendanceDate={attendanceDate}
                 onAttendanceUpdated={(state) => {
                   validateAttendanceList(
-                    selectedGroup.id ?? '',
+                    editAttendanceRegisterVisible
+                      ? classroomgroupId ?? ''
+                      : selectedGroup.id ?? '',
                     state.listItems,
                     isPrimaryList
                   );
