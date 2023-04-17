d064a2542ab79797ecf59cd77c379d663dffbd87
Elisha <elisha@ecdconnect.co.za>
2023-04-17 18:02:20 +0200

    ec-240 refactor generate pdf button method

--- src/lxp/src/components/download-pdf-button/download-pdf-button.tsx
+++ src/lxp/src/components/download-pdf-button/download-pdf-button.tsx
@@ -2,68 +2,44 @@ import { jsPDF } from 'jspdf';
 import autoTable from 'jspdf-autotable';
 import { Typography, Button, renderIcon } from '@ecdlink/ui';
 
-function generateReport(url: string) {
+function generateReport(
+  headerColumns: any[],
+  bodyRows: any[],
+  footer: any[],
+  content: any,
+  tableBottomContent: any,
+  outputName: string
+) {
   const doc = new jsPDF('l');
-  const numDays = 29;
 
   const options = (data: any) => {
     // Add table header to each new page
     // Add left header
     doc.setFontSize(20);
     doc.setFont('bold');
-    doc.text('Left Header Text', 10, 10);
+    doc.text(content.pageTitle, 10, 10);
 
     // Add right header
     doc.setFontSize(16);
     doc.setFont('bold');
     const pageWidth = doc.internal.pageSize.getWidth();
     doc.text(
-      'Right Header Text',
-      pageWidth - doc.getStringUnitWidth('Right Header Text') - 50,
+      content.subtitle,
+      pageWidth - doc.getStringUnitWidth(content.subtitle) - 50,
       10
     );
   };
   doc.setFontSize(12);
   doc.setFont('bold');
-  doc.text('Name: Anesu ndoro', 10, 20);
-  doc.text('Phone Number: +1 123-456-7890', 10, 25);
-  doc.text('ID Number: 123456789', 10, 30);
-  doc.text('Programme Type: XYZ', 100, 20);
-  doc.text('Program Days: Monday to Friday', 100, 25);
-  doc.text('Site Address: 1234 ABC St, City, State, Country', 100, 30);
+
+  doc.text(content?.practitioner_name, 10, 20);
+  doc.text(content?.phone, 10, 25);
+  doc.text(content?.id_number, 10, 30);
+  doc.text(content.programme_type, 100, 20);
+  doc.text(content.programme_days, 100, 25);
+  doc.text(content.site_address, 100, 30);
   doc.setFontSize(8);
-  const data = [
-    { child: 'John', id: 'IDTEST2525255', day1: '1', day2: '1', day3: '0' },
-  ];
 
-  for (let i = 0; i < 50; i++) {
-    const newArray = {
-      child: 'John Bblocks',
-      id: 'IDTEST2525255',
-      day1: '1',
-      day2: '1',
-      day3: '0',
-      day4: '0',
-      day5: '0',
-    };
-    data.push(newArray);
-  }
-  const tableColumns = [
-    { header: 'Child', dataKey: 'child' },
-    { header: 'ID/Passport', dataKey: 'id' },
-    ...Array.from({ length: numDays }, (_, i) => ({
-      header: `${i + 1}`, // day number as header
-      dataKey: `day${i + 1}`, // unique key for each day column
-    })),
-  ];
-
-  const footer = [
-    'Child Attendance per Day',
-    '', // Placeholder for ID/Passport column
-    '', // Placeholder for Day 1 column
-    '', // Placeholder for Day 2 column
-    // ... continue with empty placeholders for Day 3 to Day 29 columns ...
-  ];
   doc.setTextColor(0, 0, 0);
   autoTable(doc, {
     headStyles: {
@@ -84,9 +60,9 @@ function generateReport(url: string) {
       lineWidth: 0.1,
       lineColor: 0x000000,
     },
-    columns: tableColumns,
-    body: data,
-    foot: [footer],
+    columns: headerColumns,
+    body: bodyRows,
+    foot: footer,
     startY: 40, // Adjust Y coordinate for table placement
     horizontalPageBreak: true, //break table to multiple pages
     didDrawPage: options,
@@ -94,37 +70,54 @@ function generateReport(url: string) {
 
   let afterTable = (doc as any).lastAutoTable.finalY;
   doc.setFontSize(14);
-  doc.text('Total Monthly Attendance:', 10, afterTable + 15);
-  doc.text('Total number of sessions: 198', 100, afterTable + 15);
-  doc.text(
-    'Number of children who attended all sessions: 9',
-    180,
-    afterTable + 15
-  );
+  if (tableBottomContent.length > 0) {
+    doc.text(tableBottomContent[0], 15, afterTable + 15);
+    doc.text(tableBottomContent[1], 120, afterTable + 15);
+    doc.text(tableBottomContent[2], 190, afterTable + 15);
+  }
 
   doc.text('Sign: ', 10, afterTable + 35);
   doc.rect(25, afterTable + 28, 65, 10);
   doc.text('Date: ', 110, afterTable + 35);
   doc.rect(130, afterTable + 28, 65, 10);
 
-  doc.save('report.pdf');
+  doc.save(outputName);
 }
 
 export interface GeneratePdfReportButtonProps {
   title: string;
-  url: string;
+  outputName: string;
+  headerColumns: any[];
+  bodyRows: any[];
+  tableFooter?: any[];
+  content?: any;
+  tableBottomContent?: any;
 }
 
 const GeneratePdfReportButton = ({
   title,
-  url,
+  headerColumns,
+  bodyRows,
+  tableFooter,
+  content,
+  tableBottomContent,
+  outputName,
 }: GeneratePdfReportButtonProps) => {
   return (
     <Button
       type="filled"
       color="primary"
       className={'mt'}
-      onClick={() => generateReport(url)}
+      onClick={() =>
+        generateReport(
+          headerColumns,
+          bodyRows,
+          [tableFooter],
+          content,
+          tableBottomContent,
+          outputName
+        )
+      }
     >
       {renderIcon('DownloadIcon', 'h-5 w-5 text-primary')}
       <Typography
--- src/lxp/src/pages/classroom/attendance/components/attendance-report/components/attendance-monthly-report/attendance-report.tsx
+++ src/lxp/src/pages/classroom/attendance/components/attendance-report/components/attendance-monthly-report/attendance-report.tsx
@@ -30,20 +30,20 @@ export interface MonthlyAttendanceReportProps extends ComponentBaseProps {
 
 export const MonthlyAttendanceReport = ({
   reportMonth,
-  onDownloadReport,
   onBack,
-  classroomGroupId,
   reportData,
 }: MonthlyAttendanceReportProps) => {
-  // const history = useHistory();
   const { isOnline } = useOnlineStatus();
-  const { state } = useLocation<ChildAttendanceReportState>();
   const appDispatch = useAppDispatch();
+  const numDays = 29;
 
-  // const downloadReport = (attendanceSuccessList: AttendanceResult) => {
-  //   if (onDownloadReport) {
-  //     onDownloadReport(new Date());
-  //   }
+  // data that must be used to display in report
+  // const dataX = {
+  //   "detailedReport":[{ child: 'John Doe', id: 'IDTEST2525255', day1: '1', day2: '1', day3: '0', day4: '1' }, { child: 'Jack Bauer', id: 'IDTEST2525255', day1: '1', day2: '1', day3: '0', day4: '1' }],
+  //   "todayAttendance": {day1: '10', day2: '21', day3: '10', day4: '4', },
+  //   "totalMonthlyAttendance": 23,
+  //   "totalSessions": 22,
+  //   "totalChildrenAttendedSessions": 53
   // };
 
   useEffect(() => {
@@ -55,8 +55,53 @@ export const MonthlyAttendanceReport = ({
         })
       );
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isOnline]);
+  }, [appDispatch, isOnline, reportMonth]);
+
+  const data = [
+    { child: 'John', id: 'IDTEST2525255', day1: '1', day2: '1', day3: '0' },
+  ];
+
+  for (let i = 0; i < 50; i++) {
+    const newArray = {
+      child: 'John Bblocks',
+      id: 'IDTEST2525255',
+      day1: '1',
+      day2: '1',
+      day3: '0',
+      day4: '0',
+      day5: '0',
+    };
+    data.push(newArray);
+  }
+  const tableColumns = [
+    { header: 'Child', dataKey: 'child' },
+    { header: 'ID/Passport', dataKey: 'id' },
+    ...Array.from({ length: numDays }, (_, i) => ({
+      header: `${i + 1}`, // day number as header
+      dataKey: `day${i + 1}`, // unique key for each day column
+    })),
+  ];
+
+  const footer = [
+    'Child Attendance per Day',
+    '', // Placeholder for ID/Passport column
+    '', // Placeholder for Day 1 column
+    '', // Placeholder for Day 2 column
+    // ... continue with empty placeholders for Day 3 to Day 29 columns ...
+  ];
+
+  const content = {
+    pageTitle: `${reportMonth}`,
+    subtitle: 'Text 2',
+    practitioner_name: 'Name: Jenny Droe',
+    id_number: 'ID: ID23YGH444',
+    programme_type: 'ProgrammeType: 46372test',
+    programme_days: 'Programmme Days: Monday to Friday',
+    site_address: 'Site Address1234 ABC St, City, State, Country',
+    phone: "Phone: 0123456789"
+  };
+
+  const tableBottomContent = [`Number of children who attended all sessions: 9`, `Total number of sessions: 198`, `Number of children who attended all sessions: 9`]
 
   return (
     <BannerWrapper
@@ -137,8 +182,18 @@ export const MonthlyAttendanceReport = ({
           </div>
         );
       })}
-      <div className={'flex h-full w-full flex-1 flex-col px-4'}>
-        {<GeneratePdfReportButton title="Download Register" url=""/>}
+      <div className={'flex h-full w-full flex-1 flex-col px-4 py-4'}>
+        {
+          <GeneratePdfReportButton
+            title="Download Register"
+            outputName={`${reportMonth}-attandance-report.pdf`}
+            headerColumns={tableColumns}
+            bodyRows={data}
+            tableFooter={footer}
+            content={content}
+            tableBottomContent={tableBottomContent}
+          />
+        }
       </div>
     </BannerWrapper>
   );
