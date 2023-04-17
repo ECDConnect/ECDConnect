88b77197da08290e393a98f839a48c495431fc62
Elisha <elisha@ecdconnect.co.za>
2023-04-17 10:18:12 +0200

    ec-240 fix header on every page

--- src/lxp/src/components/download-pdf-button/download-pdf-button.tsx
+++ src/lxp/src/components/download-pdf-button/download-pdf-button.tsx
@@ -5,19 +5,22 @@ import { Typography, Button, renderIcon } from '@ecdlink/ui';
 function generateReport(url: string) {
   const doc = new jsPDF('l');
 
-  // Add left header
-  doc.setFontSize(12);
-  doc.text('Left Header Text', 10, 10);
+  const options = (data: any) => {
+    // Add table header to each new page
+    // Add left header
+    doc.setFontSize(12);
+    doc.text('Left Header Text', 10, 10);
 
-  // Add right header
-  doc.setFontSize(16);
-  doc.setFont('bold');
-  const pageWidth = doc.internal.pageSize.getWidth();
-  doc.text(
-    'Right Header Text',
-    pageWidth - doc.getStringUnitWidth('Right Header Text') - 50,
-    10
-  );
+    // Add right header
+    doc.setFontSize(16);
+    doc.setFont('bold');
+    const pageWidth = doc.internal.pageSize.getWidth();
+    doc.text(
+      'Right Header Text',
+      pageWidth - doc.getStringUnitWidth('Right Header Text') - 50,
+      10
+    );
+  };
 
   doc.setFontSize(8);
   doc.setFont('bold');
@@ -28,52 +31,17 @@ function generateReport(url: string) {
   doc.text('Program Days: Monday to Friday', 100, 25);
   doc.text('Site Address: 1234 ABC St, City, State, Country', 100, 30);
   doc.setFontSize(5);
-  // const customTableStyle: any = {
-  //   child: { halign: 'left' },
-  //   passport: { halign: 'left' },
-  //   'Day 1': { halign: 'center' },
-  //   'Day 2': { halign: 'center' },
-  //   'Day 3': { halign: 'center' },
-  //   'Day 4': { halign: 'center' },
-  //   'Day 5': { halign: 'center' },
-  //   'Day 6': { halign: 'center' },
-  //   'Day 7': { halign: 'center' },
-  //   'Day 8': { halign: 'center' },
-  //   'Day 9': { halign: 'center' },
-  //   'Day 10': { halign: 'center' },
-  //   'Day 11': { halign: 'center' },
-  //   'Day 12': { halign: 'center' },
-  //   'Day 13': { halign: 'center' },
-  //   'Day 14': { halign: 'center' },
-  //   'Day 15': { halign: 'center' },
-  //   'Day 16': { halign: 'center' },
-  //   'Day 17': { halign: 'center' },
-  //   'Day 18': { halign: 'center' },
-  //   'Day 19': { halign: 'center' },
-  //   'Day 20': { halign: 'center' },
-  //   'Day 21': { halign: 'center' },
-  //   'Day 22': { halign: 'center' },
-  //   'Day 23': { halign: 'center' },
-  //   'Day 24': { halign: 'center' },
-  //   'Day 25': { halign: 'center' },
-  //   'Day 26': { halign: 'center' },
-  //   'Day 27': { halign: 'center' },
-  //   'Day 28': { halign: 'center' },
-  //   'Day 29': { halign: 'center' },
-  // };
+
   const numDays = 29;
-  const numChildren = 50;
+
+  const data = [
+    { child: 'John', id: 'IDTEST2525255', day1: '1', day2: '1', day3:'0' },
+  ];
   
-  // Generate child data
-  const data = Array.from({ length: numChildren }, (_, i) => ({
-    child: `Child ${i + 1}`,
-    id: `IDTEST${Math.floor(Math.random() * 100000000)}`,
-    ...Array.from({ length: numDays }, () => ({
-      [`day${Math.floor(Math.random() * 3)}`]: '1' // randomize between '0' and '1'
-    })).reduce((acc, val) => ({ ...acc, ...val }), {}) // merge day data into object
-  }));
-  
-  // Generate table columns
+    for (let i = 0; i < 50; i++) {
+    const newArray = { child: 'John Bblocks', id: 'IDTEST2525255', day1: '1', day2: '1', day3:'0', day4: '0', day5: '0' };
+    data.push(newArray);
+  }
   const tableColumns = [
     { header: 'Child', dataKey: 'child' },
     { header: 'ID/Passport', dataKey: 'id' },
@@ -102,7 +70,7 @@ function generateReport(url: string) {
     footStyles: {
       textColor: [0, 0, 0],
       fillColor: [211, 211, 211], // Light grey
-      fontSize: 8,
+      fontSize: 6,
       lineWidth: 0.1,
       lineColor: 0x000000,
     },
@@ -110,13 +78,13 @@ function generateReport(url: string) {
       lineWidth: 0.1,
       lineColor: 0x000000,
     },
-    // columnStyles: customTableStyle,
     // head: [tableHeader],
     columns: tableColumns,
     body: data,
     foot: [footer],
     startY: 40, // Adjust Y coordinate for table placement
     horizontalPageBreak: true, //break table to multiple pages
+    didDrawPage: options
   });
 
   let afterTable = (doc as any).lastAutoTable.finalY;
