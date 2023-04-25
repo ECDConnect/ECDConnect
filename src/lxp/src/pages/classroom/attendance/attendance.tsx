b3fa3fadc29f8e762a2f344a24b8bc989bfa5f57
Derren Page <derren@meraki-it.co.za>
2023-04-25 10:41:59 +0200

    chore(merge): add missing function

--- src/lxp/src/pages/classroom/attendance/attendance.tsx
+++ src/lxp/src/pages/classroom/attendance/attendance.tsx
@@ -237,6 +237,11 @@ export const AttendanceComponent: React.FC<ComponentBaseProps> = () => {
     );
   }
 
+  const gotToReports = () => {
+    setSeeRegister(false);
+    setAttendanceComponentType('report');
+  };
+
   return (
     <div>
       <MultiRouteWrapper />
