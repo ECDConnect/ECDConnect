f0f33f9c6416356ea67d845e45178b315a6e9b39
Amanda van Heerden <amanda@meraki-it.co.za>
2023-04-26 08:23:20 +0200

    Bugfix(EC-373): multiple bug fixes

--- src/growgreat/src/schemas/infant/mother-contact-information.ts
+++ src/growgreat/src/schemas/infant/mother-contact-information.ts
@@ -11,7 +11,14 @@ export const initialMothertContactInformationValues: MothertContactInformationMo
     whatsapp: '',
   };
 
+const phoneRegExp =
+  /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+
 export const motherContactInformationModelSchema = Yup.object().shape({
-  cellphone: Yup.string().required('Cellphone number is required'),
+  cellphone: Yup.string()
+    .required('Cellphone number is required')
+    .matches(phoneRegExp, 'Phone number is not valid')
+    .min(10, 'too short')
+    .max(10, 'too long'),
   whatsapp: Yup.string(),
 });
