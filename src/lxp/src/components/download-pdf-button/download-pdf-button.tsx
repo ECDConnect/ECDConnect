5493a1482e2bb8266d194e066ae2edef2283fb58
Elisha <elisha@ecdconnect.co.za>
2023-04-28 00:41:14 +0200

    ec-240 final income pdf refactors

--- src/lxp/src/components/download-pdf-button/download-pdf-button.tsx
+++ src/lxp/src/components/download-pdf-button/download-pdf-button.tsx
@@ -73,6 +73,9 @@ const GeneratePdfReportButton = ({
         doc.text(tableType, 10, 19 + 7);
         lastTableType = tableType;
       }
+
+      const total = tables.reduce((acc, obj) => acc + obj.total, 0);
+
       tables.forEach((table, index) => {
         const headers = table.headers;
         let finalFooter =
@@ -82,10 +85,10 @@ const GeneratePdfReportButton = ({
                 [
                   'Total',
                   ...new Array(headers.length - 2).fill(''),
-                  `${table.total}`,
+                  `R ${table.total}`,
                 ],
               ];
-console.log(table)
+
         // table section with styles
         autoTable(doc, {
           headStyles: tableHeadStyles,
@@ -141,7 +144,7 @@ console.log(table)
             doc.text(content.text_column_two_row_one ?? '', 100, 20);
             doc.text(content.text_column_two_row_two ?? '', 100, 25);
             doc.text(content.text_column_two_row_three ?? '', 100, 30);
-          }
+          },
         });
         // Calculate position for next table
         startY = (doc as any).lastAutoTable.finalY + 10;
@@ -149,14 +152,6 @@ console.log(table)
       if (component === 'income-statements') {
         const columns = ['Additional Notes'];
         const data = [['']];
-        doc.setFillColor(200, 200, 200); // set grey background color
-        doc.rect(
-          15,
-          (doc as any).lastAutoTable.finalY + 4,
-          doc.internal.pageSize.width - 30,
-          10,
-          'F'
-        );
         autoTable(doc, {
           columns,
           headStyles: tableHeadStyles,
@@ -164,37 +159,56 @@ console.log(table)
           columnStyles: { 0: { minCellHeight: 20 } },
           rowPageBreak: 'avoid', // avoid breaking rows into multiple sections
         });
-        doc.setFillColor(215, 215, 215); // set grey background color
+        doc.setFillColor(200, 200, 200); // set grey background color
         doc.rect(
           15,
-          (doc as any).lastAutoTable.finalY + 4,
+          (doc as any).lastAutoTable.finalY + 10,
           doc.internal.pageSize.width - 30,
           15,
           'F'
         );
 
+        //DBE section
+        doc.setFillColor(215, 215, 215); // set grey background color
+        doc.rect(
+          15,
+          (doc as any).lastAutoTable.finalY + 40,
+          doc.internal.pageSize.width - 30,
+          15,
+          'F'
+        );
+        doc.setFontSize(14);
+        doc.setFillColor(255, 255, 255);
+        doc.text('Total', 25, (doc as any).lastAutoTable.finalY + 18);
+        doc.text(
+          `R ${total.toFixed(2)}`,
+          150,
+          (doc as any).lastAutoTable.finalY + 18
+        );
+
+        //DBE reg section
         doc.setDrawColor(0);
         doc.setFontSize(10);
         doc.text(
           'Level of DBE registration:',
           25,
-          (doc as any).lastAutoTable.finalY + 12
+          (doc as any).lastAutoTable.finalY + 48
         );
         doc.setFillColor(255, 0, 0);
-        doc.rect(65, (doc as any).lastAutoTable.finalY + 6, 25, 10, 'S');
+        doc.rect(65, (doc as any).lastAutoTable.finalY + 42, 25, 10, 'S');
         doc.text(
           'Number of Children:',
           105,
-          (doc as any).lastAutoTable.finalY + 12
+          (doc as any).lastAutoTable.finalY + 48
         );
         doc.setFillColor(255, 0, 0);
-        doc.rect(140, (doc as any).lastAutoTable.finalY + 6, 25, 10, 'S');
+        doc.rect(140, (doc as any).lastAutoTable.finalY + 42, 25, 10, 'S');
 
         //sig
-        doc.text('Sign: ', 20, (doc as any).lastAutoTable.finalY + 45);
-        doc.rect(30, (doc as any).lastAutoTable.finalY + 40, 65, 10);
-        doc.text('Date: ', 110, (doc as any).lastAutoTable.finalY + 45);
-        doc.rect(120, (doc as any).lastAutoTable.finalY + 40, 65, 10);
+        doc.text('Sign: ', 20, (doc as any).lastAutoTable.finalY + 66);
+        doc.rect(30, (doc as any).lastAutoTable.finalY + 60, 65, 10);
+        doc.text('Date: ', 110, (doc as any).lastAutoTable.finalY + 66);
+        doc.rect(120, (doc as any).lastAutoTable.finalY + 60, 65, 10);
       }
     });
     doc.setFillColor(255, 255, 255); // set grey background color
@@ -207,11 +221,6 @@ console.log(table)
       doc.text(tableBottomContent[1], 120, afterTable + 15);
       doc.text(tableBottomContent[2], 190, afterTable + 15);
     }
-    //add signature at end of document
-    doc.text('Sign: ', 20, afterTable + 45);
-    doc.rect(30, afterTable + 40, 65, 10);
-    doc.text('Date: ', 110, afterTable + 45);
-    doc.rect(120, afterTable + 40, 65, 10);
 
     //export pdf report
     doc.save(outputName);
