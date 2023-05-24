bb049410da4bbfaae9092d0808ea7f41e6a27c64
lubezerra <lucilene@appy.co.za>
2023-05-24 16:05:55 -0300

    chore(ss): fix step

--- src/lxp/src/pages/coach/coach-practitioner-journey/forms/index.tsx
+++ src/lxp/src/pages/coach/coach-practitioner-journey/forms/index.tsx
@@ -44,7 +44,7 @@ const sessionStorageKey = 'currentStepNumber';
 
 export const Form = ({ visitId, onBack }: FormProps) => {
   const [isTip, setIsTip] = useState(false);
-  const [step, setStep] = useState(2);
+  const [step, setStep] = useState(0);
   const [sectionQuestions, setSectionQuestions] =
     useState<SectionQuestions[]>();
 
