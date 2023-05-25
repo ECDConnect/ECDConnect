53d1eea2b8908f5d60f4b2ece01d24c4f7ca8955
Ubiratan <ubiratan@appy.co.za>
2023-05-25 17:58:08 -0300

    fix(statements): fix statements inputs issues

--- src/lxp/src/schemas/income-statements/dsd-subsidy.ts
+++ src/lxp/src/schemas/income-statements/dsd-subsidy.ts
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-
+const digitsOnly = (value: any) => /^\d+$/.test(value);
 export interface DsdSubsidyModel {
   date: Date | string;
   childrenNumber: number;
@@ -9,7 +9,7 @@ export interface DsdSubsidyModel {
 
 export const dsdSubsidySchema = Yup.object().shape({
   date: Yup.date().required(),
-  childrenNumber: Yup.number().required(),
+  childrenNumber: Yup.number().required().integer().positive().max(100),
   subsidyAmount: Yup.string().required(),
   note: Yup.string(),
 });
