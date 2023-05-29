f3291f399e4a0eaa4a528ebfd6ca68d7877c9c52
Derren Page <derren@meraki-it.co.za>
2023-05-29 22:17:01 +0200

    feat(ec-393): moodle cohort and course assignment

--- src/api/ECDLink.Moodle/Managers/MoodleManager.cs
+++ src/api/ECDLink.Moodle/Managers/MoodleManager.cs
@@ -1,8 +1,11 @@
 ﻿using ECDLink.Moodle.Models;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Npgsql;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection.Metadata;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 
@@ -25,99 +28,35 @@ namespace ECDLink.Moodle.Managers
             await conn.OpenAsync();
 
             // First see if record already exists
-            await using var getUserIdCmd = new NpgsqlCommand("SELECT id FROM public.mdl_user where userName = (@userName)", conn)
-            {
-                Parameters = { new NpgsqlParameter("userName", user.UserName) }
-            };
-            await using var reader = await getUserIdCmd.ExecuteReaderAsync();
+            long userId = await GetMoodleUserId(conn, user.UserName);
 
-            long userId = -1;
-            while (await reader.ReadAsync())
-            {
-                userId = reader.GetInt64(0);
-            }
-            reader.Close();
+            DateTimeOffset dto = new DateTimeOffset(DateTime.Now);
+            long timestamp = dto.ToUnixTimeSeconds();
 
             // Add if new user
             if (userId == -1)
             {
-                await using var cmd = new NpgsqlCommand("INSERT INTO public.mdl_user" +
-                    "(confirmed, mnethostid, username, password, idnumber, firstname, lastname, email, phone1)" +
-                    "VALUES((@Confirmed),(@Mnethostid),(@UserName),(@Password),(@IdNumber),(@Firstname),(@Lastname),(@Email),(@Phone1));",
-                    conn)
-                {
-                    Parameters =
-                    {
-                        new NpgsqlParameter("Confirmed", user.Confirmed),
-                        new NpgsqlParameter("Mnethostid", user.Mnethostid),
-                        new NpgsqlParameter("UserName", user.UserName),
-                        new NpgsqlParameter("Password", "$2y$10$NC4irSPAfnZHUN8HjWXD8e9.MotF0pGqZq6KDPtbfbUquHOQplQbq"), //HashPassword(user.Password)),
-                        new NpgsqlParameter("IdNumber", user.IdNumber),
-                        new NpgsqlParameter("Firstname", user.Firstname),
-                        new NpgsqlParameter("Lastname", user.Lastname),
-                        new NpgsqlParameter("Email", user.Email),
-                        new NpgsqlParameter("Phone1", user.Phone1),
-                    }
-                };
-                await cmd.ExecuteNonQueryAsync();
+                userId = await InsertMoodleUserId(conn, user);
             }
 
             foreach (var cohortName in cohorts)
             {
                 // get cohort id
-                await using var getCohortId = new NpgsqlCommand("SELECT id FROM public.mdl_cohort where name = (@cohortName)", conn)
-                {
-                    Parameters = { new NpgsqlParameter("cohortName", cohortName) }
-                };
-                await using var cohortReader = await getCohortId.ExecuteReaderAsync();
+                long cohortId = await GetMoodleCohortId(conn, cohortName);
+                if (cohortId == -1) continue;
 
-                long cohortId = -1;
-                while (await cohortReader.ReadAsync())
-                {
-                    cohortId = cohortReader.GetInt64(0);
-                }
-                reader.Close();
-
-                // get cohort member id
-                await using var getCohortMemberId = new NpgsqlCommand("SELECT id FROM public.mdl_cohort_members where cohortid = (@cohortId) and userid = (@userId)", conn)
-                {
-                    Parameters = {
-                    new NpgsqlParameter("cohortId", cohortId),
-                    new NpgsqlParameter("userId", userId)
-                }
-
-                };
-                await using var cohortMemberReader = await getCohortMemberId.ExecuteReaderAsync();
-
-                long cohortMemberId = -1;
-                while (await cohortMemberReader.ReadAsync())
-                {
-                    cohortMemberId = cohortMemberReader.GetInt64(0);
-                }
-                reader.Close();
+                long cohortMemberId = await GetMoodleCohortMemberId(conn, cohortId, userId, timestamp);
 
                 // Add the cohort member record if not existed
                 if (cohortMemberId == -1)
                 {
-                    DateTimeOffset dto = new DateTimeOffset(DateTime.Now);
-                    var unixTimeStamp = dto.ToUnixTimeSeconds();
-
-                    await using var cmd = new NpgsqlCommand("INSERT INTO public.mdl_cohort_members" +
-                        "(cohortid, userid, timeadded)" +
-                        "VALUES((@cohortId),(@userId),(@unixTimeStamp))" +
-                        "RETURNING id",
-                        conn)
-                    {
-                        Parameters =
-                    {
-                        new NpgsqlParameter("cohortId", cohortId),
-                        new NpgsqlParameter("userId", userId),
-                        new NpgsqlParameter("unixTimeStamp", unixTimeStamp),
-                    }
-                    };
-
-                    await cmd.ExecuteNonQueryAsync();
+                    await InsertMoodleCohortMemberId(conn, userId, cohortId, timestamp);
                 }
+            }
+
+            if (cohorts.Count > 0)
+            {
+                await EnrolUserToCohortCourses(conn, userId, timestamp);
             }
 
             conn.Close();
@@ -132,34 +71,10 @@ namespace ECDLink.Moodle.Managers
             await using var conn = new NpgsqlConnection(connectionString);
             await conn.OpenAsync();
 
-            // get user id for user name
-            await using var getUserIdCmd = new NpgsqlCommand("SELECT id FROM public.mdl_user where userName = (@userName)", conn)
-            {
-                Parameters = { new NpgsqlParameter("userName", userName) }
-            };
-            await using var reader = await getUserIdCmd.ExecuteReaderAsync();
+            long userId = await GetMoodleUserId(conn, userName);
+            if (userId == -1) return "";
 
-            long userId = -1;
-            while (await reader.ReadAsync())
-            {
-                userId = reader.GetInt64(0);
-            }
-            reader.Close();
-
-            // get session id for user id
-            await using var getSessionIdCmd = new NpgsqlCommand("SELECT sid FROM public.mdl_sessions where userid = (@userId)", conn)
-            {
-                Parameters = { new NpgsqlParameter("userId", userId) }
-            };
-            await using var sessionReader = await getSessionIdCmd.ExecuteReaderAsync();
-
-            string dbSessionId = "";
-            while (await reader.ReadAsync())
-            {
-                dbSessionId = sessionReader.GetString(0);
-            }
-            reader.Close();
-
+            string dbSessionId = await GetMoodleSessionId(conn, userId);
 
             var sessionId = Guid.NewGuid().ToString();
             if (userId >= 0 && dbSessionId == "")
@@ -190,9 +105,7 @@ namespace ECDLink.Moodle.Managers
 
             conn.Close();
 
-            // TODO return error if user is not found
             return sessionId;
-
         }
 
         private static string HashPassword(string password)
@@ -212,6 +125,175 @@ namespace ECDLink.Moodle.Managers
             Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
             Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
             return Convert.ToBase64String(dst);
+        }
+
+        private async Task<long> GetMoodleUserId(NpgsqlConnection conn, string name)
+        {
+            await using var cmd = new NpgsqlCommand("SELECT id FROM public.mdl_user where userName = (@userName)", conn)
+            {
+                Parameters = { new NpgsqlParameter("userName", name) }
+            };
+            await using var reader = await cmd.ExecuteReaderAsync();
+
+            long userId = -1;
+            while (await reader.ReadAsync())
+            {
+                userId = reader.GetInt64(0);
+            }
+            reader.Close();
+            return userId;
+        }
+
+        private async Task<long> InsertMoodleUserId(NpgsqlConnection conn, MoodleUser user)
+        {
+            DateTimeOffset dto = new DateTimeOffset(DateTime.Now);
+            var unixTimeStamp = dto.ToUnixTimeSeconds();
+
+            await using var cmd = new NpgsqlCommand("INSERT INTO public.mdl_user" +
+                "(confirmed, mnethostid, username, password, idnumber, firstname, lastname, email, phone1, country, timezone, description, timecreated, timemodified, imagealt, lastnamephonetic, firstnamephonetic, middlename, alternatename, moodlenetprofile)" +
+                "VALUES((@Confirmed),(@Mnethostid),(@UserName),(@Password),(@IdNumber),(@Firstname),(@Lastname),(@Email),(@Phone1),(@Country),(@Timezone),(''),(@TimeCreated),(@TimeModified),(''),(''),(''),(''),(''),(''));",
+                conn)
+            {
+                Parameters =
+                    {
+                        new NpgsqlParameter("Confirmed", user.Confirmed),
+                        new NpgsqlParameter("Mnethostid", user.Mnethostid),
+                        new NpgsqlParameter("UserName", user.UserName),
+                        new NpgsqlParameter("Password", "$2y$10$NC4irSPAfnZHUN8HjWXD8e9.MotF0pGqZq6KDPtbfbUquHOQplQbq"), //HashPassword(user.Password)),
+                        new NpgsqlParameter("IdNumber", user.IdNumber),
+                        new NpgsqlParameter("Firstname", user.Firstname),
+                        new NpgsqlParameter("Lastname", user.Lastname),
+                        new NpgsqlParameter("Email", user.Email),
+                        new NpgsqlParameter("Phone1", user.Phone1),
+                        new NpgsqlParameter("Country", user.Country),
+                        new NpgsqlParameter("Timezone", user.Timezone),
+                        new NpgsqlParameter("TimeCreated", unixTimeStamp),
+                        new NpgsqlParameter("TimeModified", unixTimeStamp)
+                    }
+            };
+            await cmd.ExecuteNonQueryAsync();
+
+            long userId = await GetMoodleUserId(conn, user.UserName);
+            return userId;
+        }
+
+        private async Task<long> GetMoodleCohortId(NpgsqlConnection conn, string name)
+        {
+            await using var cmd = new NpgsqlCommand("SELECT id FROM public.mdl_cohort where name = (@cohortName)", conn)
+            {
+                Parameters = { new NpgsqlParameter("cohortName", name) }
+            };
+            await using var reader = await cmd.ExecuteReaderAsync();
+
+            long cohortId = -1;
+            while (await reader.ReadAsync())
+            {
+                cohortId = reader.GetInt64(0);
+            }
+            reader.Close();
+
+            return cohortId;
+        }
+
+        private async Task InsertMoodleCohortMemberId(NpgsqlConnection conn, long userId, long cohortId, long timestamp = 0)
+        {
+            if (timestamp == 0)
+            {
+                DateTimeOffset dto = new DateTimeOffset(DateTime.Now);
+                timestamp = dto.ToUnixTimeSeconds();
+            }
+
+            await using var cmd = new NpgsqlCommand("INSERT INTO public.mdl_cohort_members" +
+                "(cohortid, userid, timeadded)" +
+                "VALUES((@cohortId),(@userId),(@unixTimeStamp))",
+                conn)
+            {
+                Parameters =
+                        {
+                            new NpgsqlParameter("cohortId", cohortId),
+                            new NpgsqlParameter("userId", userId),
+                            new NpgsqlParameter("unixTimeStamp", timestamp),
+                        }
+            };
+
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+        private async Task<long> GetMoodleCohortMemberId(NpgsqlConnection conn, long cohortId, long userId, long timestamp = 0)
+        {
+            if (timestamp == 0)
+            {
+                DateTimeOffset dto = new DateTimeOffset(DateTime.Now);
+                timestamp = dto.ToUnixTimeSeconds();
+            }
+
+            await using var cmd = new NpgsqlCommand("SELECT id FROM public.mdl_cohort_members where cohortid = (@cohortId) and userid = (@userId)", conn)
+            {
+                Parameters = {
+                    new NpgsqlParameter("cohortId", cohortId),
+                    new NpgsqlParameter("userId", userId)
+                }
+
+            };
+            await using var reader = await cmd.ExecuteReaderAsync();
+
+            long cohortMemberId = -1;
+            while (await reader.ReadAsync())
+            {
+                cohortMemberId = reader.GetInt64(0);
+            }
+            reader.Close();
+
+            return cohortMemberId;
+        }
+
+        private async Task EnrolUserToCohortCourses(NpgsqlConnection conn, long userId, long timestamp = 0)
+        {
+            if (timestamp == 0)
+            {
+                DateTimeOffset dto = new DateTimeOffset(DateTime.Now);
+                timestamp = dto.ToUnixTimeSeconds();
+            }
+
+            await using var cmd = new NpgsqlCommand(@"
+insert into public.mdl_user_enrolments
+(status, enrolid, userid, timestart, timeend, modifierid, timecreated, timemodified)
+select 0, me.id, mcm.userid, 0, 0, 2, @timestamp, @timestamp
+from public.mdl_user mu
+join public.mdl_cohort_members mcm on mcm.userid = mu.id
+join public.mdl_enrol me on me.enrol = 'cohort' and mcm.cohortid = me.customint1
+left join public.mdl_user_enrolments mue on mue.enrolid = me.id and mue.userid = mcm.userid
+where mu.id = @userId
+    and mue.id  is null;
+                ",
+            conn)
+            {
+                Parameters =
+                    {
+                        new NpgsqlParameter("userId", userId),
+                        new NpgsqlParameter("timestamp", timestamp),
+                    }
+            };
+
+            await cmd.ExecuteNonQueryAsync();
+        }
+
+        private async Task<string> GetMoodleSessionId(NpgsqlConnection conn, long userId)
+        {
+            await using var cmd = new NpgsqlCommand("SELECT sid FROM public.mdl_sessions where userid = (@userId)", conn)
+            {
+                Parameters = { new NpgsqlParameter("userId", userId) }
+            };
+            await using var reader = await cmd.ExecuteReaderAsync();
+
+            string dbSessionId = "";
+            while (await reader.ReadAsync())
+            {
+                dbSessionId = reader.GetString(0);
+            }
+            reader.Close();
+
+            return dbSessionId;
         }
 
     }
