14f00ca134dd667e6549704eaec9d0f97fb5acec
Liana Wiehahn <liana@ecdconnect.co.za>
2023-05-29 09:28:09 +0200

    feat(accreditation): add annual accreditation
    EC-435

--- src/api/Libraries/Modules/ECDLink.DataAccessLayer/Entities/Users/Mapping/PractitionerTimeline.cs
+++ src/api/Libraries/Modules/ECDLink.DataAccessLayer/Entities/Users/Mapping/PractitionerTimeline.cs
@@ -8,6 +8,7 @@ namespace ECDLink.DataAccessLayer.Entities.Users.Mapping
     {
         public string StarterLicenseStatus { get; set; }
         public string SmartSpaceLicenseStatus { get; set; }
+        public string PracticeLicenseStatus { get; set; }
         public string ConsolidationMeetingStatus { get; set; }
         public string FirstAidCourseStatus { get; set; }
         public string ClubMeetingDate1Status { get; set; }
@@ -19,9 +20,11 @@ namespace ECDLink.DataAccessLayer.Entities.Users.Mapping
         public string CoachingCircle2Status { get; set; }
         public string CoachingCircle3Status { get; set; }
         public string CoachingCircle4Status { get; set; }
+        public string ChildProgressTrainingStatus { get; set; }
 
         public string StarterLicenseColor { get; set; }
         public string SmartSpaceLicenseColor { get; set; }
+        public string PracticeLicenseColor { get; set; }
         public string ConsolidationMeetingColor { get; set; }
         public string FirstAidCourseColor { get; set; }
         public string ClubMeetingDate1Color { get; set; }
@@ -33,10 +36,12 @@ namespace ECDLink.DataAccessLayer.Entities.Users.Mapping
         public string CoachingCircle2Color { get; set; }
         public string CoachingCircle3Color { get; set; }
         public string CoachingCircle4Color { get; set; }
+        public string ChildProgressTrainingColor { get; set; }
 
 
         public DateTime? StarterLicenseDate { get; set; }
         public DateTime? SmartSpaceLicenseDate { get; set; }
+        public DateTime? PracticeLicenseDate { get; set; }
         public DateTime? ConsolidationMeetingDate { get; set; }
         public DateTime? FirstAidDate { get; set; }
         public DateTime? ClubMeetingDate1 { get; set; }
@@ -44,11 +49,11 @@ namespace ECDLink.DataAccessLayer.Entities.Users.Mapping
         public DateTime? ClubMeetingDate3 { get; set; }
         public DateTime? PrePQAVisitDate1 { get; set; }
         public DateTime? PrePQAVisitDate2 { get; set; }
-
         public DateTime? CoachingCircleDate1 { get; set; }
         public DateTime? CoachingCircleDate2 { get; set; }
         public DateTime? CoachingCircleDate3 { get; set; }
         public DateTime? CoachingCircleDate4 { get; set; }
+        public DateTime? ChildProgressTrainingDate { get; set; }
 
         public virtual ICollection<Visit> PrePQASiteVisits { get; set; }
         public virtual ICollection<Visit> PQASiteVisits { get; set; }
