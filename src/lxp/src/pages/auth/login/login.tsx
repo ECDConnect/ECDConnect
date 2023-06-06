9057a2f4079b1c121caff189903baa29302e18fd
Ubiratan <ubiratan@appy.co.za>
2023-06-05 23:08:42 -0300

    feat(config): add an offline credentials check

--- src/lxp/src/pages/auth/login/login.tsx
+++ src/lxp/src/pages/auth/login/login.tsx
@@ -12,7 +12,7 @@ import {
   DialogPosition,
 } from '@ecdlink/ui';
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, useWatch } from 'react-hook-form';
 import { useHistory } from 'react-router-dom';
 import * as styles from './login.styles';
 import {
@@ -26,6 +26,8 @@ import { useOnlineStatus } from '@hooks/useOnlineStatus';
 import { settingActions } from '@store/settings';
 import ROUTES from '@routes/routes';
 import { StorageFull } from './storage-full/storage-full';
+import { getTime } from 'date-fns';
+var CryptoJS = require('crypto-js');
 const { version } = require('../../../../package.json');
 
 export const Login: React.FC = () => {
@@ -52,14 +54,24 @@ export const Login: React.FC = () => {
     setValue: loginSetValue,
     formState: loginFormState,
     getValues: loginFormGetValues,
+    control,
   } = useForm({
     resolver: yupResolver(loginSchema),
     defaultValues: initialLoginValues,
     mode: 'onChange',
   });
   const { isValid, errors } = loginFormState;
+  const { password } = useWatch({ control });
+
+  const userHash = CryptoJS.AES.encrypt(password, 'secret key 123').toString();
+  const userLocalxpiration = Date.now() + 3600000;
 
   const submitForm = async () => {
+    localStorage.setItem('userHash', JSON.stringify(userHash));
+    localStorage.setItem(
+      'userLocalxpiration',
+      JSON.stringify(userLocalxpiration)
+    );
     setDisplayError(false);
     if (isValid) {
       if (freeMemory > 50 || freeMemory === 0) {
