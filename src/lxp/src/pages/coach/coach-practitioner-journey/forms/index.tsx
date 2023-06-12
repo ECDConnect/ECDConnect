4d0172def5e9a7bc222a4334138a38db87406c75
Amanda van Heerden <amanda@meraki-it.co.za>
2023-06-12 16:28:04 +0200

    fix(ss/coach-pre-pqa): add polishes
    EC-477 EC-730

--- src/lxp/src/pages/coach/coach-practitioner-journey/forms/index.tsx
+++ src/lxp/src/pages/coach/coach-practitioner-journey/forms/index.tsx
@@ -180,18 +180,19 @@ export const Form = ({ visitId, onBack }: FormProps) => {
     setStep((preState) => preState + 1);
   }, []);
 
-  const displayChildrenDialog = useCallback(() => {
-    dialog({
-      blocking: false,
-      position: DialogPosition.Middle,
-      color: 'bg-white',
-      render: (onClose) => {
-        return (
-          <ActionModal
-            className="z-50"
-            customIcon={<IconRobot className="mb-4" />}
-            title={`Would you like to register any children for ${name}’s programme?`}
-            detailText={`You can register children on your phone now. Or, help ${name} to register children on her phone.
+  const displayChildrenDialog = useCallback(
+    (name: string) => {
+      dialog({
+        blocking: false,
+        position: DialogPosition.Middle,
+        color: 'bg-white',
+        render: (onClose) => {
+          return (
+            <ActionModal
+              className="z-50"
+              customIcon={<IconRobot className="mb-4" />}
+              title={`Would you like to register any children for ${name}’s programme?`}
+              detailText={`You can register children on your phone now. Or, help ${name} to register children on her phone.
             ${
               !isOnline
                 ? `\n
@@ -199,39 +200,41 @@ export const Form = ({ visitId, onBack }: FormProps) => {
             \nIn order for you to view the answers, it is necessary to synchronize your account.`
                 : ''
             }`}
-            actionButtons={[
-              {
-                colour: 'primary',
-                text: 'Yes, register children now',
-                textColour: 'white',
-                type: 'filled',
-                leadingIcon: 'CheckCircleIcon',
-                onClick: () => {
-                  onBack?.();
-                  history.push(ROUTES.CHILD_REGISTRATION_LANDING, {
-                    practitionerId,
-                  });
-                  onClose();
+              actionButtons={[
+                {
+                  colour: 'primary',
+                  text: 'Yes, register children now',
+                  textColour: 'white',
+                  type: 'filled',
+                  leadingIcon: 'CheckCircleIcon',
+                  onClick: () => {
+                    onBack?.();
+                    history.push(ROUTES.CHILD_REGISTRATION_LANDING, {
+                      practitionerId,
+                    });
+                    onClose();
+                  },
                 },
-              },
-              {
-                colour: 'primary',
-                text: 'No, skip',
-                textColour: 'primary',
-                type: 'outlined',
-                leadingIcon: 'XIcon',
-                onClick: () => {
-                  setTimeout(() => onSuccess(), 100);
-                  onBack?.();
-                  onClose();
+                {
+                  colour: 'primary',
+                  text: 'No, skip',
+                  textColour: 'primary',
+                  type: 'outlined',
+                  leadingIcon: 'XIcon',
+                  onClick: () => {
+                    setTimeout(() => onSuccess(), 100);
+                    onBack?.();
+                    onClose();
+                  },
                 },
-              },
-            ]}
-          />
-        );
-      },
-    });
-  }, [dialog, history, isOnline, name, onBack, onSuccess, practitionerId]);
+              ]}
+            />
+          );
+        },
+      });
+    },
+    [dialog, history, isOnline, onBack, onSuccess, practitionerId]
+  );
 
   const displayOfflineWarning = useCallback(() => {
     dialog({
@@ -322,7 +325,11 @@ export const Form = ({ visitId, onBack }: FormProps) => {
           })
         );
         appDispatch(pqaThunkActions.addVisitFormData(payload));
-        displayChildrenDialog();
+        displayChildrenDialog(
+          activityName === visitTypes.prePqa.first.name
+            ? 'First site visit'
+            : 'Second site visit'
+        );
         break;
     }
   }, [
