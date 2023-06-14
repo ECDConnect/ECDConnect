c82a25bb64579f63cd86b0d6aca6ca9c3ef639d7
Liana Wiehahn <liana@ecdconnect.co.za>
2023-06-14 10:31:47 +0200

    feat(community tabs): adding tabs with coming soon
    EC-363

--- src/lxp/src/pages/community/community.tsx
+++ src/lxp/src/pages/community/community.tsx
@@ -23,6 +23,16 @@ export const Community: React.FC = () => {
 
   const tabItems: TabItem[] = [
     {
+      title: 'Clubs',
+      initActive: false,
+      child: 'Coming Soon!',
+    },
+    {
+      title: 'Leagues',
+      initActive: false,
+      child: 'Coming Soon!',
+    },
+    {
       title: 'Connect',
       initActive: true,
       child: <Connect />,
