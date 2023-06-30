bea47cd17eda994d492871e32ee704c22aaa0d40
luhbezerra <lucilene@ecdconnect.co.za>
2023-06-30 14:26:39 -0300

    fix(gg/infant): fix charts
    EC-870

--- src/growgreat/src/pages/infant/infant-profile/progress-tab/activity-list/forms/pillar-1-steps/weight-and-length/result/index.tsx
+++ src/growgreat/src/pages/infant/infant-profile/progress-tab/activity-list/forms/pillar-1-steps/weight-and-length/result/index.tsx
@@ -460,7 +460,6 @@ export const WeightAndLengthResultStep = ({
           differenceFunction = differenceInYears;
           break;
       }
-
       const newResult = [...result];
 
       const formattedResult = data.map((item) => {
@@ -468,12 +467,15 @@ export const WeightAndLengthResultStep = ({
           new Date(item.visit?.plannedVisitDate),
           new Date(age)
         );
-        // if date is 0, it means that planned date and age is in same month and year
-        if (date === 0) {
-          date = 1;
-        }
+
         const scale = dateAxios;
         const index = scale.indexOf(date);
+
+        if (index === -1) {
+          newResult[0] = Number(item?.questionAnswer);
+
+          return newResult;
+        }
 
         if (index !== -1) {
           newResult[index] = Number(item?.questionAnswer);
@@ -483,6 +485,8 @@ export const WeightAndLengthResultStep = ({
 
         return result;
       })[0];
+
+      console.log({ formattedResult });
 
       return formattedResult;
     },
@@ -528,12 +532,13 @@ export const WeightAndLengthResultStep = ({
 
     const {
       years: ageYears,
-      months: ageMonths,
+      months: ageMonthsPart,
       days: ageDays,
     } = getAgeInYearsMonthsAndDays(dateOfBirth);
     const ageWeeks = differenceInWeeks(new Date(), new Date(dateOfBirth));
+    const ageMonths = differenceInMonths(new Date(), new Date(dateOfBirth));
 
-    if (!ageYears && !ageMonths && ageDays <= 14) {
+    if (!ageYears && !ageMonthsPart && ageDays <= 14) {
       const weightChartData = getChartData(ageDays, weightPerDay);
       const lengthChartDate = getChartData(ageDays, lengthPerDay);
 
@@ -595,10 +600,10 @@ export const WeightAndLengthResultStep = ({
       return setWeightAxios(weightChartData);
     }
 
-    if (!ageYears && ageMonths <= 12) {
+    if (ageYears < 2) {
       const weightChartData = getChartData(ageMonths, weightPerMonth);
-      const lengthChartDate = getChartData(ageMonths, lengthPerMonth);
-
+      const lengthChartData = getChartData(ageMonths, lengthPerMonth);
+      console.log({ weightPerMonth, lengthPerMonth });
       setWeightResult(
         fillInMissingNumbers(
           getWeightOrLengthResult(
@@ -615,14 +620,14 @@ export const WeightAndLengthResultStep = ({
           getWeightOrLengthResult(
             dateOfBirth,
             lengthHistory,
-            lengthChartDate.date,
+            lengthChartData.date,
             'm',
             lengthOrHeightResult
           )
         )
       );
       setSuffix('m');
-      setLengthAxios(lengthChartDate);
+      setLengthAxios(lengthChartData);
       return setWeightAxios(weightChartData);
     }
 
@@ -664,6 +669,7 @@ export const WeightAndLengthResultStep = ({
     groupedGrowthData?.weight,
     infant?.user?.dateOfBirth,
   ]);
+  console.log({ lengthOrHeightResult, weightResult });
 
   useEffect(() => {
     setGrowthMonitoring?.({
