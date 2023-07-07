0a109f1774b0b2663cbbc2d2bc38a97b1daea14d
birarr <birarr@gmail.com>
2023-07-07 15:33:46 -0300

    feat(settings): create sync check by app use

--- src/lxp/src/pages/dashboard/dashboard.tsx
+++ src/lxp/src/pages/dashboard/dashboard.tsx
@@ -50,6 +50,7 @@ import { convertImageToBase64 } from '@/utils/common/convert-image-to-64.utils';
 import { traineeSelectors, traineeThunkActions } from '@/store/trainee';
 import { timelineSteps } from '../trainee/trainee-onboarding/components/trainee-onboarding-dashboard/timeline-steps';
 import { calendarThunkActions } from '@/store/calendar';
+import { differenceInHours, isSameDay } from 'date-fns';
 // import { browserName, browserVersion } from 'react-device-detect';
 const { version } = require('../../../package.json');
 
@@ -102,6 +103,43 @@ export const Dashboard: React.FC = () => {
   const dashboardNotification = useSelector(
     notificationsSelectors.getDashboardNotification
   );
+
+  useEffect(() => {
+    if (document.hasFocus()) {
+      console.log('focusssssssssssssssssssss');
+    }
+  }, []);
+
+  // User has switched back to the tab
+  // const onFocus = () => {
+  //   const focusItem = JSON.parse(localStorage?.getItem('appFocus')!)
+  //   console.log({focusItem})
+  //   const isSameDayItem = isSameDay(new Date(), new Date(focusItem))
+  //   const differenceInHoursBetweenItems = differenceInHours(new Date(), new Date(focusItem))
+  //   console.log({isSameDayItem})
+  //   console.log({differenceInHoursBetweenItems})
+  //   if(!isSameDayItem ||  differenceInHoursBetweenItems > 6) {
+  //   localStorage.setItem('appFocus', JSON.stringify(new Date()));
+  //   }
+  //   console.log("Tab is in focus");
+  // };
+
+  // User has switched away from the tab (AKA tab is hidden)
+  // const onBlur = () => {
+  //   console.log("Tab is blurred");
+  // };
+
+  //     useEffect(() => {
+  //       window.addEventListener("focus", onFocus);
+  //       window.addEventListener("blur", onBlur);
+  //       // Calls onFocus when the window first loads
+  //       onFocus();
+  //       // Specify how to clean up after this effect:
+  //       return () => {
+  //           window.removeEventListener("focus", onFocus);
+  //           window.removeEventListener("blur", onBlur);
+  //       };
+  // }, []);
 
   const timeline = useSelector(traineeSelectors.getTraineeOnboardTimeline);
   const uncompletedSteps = timelineSteps(
