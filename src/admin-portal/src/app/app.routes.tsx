ec8f33e1439dd9371d55799f1989afeb923c3361
Elisha <elisha@ecdconnect.co.za>
2023-07-11 15:53:53 +0200

    fix reset password

--- src/admin-portal/src/app/app.routes.tsx
+++ src/admin-portal/src/app/app.routes.tsx
@@ -48,9 +48,8 @@ const PublicRoutes: React.FC = () => {
       <Route exact path="/" component={Login} />
       <Route exact path="/register/:resetToken" component={Register} />
       <Route exact path="/forgot-password" component={ForgotPassword} />
-      <Route exact path="/reset/:resetToken/:email" component={ResetPassword} />
+      <Route exact path="/reset/:resetToken" component={ResetPassword} />
       <Route path={`/ecd-terms`} component={TermsPage}></Route>
-
     </Switch>
   );
 };
