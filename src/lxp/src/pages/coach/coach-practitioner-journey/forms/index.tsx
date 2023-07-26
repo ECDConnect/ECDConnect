d02098321da65b34a502e9c6fe9859f8c60b0dee
lubezerra <lucilene@ecdconnect.co.za>
2023-07-26 16:42:53 -0300

    fix(ss/coach): fix programme details
    EC-511

--- src/lxp/src/pages/coach/coach-practitioner-journey/forms/index.tsx
+++ src/lxp/src/pages/coach/coach-practitioner-journey/forms/index.tsx
@@ -98,7 +98,7 @@ export const Form = ({
   setPqaRating: setPqaRatingForm,
 }: FormProps) => {
   const [isTip, setIsTip] = useState(false);
-  const [step, setStep] = useState(0);
+  const [step, setStep] = useState(14);
   const [sectionQuestions, setSectionQuestions] =
     useState<SectionQuestions[]>();
   const [currentActivity, setCurrentActivity] = useState('');
