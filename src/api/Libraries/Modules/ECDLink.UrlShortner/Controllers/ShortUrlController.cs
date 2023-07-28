f9dc3c5a137c8935ef2094aaec07efcb3dbbc4a2
Amanda van Heerden <amanda@meraki-it.co.za>
2023-07-28 14:29:03 +0200

    fix(ec-1337): test url redirect to login

--- src/api/Libraries/Modules/ECDLink.UrlShortner/Controllers/ShortUrlController.cs
+++ src/api/Libraries/Modules/ECDLink.UrlShortner/Controllers/ShortUrlController.cs
@@ -1,3 +1,5 @@
+using ECDLink.Core.Services.Interfaces;
+using ECDLink.Core.SystemSettings.SystemOptions;
 using ECDLink.UrlShortner.Managers;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -8,20 +10,25 @@ namespace ECDLink.UrlShortner.Controllers
     public class ShortUrlController : ControllerBase
     {
         private readonly ShortUrlManager _manager;
+        private ISystemSetting<SecurityNotificationOptions> _options;
 
-        public ShortUrlController(ShortUrlManager manager)
+        public ShortUrlController(ShortUrlManager manager, ISystemSetting<SecurityNotificationOptions> optionAccessor)
         {
             _manager = manager;
+            _options = optionAccessor;
         }
 
         [HttpGet, Route("/{chunk}")]
         public IActionResult ShortenRedirect([FromRoute] string chunk)
         {
             var redirect = _manager.GetRedirectFromChunk(chunk);
+            var loginUrl = _options.Value.Login;
 
             if (string.IsNullOrWhiteSpace(redirect))
             {
-                throw new Exception("Error parsing Chunk");
+                Console.WriteLine("Error parsing chunk - redirect to Login");
+                return Redirect(loginUrl);
+                //throw new Exception("Error parsing Chunk");
             }
 
             return Redirect(redirect);
