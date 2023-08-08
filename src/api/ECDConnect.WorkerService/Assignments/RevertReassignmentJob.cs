f9fcd75b6ccdee1ad341eefb6c86e2d0158bbd7d
Tiaan <tiaan@appy.co.za>
2023-08-08 08:33:16 +0200

    fix(users): delete children older than 30 days not fully registered

--- src/api/ECDConnect.WorkerService/Assignments/RevertReassignmentJob.cs
+++ src/api/ECDConnect.WorkerService/Assignments/RevertReassignmentJob.cs
@@ -1,28 +1,28 @@
-﻿//using ECDLink.AutomatedJobs.Cron;
-//using ECDLink.Core.Services.Interfaces;
+﻿using ECDLink.AutomatedJobs.Cron;
+using ECDLink.Core.Services.Interfaces;
 
-//namespace ECDConnect.WorkerService.Assignments;
+namespace ECDConnect.WorkerService.Assignments;
 
-//public class RevertReassignmentJob : CronJobService
-//{
-//    private readonly IServiceScopeFactory _scopeFactory;
+public class RevertReassignmentJob : CronJobService
+{
+    private readonly IServiceScopeFactory _scopeFactory;
 
-//    public RevertReassignmentJob(IServiceScopeFactory scopeFactory, IScheduleConfig<RevertReassignmentJob> config)
-//        : base(config.CronExpression, config.TimeZoneInfo)
-//    {
-//        _scopeFactory = scopeFactory;
-//    }
+    public RevertReassignmentJob(IServiceScopeFactory scopeFactory, IScheduleConfig<RevertReassignmentJob> config)
+        : base(config.CronExpression, config.TimeZoneInfo)
+    {
+        _scopeFactory = scopeFactory;
+    }
 
-//    public override async Task DoWork(CancellationToken cancellationToken)
-//    {
-//        using (var scope = _scopeFactory.CreateScope())
-//        {
-//            var anonChildService = scope.ServiceProvider.GetRequiredService<IChildrenAnonymiseService>();
+    public override async Task DoWork(CancellationToken cancellationToken)
+    {
+        using (var scope = _scopeFactory.CreateScope())
+        {
+            var anonChildService = scope.ServiceProvider.GetRequiredService<IChildrenAnonymiseService>();
 
-//            //anonChildService.AnonymiseChild();
+            //anonChildService.AnonymiseChild();
 
-//            //AssignFutureAbsentees //settle Future dated absentees first
-//            //ReassignAbsentees
-//        }
-//    }
-//}
+            //AssignFutureAbsentees //settle Future dated absentees first
+            //ReassignAbsentees
+        }
+    }
+}
