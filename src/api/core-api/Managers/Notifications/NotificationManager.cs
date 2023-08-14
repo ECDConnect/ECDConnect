8a8f280bed30f39f39790350c9792abafcac6bbf
Tiaan <tiaan@appy.co.za>
2023-08-14 12:04:45 +0200

    fix(notifications): improve notifications

--- src/api/core-api/Managers/Notifications/NotificationManager.cs
+++ src/api/core-api/Managers/Notifications/NotificationManager.cs
@@ -13,7 +13,7 @@ namespace EcdLink.Api.CoreApi.Managers.Notifications
     public class NotificationManager
     {
         private INotificationProviderFactory<ApplicationUser> _notificationProviderFactory;
-        private ISystemSetting<InvitationOptions> _options;
+        private ISystemSetting<InvitationOptions> _options;        
 
         public NotificationManager(INotificationProviderFactory<ApplicationUser> notificationProviderFactory, ISystemSetting<InvitationOptions> optionAccessor)
         {
@@ -21,6 +21,65 @@ namespace EcdLink.Api.CoreApi.Managers.Notifications
             _options = optionAccessor;
         }
 
+        public async Task SendNotificationAsync(ApplicationUser user, string token)
+        {
+            var encodedToken = TokenHelper.EncodeToken(token);
+
+            var invitationUrl = $"{_options.Value.AdminSignup}/{encodedToken}";
+            var applicationName = TenantExecutionContext.Tenant.ApplicationName;
+            var organisationName = TenantExecutionContext.Tenant.OrganisationName;
+            string firstName = user.FirstName;
+
+            var notificationProvider = _notificationProviderFactory.Create(user);
+
+            await notificationProvider
+              .SetMessageTemplate(TemplateTypeEnum.AdminPortalInvitation)
+              .AddOrUpdateFieldReplacement(MessageTemplateConstants.InvitationLink, invitationUrl)
+              .AddOrUpdateFieldReplacement(MessageTemplateConstants.FirstName, firstName)
+              .AddOrUpdateFieldReplacement(MessageTemplateConstants.ApplicationName, applicationName)
+              .AddOrUpdateFieldReplacement(MessageTemplateConstants.OrganisationName, organisationName)
+              .SendMessageAsync();
+        }
+
+        public async Task SendEmailAsync(ApplicationUser user, string token)
+        {
+            var encodedToken = TokenHelper.EncodeToken(token);
+
+            var invitationUrl = $"{_options.Value.AdminSignup}/{encodedToken}";
+            var applicationName = TenantExecutionContext.Tenant.ApplicationName;
+            var organisationName = TenantExecutionContext.Tenant.OrganisationName;
+            string firstName = user.FirstName;
+
+            var notificationProvider = _notificationProviderFactory.Create(user);
+
+            await notificationProvider
+              .SetMessageTemplate(TemplateTypeEnum.AdminPortalInvitation)
+              .AddOrUpdateFieldReplacement(MessageTemplateConstants.InvitationLink, invitationUrl)
+              .AddOrUpdateFieldReplacement(MessageTemplateConstants.FirstName, firstName)
+              .AddOrUpdateFieldReplacement(MessageTemplateConstants.ApplicationName, applicationName)
+              .AddOrUpdateFieldReplacement(MessageTemplateConstants.OrganisationName, organisationName)
+              .SendMessageAsync();
+        }
+
+        public async Task SendSMSAsync(ApplicationUser user, string token)
+        {
+            var encodedToken = TokenHelper.EncodeToken(token);
+
+            var invitationUrl = $"{_options.Value.AdminSignup}/{encodedToken}";
+            var applicationName = TenantExecutionContext.Tenant.ApplicationName;
+            var organisationName = TenantExecutionContext.Tenant.OrganisationName;
+            string firstName = user.FirstName;
+
+            var notificationProvider = _notificationProviderFactory.Create(user);
+
+            await notificationProvider
+              .SetMessageTemplate(TemplateTypeEnum.AdminPortalInvitation)
+              .AddOrUpdateFieldReplacement(MessageTemplateConstants.InvitationLink, invitationUrl)
+              .AddOrUpdateFieldReplacement(MessageTemplateConstants.FirstName, firstName)
+              .AddOrUpdateFieldReplacement(MessageTemplateConstants.ApplicationName, applicationName)
+              .AddOrUpdateFieldReplacement(MessageTemplateConstants.OrganisationName, organisationName)
+              .SendMessageAsync();
+        }
 
     }
 }
