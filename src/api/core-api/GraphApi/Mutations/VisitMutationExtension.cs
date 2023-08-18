2834e5295740b6c0b0a9c7661f65fdc9b883c6f5
Liana Wiehahn <liana@ecdconnect.co.za>
2023-08-18 07:34:42 +0200

    fix(points): fix mother visit points
    EC-1363

--- src/api/core-api/GraphApi/Mutations/VisitMutationExtension.cs
+++ src/api/core-api/GraphApi/Mutations/VisitMutationExtension.cs
@@ -1,6 +1,5 @@
 ﻿using EcdLink.Api.CoreApi.GraphApi.Models.GrowGreat;
 using EcdLink.Api.CoreApi.Managers.Visits;
-using ECDLink.Abstractrions.Enums;
 using ECDLink.Abstractrions.GraphQL.Enums;
 using ECDLink.DataAccessLayer.Entities;
 using ECDLink.DataAccessLayer.Entities.Users;
@@ -515,7 +514,7 @@ namespace EcdLink.Api.CoreApi.GraphApi.Mutations
             Coach coach = coachRepo.GetAll().Where(x => x.UserId == input.CoachId.ToString()).FirstOrDefault();
             Practitioner practitioner = practitionerRepo.GetAll().Where(x => x.UserId == input.PractitionerId.ToString()).FirstOrDefault();
 
-            if (input.CoachId == null || input.PractitionerId == null)
+            if (coach == null || practitioner == null)
             {
                 return new Visit();
             }
