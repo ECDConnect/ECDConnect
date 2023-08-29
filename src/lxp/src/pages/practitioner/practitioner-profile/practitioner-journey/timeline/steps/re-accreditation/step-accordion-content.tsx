c6c8103f5ff0aeac247a3dde78e51795b486f555
lubezerra <lucilene@ecdconnect.co.za>
2023-08-29 11:08:05 -0300

    fix(ss/coach/practitioner): fix timeline and orange ratings rules
    EC-544

--- src/lxp/src/pages/practitioner/practitioner-profile/practitioner-journey/timeline/steps/re-accreditation/step-accordion-content.tsx
+++ src/lxp/src/pages/practitioner/practitioner-profile/practitioner-journey/timeline/steps/re-accreditation/step-accordion-content.tsx
@@ -8,7 +8,10 @@ import {
   maxNumberOfVisits,
   visitTypes,
 } from '@/pages/coach/coach-practitioner-journey/coach-practitioner-journey.types';
-import { getRatingData } from '@/pages/coach/coach-practitioner-journey/timeline/utils';
+import {
+  getRatingData,
+  sortVisits,
+} from '@/pages/coach/coach-practitioner-journey/timeline/utils';
 import { ViewEvent } from '../../timeline-steps';
 import { chunkArray } from '@ecdlink/core';
 
@@ -29,10 +32,6 @@ export const ReAccreditationVisits = ({
     getPractitionerTimelineByIdSelector(practitionerId)
   );
 
-  const attendedReAccreditationVisits = reAccreditationVisits?.filter(
-    (item) => !!item?.attended
-  );
-
   // All years
   const filteredReAccreditationRatings =
     timeline?.reAccreditationRatings?.filter(
@@ -51,20 +50,7 @@ export const ReAccreditationVisits = ({
   const rating2 = reAccreditationRatingsFromCurrentYear?.[1];
   const rating3 = reAccreditationRatingsFromCurrentYear?.[2];
 
-  const sortedVisits = attendedReAccreditationVisits?.sort((a, b) => {
-    if (!a?.actualVisitDate && !b?.actualVisitDate) {
-      return 0;
-    } else if (!a?.actualVisitDate) {
-      return 1;
-    } else if (!b?.actualVisitDate) {
-      return -1;
-    }
-
-    return (
-      new Date(a.actualVisitDate).getTime() -
-      new Date(b.actualVisitDate).getTime()
-    );
-  });
+  const sortedVisits = sortVisits(reAccreditationVisits);
 
   const getVisitRating = (item: Maybe<Visit>) => {
     if (item?.id === rating3?.visitId) {
@@ -133,11 +119,10 @@ export const ReAccreditationVisits = ({
             type="body"
             color={getStepType(String('Success'))?.color || 'textMid'}
             text={
-              !!item?.actualVisitDate
-                ? new Date(item.actualVisitDate).toLocaleDateString(
-                    'en-ZA',
-                    dateOptions
-                  )
+              !!item?.plannedVisitDate
+                ? new Date(
+                    item.attended ? item.actualVisitDate : item.plannedVisitDate
+                  ).toLocaleDateString('en-ZA', dateOptions)
                 : ''
             }
           />
