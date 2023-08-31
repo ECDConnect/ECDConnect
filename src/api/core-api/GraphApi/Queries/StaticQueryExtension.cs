72c3604ed2593914cd94e27e83f76217f3d5a85e
Ruald de Bruyn <ruald@appy.co.za>
2023-08-31 13:48:06 +0200

    fix(admin-portal): document user name search

--- src/api/core-api/GraphApi/Queries/StaticQueryExtension.cs
+++ src/api/core-api/GraphApi/Queries/StaticQueryExtension.cs
@@ -32,7 +32,8 @@ namespace EcdLink.Api.CoreApi.GraphApi.Queries
             IGenericRepositoryFactory repoFactory,
             string userId,
             string[] showOnlyTypes,
-            PagedQueryInput pagingInput)
+            string search = null,
+            PagedQueryInput pagingInput = null)
         {
             var uId = contextAccessor.HttpContext.GetUser().Id;
             var docRepo = repoFactory.CreateRepository<Document>(userContext: uId);
@@ -49,6 +50,12 @@ namespace EcdLink.Api.CoreApi.GraphApi.Queries
             if (pagingInput?.FilterBy is not null)
             {
                 docsQuery = PaginationHelper.AddFiltering(pagingInput?.FilterBy, docsQuery);
+            }
+
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                docsQuery = docsQuery.Where(x => EF.Functions.ILike(x.User.FirstName, search) || EF.Functions.ILike(x.User.Surname, search)
+                 || EF.Functions.ILike(x.Name, search));
             }
 
             return docsQuery;
