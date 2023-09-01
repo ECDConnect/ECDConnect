a1432ae8ed505d333d832aca03ee9c73963f13b1
Liana Wiehahn <liana@ecdconnect.co.za>
2023-09-01 08:51:12 +0200

    fix(class name): fix class name
    EC-1365

--- src/api/Libraries/Modules/ECDLink.DataAccessLayer/Entities/Users/Mapping/PractitionerTimeline.cs
+++ src/api/Libraries/Modules/ECDLink.DataAccessLayer/Entities/Users/Mapping/PractitionerTimeline.cs
@@ -64,7 +64,7 @@ namespace ECDLink.DataAccessLayer.Entities.Users.Mapping
         public virtual PQARating ReAccreditationRating3 { get; set; }
     }
 
-    public class PractitionerCoachCircle
+    public class PractitionerAttendance
     {
         public int TotalMeetings { get; set; } //X = the number of coaching circles logged by coach for the practitioner's club in the current year
         public int TotalPresent { get; set; }// Y = the total number of presents & absents logged for the practitioner for coaching circles
