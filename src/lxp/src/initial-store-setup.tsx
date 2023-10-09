4f5cf16ec6ebaacbfa9c613eb633e3e06103c0da
lubezerra <lucilene@ecdconnect.co.za>
2023-10-09 09:59:26 -0300

    chore(ss): remove console

--- src/lxp/src/initial-store-setup.tsx
+++ src/lxp/src/initial-store-setup.tsx
@@ -284,7 +284,6 @@ const InitialStoreSetup: React.FC = ({ children }) => {
     await appDispatch(
       practitionerThunkActions.getAllPractitioners({})
     ).unwrap();
-    console.log('loggin in initAdditionalStoreSetup');
     await appDispatch(childrenThunkActions.getChildren({})).unwrap();
     await appDispatch(caregiverThunkActions.getCaregivers({})).unwrap();
     await appDispatch(documentThunkActions.getDocuments({})).unwrap();
