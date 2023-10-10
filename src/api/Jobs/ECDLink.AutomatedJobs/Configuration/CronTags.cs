036b34c87a455b4646ff1c0a2f70545b59cd12dc
Tiaan <tiaan@appy.co.za>
2023-10-10 17:46:31 +0200

    fix(crontags): cron timing update to avoid issue

--- src/api/Jobs/ECDLink.AutomatedJobs/Configuration/CronTags.cs
+++ src/api/Jobs/ECDLink.AutomatedJobs/Configuration/CronTags.cs
@@ -20,7 +20,7 @@
 
         public const string EveryTwoHours = "0 * */2 * * *";
         public const string EighthOfEveryMonth = "0 0 0 8 * *";
-        public const string FirstOfEveryMonth = "0 0 * 1 * *";
+        public const string FirstOfEveryMonth = "0 0 0 1 * *";
         public const string FirstDayofEveryYear = "0 0 0 1 1 *";
     }
 }
