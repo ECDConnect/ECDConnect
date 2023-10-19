62abac8bdfe398b5c182e099e988c588ad7bd19f
Derren Page <derren@meraki-it.co.za>
2023-10-19 16:48:29 +0200

    feat(ec-1865): log graphql calls and error popup

--- src/lxp/src/services/axios.helper.ts
+++ src/lxp/src/services/axios.helper.ts
@@ -1,5 +1,5 @@
-import { APIs } from '@ecdlink/core';
-import type { AxiosInstance } from 'axios';
+import { APIs, Config } from '@ecdlink/core';
+import type { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 import { store } from '@store';
@@ -50,6 +50,65 @@ export const api = (baseUrl: string, token?: string): AxiosInstance => {
       return config;
     },
     (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  axiosInstance.interceptors.response.use(
+    (response: AxiosResponse<any, any>) => {
+      if (response.config.baseURL === Config.graphQlApi) {
+        // checked for internal exception.
+        if (response.data.errors === undefined) {
+          console.log(
+            `GRAPHQL: ${response.request.statusText}[${response.request.status}] `,
+            {
+              query: response.config.data,
+              result: response.data,
+            }
+          );
+        } else {
+          (response as any).original_status = response.status;
+          (response as any).original_statusText = response.statusText;
+          response.status = 500;
+          response.statusText =
+            'Internal Server Error' +
+            (response.data.errors?.length > 0 &&
+            response.data.errors[0] &&
+            response.data.errors[0].message
+              ? ': ' + response.data.errors[0].message
+              : '');
+          console.error(
+            `GRAPHQL: ${response.statusText}[${response.status}] `,
+            {
+              query: response.config.data,
+              result: response.data,
+            }
+          );
+        }
+        if (response.status >= 400) {
+          // temporary alert message - to be replaced with nicer UI.
+          alert(
+            'Error communicating with the server.\nSee the browser console for more details.'
+          );
+        }
+        return response;
+      }
+    },
+    (error: AxiosError) => {
+      if (error.config.baseURL === Config.graphQlApi) {
+        console.error(
+          `GRAPHQL: ${error.request.statusText}[${error.request.status}] `,
+          {
+            query: error.config.data,
+            result: error.response?.data,
+          }
+        );
+        // temporary alert message - to be replaced with nicer UI.
+        alert(
+          'Error communicating with the server.\nSee the browser console for more details.'
+        );
+      }
+
       return Promise.reject(error);
     }
   );
