7db05838edea126b4de50c4e1770ce5139a660db
Amanda van Heerden <amanda@meraki-it.co.za>
2023-10-26 14:45:26 +0200

    fix(ec-1881): init static data

--- src/lxp/src/pages/dashboard/dashboard.tsx
+++ src/lxp/src/pages/dashboard/dashboard.tsx
@@ -338,6 +338,7 @@ export const Dashboard: React.FC = () => {
   };
 
   useEffect(() => {
+    initStaticStoreSetup();
     if (
       dashboardNotification?.isNew &&
       practitioner?.progress! >= 2 &&
