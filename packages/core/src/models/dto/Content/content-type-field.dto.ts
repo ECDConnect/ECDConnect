dd50f97fce6d0c8be6ff8658c457b4566a4ff64b
Derren Page <derren@meraki-it.co.za>
2023-11-01 22:46:52 +0200

    fix(ec-922): more dto missing fields

--- packages/core/src/models/dto/Content/content-type-field.dto.ts
+++ packages/core/src/models/dto/Content/content-type-field.dto.ts
@@ -2,11 +2,13 @@ import { EntityBase } from '../entity-base';
 import { FieldTypeDto } from './field-type.dto';
 
 export interface ContentTypeFieldDto extends EntityBase {
-  fieldOrder: number;
-  fieldName: string;
-  fieldTypeId: string;
-  fieldType: FieldTypeDto;
-  isActive: boolean;
   dataLinkName: string;
   displayMainTable: boolean;
+  displayName: string;
+  displayPage: number;
+  fieldName: string;
+  fieldOrder: number;
+  fieldType: FieldTypeDto;
+  fieldTypeId: string;
+  isActive: boolean;
 }
