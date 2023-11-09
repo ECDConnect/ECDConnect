96e0bb3afdf94101b41fa33baaaf02375114d3ba
Derren Page <derren@meraki-it.co.za>
2023-11-09 21:18:04 +0200

    feat(cron): cron appsettings restructure

--- src/api/Jobs/ECDLink.AutomatedJobs/Startup.cs
+++ src/api/Jobs/ECDLink.AutomatedJobs/Startup.cs
@@ -5,6 +5,7 @@ using ECDLink.Core.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Linq;
 
 namespace ECDLink.AutomatedJobs
 {
@@ -18,13 +19,22 @@ namespace ECDLink.AutomatedJobs
                 Console.WriteLine("CronJobs: Disabled");
                 return;
             }
-            Console.WriteLine("CronJobs: Enabled [{0}]", automatedJobsSection.Jobs.Count);
 
-            foreach (var job in automatedJobsSection.Jobs)
+            var jobNames = config.GetSection(AutomatedJobsSection.JobsName).AsEnumerable()
+                .Where(x => x.Key.StartsWith(AutomatedJobsSection.JobNamePrefix) && x.Value == null)
+                .Select(x => x.Key)
+                .ToList();
+
+            Console.WriteLine("CronJobs: Enabled [{0}]", jobNames.Count);
+            foreach (var jobName in jobNames)
             {
+                var job = config.GetSection<AutomatedJobsSection.Job>(jobName);
+                job.Name = jobName.Substring((AutomatedJobsSection.JobNamePrefix).Length);
                 if (job.Enabled == 0) continue;
+                if (automatedJobsSection.Enabled == 2) job.Enabled = 2;
+
                 Type jobType = Type.GetType(job.Type, false);
-                if (jobType == null) 
+                if (jobType == null)
                 {
                     Console.WriteLine("CronJobs: {0} NOT Registered.  Unknown type {1}", job.Name, job.Type);
                     continue;
