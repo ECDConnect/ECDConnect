10f71a4ee1d4d318198245e4177e05ea623ea175
Liana Wiehahn <liana@ecdconnect.co.za>
2023-11-14 15:29:03 +0200

    fix(import): remove import not in use

--- src/api/core-api/Services/ClubService.cs
+++ src/api/core-api/Services/ClubService.cs
@@ -27,7 +27,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 
