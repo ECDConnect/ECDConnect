f675bc758c90d674a26b68199d74643ffb193c34
Amanda van Heerden <amanda@meraki-it.co.za>
2023-12-04 19:31:35 +0200

    fix(ec-??): fix missing prs into qa

--- src/lxp/src/pages/classroom/attendance/components/attendance-list/attendance-list.tsx
+++ src/lxp/src/pages/classroom/attendance/components/attendance-list/attendance-list.tsx
@@ -407,7 +407,10 @@ export const AttendanceList: React.FC<AttendanceListProps> = ({
                   selectedGroup.id ===
                   primaryClassProgramme[0]?.classroomGroupId;
                 return (
-                  <div id={`attendanceList${selectedGroup.id}`}>
+                  <div
+                    key={`attencance_list_${idx}`}
+                    id={`attendanceList${selectedGroup.id}`}
+                  >
                     <ClassProgrammeAttendanceList
                       key={`class_attencance_list_${idx}`}
                       isPrimaryClass={isPrimaryList}
--- src/lxp/src/pages/classroom/attendance/components/attendance-wrapper/AttendanceWrapper.tsx
+++ src/lxp/src/pages/classroom/attendance/components/attendance-wrapper/AttendanceWrapper.tsx
@@ -15,7 +15,7 @@ import {
 } from '@/store/practitioner';
 import { useAppDispatch } from '@/store';
 
-export default function MultiRouteWrapper() {
+export default function AttendanceWrapper() {
   const history = useHistory();
   const appDispatch = useAppDispatch();
   const practitioner = useSelector(practitionerSelectors.getPractitioner);
@@ -93,7 +93,6 @@ export default function MultiRouteWrapper() {
                   type="filled"
                   color="primary"
                   className={'w-6/12'}
-                  // icon={'SaveIcon'}
                   onClick={() => {}}
                 >
                   {/* {renderIcon('XIcon', `w-5 h-5 text-white mr-2`)} */}
