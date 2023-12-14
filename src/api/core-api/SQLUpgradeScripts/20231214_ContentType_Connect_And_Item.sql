1f9c8afee9c39784a9dddec4562efdb75d53f81d
Liana Wiehahn <liana@ecdconnect.co.za>
2023-12-14 09:55:48 +0200

    fix(connect): fix description for content type

--- src/api/core-api/SQLUpgradeScripts/20231214_ContentType_Connect_And_Item.sql
+++ src/api/core-api/SQLUpgradeScripts/20231214_ContentType_Connect_And_Item.sql
@@ -10,7 +10,7 @@ VALUES(1, 'name', 1, true, 1, 27, '0001-01-01 00:00:00.000', '0001-01-01 00:00:0
 -- ConnectItem
 INSERT INTO public."ContentType"
 ("Id", "Name", "Description", "IsActive", "InsertedDate", "UpdatedDate", "UpdatedBy", "TenantId", "IsVisiblePortal", "PortalDisplayOrder")
-VALUES(28, 'ConnectItem', 'Connect ', true, '0001-01-01 00:00:00.000', '0001-01-01 00:00:00.000', null, null, true, -1);
+VALUES(28, 'ConnectItem', 'Connect Item', true, '0001-01-01 00:00:00.000', '0001-01-01 00:00:00.000', null, null, true, -1);
 
 INSERT INTO public."ContentTypeField"
 ("FieldOrder", "FieldName", "FieldTypeId", "IsActive", "DataLinkName", "ContentTypeId", "InsertedDate", "UpdatedDate", "UpdatedBy", "TenantId")
