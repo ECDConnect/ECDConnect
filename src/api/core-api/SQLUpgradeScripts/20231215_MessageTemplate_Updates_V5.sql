7462cc5fd8f1c6d064323d0e204cb862596c7761
Tiaan Beyers <tiaan@ecdconnect.co.za>
2023-12-19 17:58:00 +0200

    fix(notifications): improvements add action field for urls

--- src/api/core-api/SQLUpgradeScripts/20231215_MessageTemplate_Updates_V5.sql
+++ src/api/core-api/SQLUpgradeScripts/20231215_MessageTemplate_Updates_V5.sql
@@ -1,2 +1,5 @@
 ALTER TABLE public."MessageTemplate" ADD "Action" text NULL;
 ALTER TABLE public."MessageLog" ADD "Action" text NULL;
+
+
+update "MessageTemplate" set "Action" = '{"url":"/coach/practitioners"}'  where  "TemplateType" =  'coach-new-practitioners-linked';
