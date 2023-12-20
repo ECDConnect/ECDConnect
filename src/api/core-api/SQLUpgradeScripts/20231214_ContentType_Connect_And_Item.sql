c59d9c8bea4286e6cd13775230b19cc98de46152
Liana Wiehahn <liana@ecdconnect.co.za>
2023-12-20 10:41:29 +0200

    fix(cms): fix insert scripts for qa and prod

--- src/api/core-api/SQLUpgradeScripts/20231214_ContentType_Connect_And_Item.sql
+++ src/api/core-api/SQLUpgradeScripts/20231214_ContentType_Connect_And_Item.sql
@@ -17,13 +17,13 @@ VALUES(28, 'ConnectItem', 'Connect Item', true, '0001-01-01 00:00:00.000', '0001
 
 INSERT INTO public."ContentTypeField"
 ("Id","FieldOrder", "FieldName", "FieldTypeId", "IsActive", "DataLinkName", "ContentTypeId", "InsertedDate", "UpdatedDate", "UpdatedBy", "TenantId")
-VALUES(1, 'buttonText', 1, true, '', 28, '0001-01-01 00:00:00.000', '0001-01-01 00:00:00.000', null, null);
+VALUES(385, 1, 'buttonText', 1, true, '', 28, '0001-01-01 00:00:00.000', '0001-01-01 00:00:00.000', null, null);
 INSERT INTO public."ContentTypeField"
 ("Id","FieldOrder", "FieldName", "FieldTypeId", "IsActive", "DataLinkName", "ContentTypeId", "InsertedDate", "UpdatedDate", "UpdatedBy", "TenantId")
-VALUES(2, 'link', 1, true, '', 28, '0001-01-01 00:00:00.000', '0001-01-01 00:00:00.000', null, null);
+VALUES(386, 2, 'link', 1, true, '', 28, '0001-01-01 00:00:00.000', '0001-01-01 00:00:00.000', null, null);
 INSERT INTO public."ContentTypeField"
 ("Id","FieldOrder", "FieldName", "FieldTypeId", "IsActive", "DataLinkName", "ContentTypeId", "InsertedDate", "UpdatedDate", "UpdatedBy", "TenantId")
-VALUES(3, 'linkedConnect', 4, true, 'Connect', 28, '0001-01-01 00:00:00.000', '0001-01-01 00:00:00.000', null, null);
+VALUES(387, 3, 'linkedConnect', 4, true, 'Connect', 28, '0001-01-01 00:00:00.000', '0001-01-01 00:00:00.000', null, null);
 
 
 update public."ContentType" set "IsActive" = false where "Id" in (21,22,23,24);
