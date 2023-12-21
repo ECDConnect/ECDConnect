60137bda89ce7a0d19e0cfe1034293b5f598c168
Tiaan Beyers <tiaan@ecdconnect.co.za>
2023-12-21 17:15:43 +0200

    fix(leagues): leagues pull from sl

--- src/api/Libraries/Modules/ECDLink.Core/Services/Interfaces/IIntegrationService.cs
+++ src/api/Libraries/Modules/ECDLink.Core/Services/Interfaces/IIntegrationService.cs
@@ -17,5 +17,6 @@ namespace ECDLink.Core.Services.Interfaces
         Task<bool> IntegrationByTrainees();
         Task AutoSubmitStatements();
         Task<bool> IntegrationByNewCoach(string remoteCoachId);
+        Task IntegrationLeagueData();
     }
 }
