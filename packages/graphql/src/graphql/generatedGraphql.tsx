21264aa56e174028bae0f83deabbd42de84bfab7
Liana Wiehahn <liana@ecdconnect.co.za>
2024-01-04 08:36:05 +0200

    fix(coach business): apply fixes to coach business view for practitioner

--- packages/graphql/src/graphql/generatedGraphql.tsx
+++ packages/graphql/src/graphql/generatedGraphql.tsx
@@ -714,23 +714,6 @@ export type BulkInvitationResult = {
   success?: Maybe<Array<Maybe<Scalars['String']>>>;
 };
 
-export type CmsConnectItemModelInput = {
-  buttonText?: InputMaybe<Scalars['String']>;
-  contentId: Scalars['Int'];
-  contentTypeId: Scalars['Int'];
-  link?: InputMaybe<Scalars['String']>;
-  linkedConnect: Scalars['Int'];
-};
-
-export type CmsConnectModelInput = {
-  contentId: Scalars['Int'];
-  contentTypeId: Scalars['Int'];
-  hint?: InputMaybe<Scalars['String']>;
-  links?: InputMaybe<Array<InputMaybe<CmsConnectItemModelInput>>>;
-  name?: InputMaybe<Scalars['String']>;
-  type?: InputMaybe<Scalars['String']>;
-};
-
 export type CmsQuestionInput = {
   answer?: InputMaybe<Scalars['String']>;
   question?: InputMaybe<Scalars['String']>;
@@ -2903,16 +2886,12 @@ export type ComparableNullableOfInt32OperationFilterInput = {
 
 export type Connect = {
   __typename?: 'Connect';
-  hint?: Maybe<Scalars['String']>;
   id?: Maybe<Scalars['Int']>;
   name?: Maybe<Scalars['String']>;
-  type?: Maybe<Scalars['String']>;
 };
 
 export type ConnectInput = {
-  hint?: InputMaybe<Scalars['String']>;
   name?: InputMaybe<Scalars['String']>;
-  type?: InputMaybe<Scalars['String']>;
 };
 
 export type ConnectItem = {
@@ -6032,7 +6011,6 @@ export type Mutation = {
   updateCommunitySupport?: Maybe<Trainee>;
   updateConnect?: Maybe<Connect>;
   updateConnectItem?: Maybe<ConnectItem>;
-  updateConnectSection: Scalars['Boolean'];
   updateConsent?: Maybe<Consent>;
   updateDailyProgramme?: Maybe<DailyProgramme>;
   updateDocument?: Maybe<Document>;
@@ -7587,7 +7565,6 @@ export type MutationOpenAccessAddChildArgs = {
 };
 
 export type MutationPromotePractitionerToPrincipalArgs = {
-  sendComm?: Scalars['Boolean'];
   userId?: InputMaybe<Scalars['String']>;
 };
 
@@ -8314,11 +8291,6 @@ export type MutationUpdateConnectItemArgs = {
   id: Scalars['String'];
   input: ConnectItemInput;
   locale?: InputMaybe<Scalars['String']>;
-  localeId?: InputMaybe<Scalars['String']>;
-};
-
-export type MutationUpdateConnectSectionArgs = {
-  input?: InputMaybe<Array<InputMaybe<CmsConnectModelInput>>>;
   localeId?: InputMaybe<Scalars['String']>;
 };
 
