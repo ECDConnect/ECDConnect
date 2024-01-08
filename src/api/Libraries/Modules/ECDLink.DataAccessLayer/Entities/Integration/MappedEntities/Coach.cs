3aeac44f6a092ad31cffa656e97b887fffb8135a
Tiaan Beyers <tiaan@ecdconnect.co.za>
2024-01-08 12:41:45 +0200

    fix(coach): update to nullable property

--- src/api/Libraries/Modules/ECDLink.DataAccessLayer/Entities/Integration/MappedEntities/Coach.cs
+++ src/api/Libraries/Modules/ECDLink.DataAccessLayer/Entities/Integration/MappedEntities/Coach.cs
@@ -7,7 +7,7 @@ namespace ECDLink.DataAccessLayer.Entities.Integration.MappedEntities
         public string ContactNumber { get; set; }
         public string Gender { get; set; }
         public string AreaOfOperation { get; set; }
-        public bool WorksAtOffice { get; set; }
+        public bool? WorksAtOffice { get; set; }
         public string SecondaryAreaOfOperation { get; set; }
         public DateTime? StartDate { get; set; }
         public MappedFranchisor Franchisor { get; set; }
