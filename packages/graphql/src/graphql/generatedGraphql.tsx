e6c117bf05aed78af1d9230a4375a1c0a69c17b6
Tiaan Beyers <tiaan@ecdconnect.co.za>
2024-01-23 20:53:20 +0200

    feat(portal): add isrequired to contentdefinitions

--- packages/graphql/src/graphql/generatedGraphql.tsx
+++ packages/graphql/src/graphql/generatedGraphql.tsx
@@ -3017,6 +3017,7 @@ export type ContentTypeField = {
   id: Scalars['Int'];
   insertedDate: Scalars['DateTime'];
   isActive: Scalars['Boolean'];
+  isRequired: Scalars['Boolean'];
   updatedBy?: Maybe<Scalars['String']>;
   updatedDate: Scalars['DateTime'];
 };
@@ -3560,6 +3561,7 @@ export type FieldDefinitionModel = {
   displayPage: Scalars['Boolean'];
   fieldTypeId: Scalars['Int'];
   graphDataTypeName?: Maybe<Scalars['String']>;
+  isRequired: Scalars['Boolean'];
   name?: Maybe<Scalars['String']>;
 };
 
@@ -5912,6 +5914,7 @@ export type Mutation = {
   openAccessAddChild: Scalars['Boolean'];
   promotePractitionerToPrincipal?: Maybe<Principal>;
   pushPQAData: Scalars['Boolean'];
+  pushReAccreditationData: Scalars['Boolean'];
   pushSmartSpaceVisitsData: Scalars['Boolean'];
   reassignAbsenteeFromHistory: Scalars['Boolean'];
   reassignAbsentees: Scalars['Boolean'];
