09c0853db7469ee8c9e9e838fa1eebb6afbc8cc8
Ubiratan <ubiratan@ecdconnect.co.za>
2024-01-25 16:47:25 -0300

    fix(programme): fix spelling errors

--- src/admin-portal/src/app/constants/content-management.ts
+++ src/admin-portal/src/app/constants/content-management.ts
@@ -1,6 +1,6 @@
 export const ContentManagementTabs = {
   PROCESS: { id: 2, name: 'Progress' },
-  PROGRAMMES: { id: 3, name: 'Programmes' },
+  PROGRAMMES: { id: 3, name: 'Programme' },
   COMMUNITY: { id: 4, name: 'Community' },
 };
 
