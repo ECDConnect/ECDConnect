cfc8ace45e5818b379247b985c51083862114794
lubezerra <lucilene@ecdconnect.co.za>
2024-01-30 09:40:07 -0300

    refactor(ss/clubs): rename page
    EC-2130

--- src/lxp/src/routes/routes.ts
+++ src/lxp/src/routes/routes.ts
@@ -62,7 +62,7 @@ const ROUTES = {
       POINTS: addPrefix('/points', {
         MEET_REGULARLY: addPrefix('/meet-regularly', {
           MEETING_DETAILS: '/:meetingId/meeting-details',
-          PLAYGROUP_REASSIGNED: '/playgroup-reassigned',
+          MISSING_MEETING_REGISTERS: '/missing-meeting-registers',
         }),
         BE_CREATIVE: '/be-creative',
         HOST_FAMILY_EVENT: '/host-family-event',
