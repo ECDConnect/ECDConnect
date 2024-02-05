ce41925d7ecd50cfeb9396b289945d427baf36e1
Tiaan Beyers <tiaan@ecdconnect.co.za>
2024-02-05 20:26:09 +0200

    fix(notifications): notification updates

--- src/api/core-api/SQLUpgradeScripts/20240205_MessageTemplate_Updates_V8.sql
+++ src/api/core-api/SQLUpgradeScripts/20240205_MessageTemplate_Updates_V8.sql
@@ -2,4 +2,13 @@ update "MessageTemplate" set "Action" = '{"url":"/principal/contact-practitioner
 update "MessageTemplate" set "Action" = '{"url":"/principal/contact-practitioner"}' where "TemplateType"  = 'marked-onleave'; 
 update "MessageTemplate" set "Action" = '{"url":"/practitioner/profile/edit"}' where "TemplateType"  = 'coach-visit-requested'; 
 update "MessageTemplate" set "Action" = '{"url":"/principal/contact-practitioner"}' where "TemplateType"  = 'fillin-self-asessment-form'; 
-update "MessageLog" set "Action" = '{"url":"/practitioner/profile/edit"}' where "MessageTemplateType" = 'fillin-self-asessment-form';
+
+update "MessageTemplate" set "Action" = '{"url":"/practitioner/profile/playgroups"}' where "TemplateType"  = 'practitioner-removed-from-programme';
+update "MessageTemplate" set "Action" = '{"url":"/practitioner/profile/playgroups"}' where "TemplateType"  = 'unassigned-classes';
+
+update "MessageLog" set "Action" = '{"url":"/practitioner/profile/playgroups"}' where "MessageTemplateType" = 'practitioner-removed-from-programme';
+update "MessageLog" set "Action" = '{"url":"/practitioner/profile/playgroups"}' where "MessageTemplateType" = 'unassigned-classes';
+
+	INSERT INTO "MessageTemplate" ("Id","IsActive","InsertedDate","UpdatedDate","UpdatedBy","Protocol","TemplateType","Message","TenantId","Subject","CTA","CTAText","TypeCode","NotificationColor","Ordering","Action") VALUES
+	 (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),true,NOW(),NOW(),NULL,'push','unassigned-classes','Assign a practitioner to [[ClassName]] as soon as possible.','258a15e6-3736-45ea-875c-48d9377de4c8','The [[ClassName]] class does not have a practitioner assigned','[[AssignPractitioner]]','Assign practitioner',NULL,NULL,3,'{"url":"/practitioner/profile/playgroups"}');
+
