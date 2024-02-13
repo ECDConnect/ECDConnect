ca9fc5a4a58d182b1f50e771341007678cf01b6e
Tiaan Beyers <tiaan@ecdconnect.co.za>
2024-02-13 21:35:44 +0200

    fix(notifications): fix coach trainee notifications

--- src/api/core-api/SQLUpgradeScripts/20240213_MessageTemplate_Updates_V11.sql
+++ src/api/core-api/SQLUpgradeScripts/20240213_MessageTemplate_Updates_V11.sql
@@ -3,3 +3,6 @@ delete from "MessageTemplate" where "TemplateType"  in ('fillin-self-asessment-f
 
 update "MessageLog" set "Action" = '{"url":"/practitioner/profile}' where  "MessageTemplateType"  in ('fillin-self-asessment-form', 'coach-fillin-self-asessment-form') and "MessageProtocol"  = 'push';
 update "MessageTemplate" set "Action" = '{"url":"/practitioner/profile}' where  "TemplateType"  in ('fillin-self-asessment-form', 'coach-fillin-self-asessment-form') and "Protocol"  = 'push';
+
+update "MessageLog" set "Action" = '{"url":"/coach/practitioners","state":{"filter":"New trainee"}}' where  "MessageTemplateType"  in ('coach-new-trainees');
+update "MessageTemplate" set "Action" = '{"url":"/coach/practitioners","state":{"filter":"New trainee"}}' where  "TemplateType"  in ('coach-new-trainees') ;
