a7bfc7a287b552431651c0f9717c2c60a7c1b8e0
Tiaan Beyers <tiaan@ecdconnect.co.za>
2024-02-17 17:54:19 +0200

    fix(notifications): generic notifications fixes

--- src/api/core-api/SQLUpgradeScripts/20240213_MessageTemplate_Updates_V12.sql
+++ src/api/core-api/SQLUpgradeScripts/20240213_MessageTemplate_Updates_V12.sql
@@ -22,3 +22,12 @@ INSERT INTO "MessageTemplate" ("Id","IsActive","InsertedDate","UpdatedDate","Upd
 INSERT INTO "MessageTemplate" ("Id","IsActive","InsertedDate","UpdatedDate","UpdatedBy","Protocol","TemplateType","Message","TenantId","Subject","CTA","CTAText","TypeCode","NotificationColor","Ordering","Action") VALUES
 	 (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),true,NOW(),NOW(),NULL,'push','reassigned-to-new-class','You were assigned to [[ClassName]]. Reach out to [[PrincipalName]] if you have any questions or view your new class.','258a15e6-3736-45ea-875c-48d9377de4c8','You have been assigned to a new class: [[ClassName]]','[[SeeNewClass]]','See new class',NULL,NULL,14,NULL),
 	 (uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),true,NOW(),NOW(),NULL,'push','reassigned-to-new-class-from-old','You were assigned to [[ClassName]] and removed from [[OldClassName]]. Reach out to [[PrincipalName]] if you have any questions or view your new class.','258a15e6-3736-45ea-875c-48d9377de4c8','You have been assigned to a new class: [[ClassName]]','[[SeeNewClass]]','See new class',NULL,NULL,14,NULL);
+
+
+	 UPDATE public."MessageLog"
+	SET "Action"='{"url":"/classroom","state":{"activeTabIndex:":"1"}}'
+	WHERE "MessageTemplateType"  in ('reassigned-to-new-class', 'reassigned-to-new-class-from-old');
+
+UPDATE public."MessageTemplate"
+	SET "Action"='{"url":"/classroom","state":{"activeTabIndex:":"1"}}'
+	WHERE "TemplateType" in ('reassigned-to-new-class', 'reassigned-to-new-class-from-old');
