bcef42b939880142160e34e27fe842d49c62b3bf
Tiaan Beyers <tiaan@ecdconnect.co.za>
2024-02-18 05:03:40 +0200

    fix(notifications): generic notifications fixes

--- src/api/core-api/Services/Notifications/NotificationTasksService.cs
+++ src/api/core-api/Services/Notifications/NotificationTasksService.cs
@@ -213,8 +213,8 @@ namespace EcdLink.Api.CoreApi.Services
             replacements.Add(new TagsReplacements()
             {
                 FindValue = "StatementCutOffDate",
-                ReplacementValue = DateTime.Now.GetStartOfMonth().AddDays(7).ToString()
-            });
+                ReplacementValue = DateTime.Now.GetStartOfMonth().AddDays(7).ToShortDateString()
+            }) ;
 
             foreach (var pracData in pracsDueSubmits)
             {
@@ -233,11 +233,11 @@ namespace EcdLink.Api.CoreApi.Services
                 }
                 replacements.Add(new TagsReplacements()
                 {
-                    FindValue = "StatementCutOffDate",
+                    FindValue = "IsStipendReceiverText",
                     ReplacementValue = isStipendReceiver ? stipendText : ""
                 });
 
-                await _notificationService.SendNotificationAsync(null, TemplateTypeConstants.IncomeStatementIncompleteBy1st, DateTime.Now, userToSend, "", MessageStatusConstants.Amber, replacements, DateTime.Now.AddDays(7));
+                await _notificationService.SendNotificationAsync(null, TemplateTypeConstants.IncomeStatementIncompleteBy1st, DateTime.Now, userToSend, "", MessageStatusConstants.Red, replacements, DateTime.Now.AddDays(6),false,true);
             }
         }
 
@@ -407,6 +407,7 @@ namespace EcdLink.Api.CoreApi.Services
 
         public async Task DailyAttendanceNotTrackedNotification()
         {
+            try { 
             var adminId = _hierarchyEngine.GetAdminUserId();
             var classroomGroupRepo = _repositoryFactory.CreateGenericRepository<ClassroomGroup>(userContext: adminId);
             var learnerRepo = _repositoryFactory.CreateGenericRepository<Learner>(userContext: adminId);
@@ -455,8 +456,12 @@ namespace EcdLink.Api.CoreApi.Services
                         }
                     }
                 }
-            //}
             }
+            catch (Exception ex)
+            {
+                _logger.LogError("Issue with attendance tracking in DailyAttendanceNotTrackedNotification" + ex.Message, ex);
+            }
+        }
 
         public async Task WeeklyCoachTraineesCheckReminderAsync()
         {
@@ -600,7 +605,7 @@ namespace EcdLink.Api.CoreApi.Services
                     }
                     catch (Exception ex)
                     {
-                        _logger.LogError(ex, "Issue with trainee timeline in DailyCoachChecksNotification", ex.Message);
+                        _logger.LogError("Issue with trainee timeline in DailyCoachChecksNotification" + ex.Message, ex);
                     }
                 }
                 var practitioners = practitionerRepo.GetAll().Where(x => x.IsActive.Equals(true) && x.IsTrainee == false && x.IsRegistered == true && (x.CoachHierarchy.HasValue && x.CoachHierarchy == coach.UserId)).ToList();
@@ -657,7 +662,7 @@ namespace EcdLink.Api.CoreApi.Services
                     }
                     catch (Exception ex)
                     {
-                        _logger.LogError(ex, "Issue with practitioner timeline in DailyCoachChecksNotification", ex.Message);
+                        _logger.LogError("Issue with practitioner timeline in DailyCoachChecksNotification " + ex.Message, ex);
                     }
                 }
 
