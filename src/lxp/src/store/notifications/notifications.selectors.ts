a153e8557d2c2c07b2c5feee5fec21717f19f25e
CameronSTaljaard <cameronstaljaard@gmail.com>
2024-02-20 10:49:55 +0200

    logic change

--- src/lxp/src/store/notifications/notifications.selectors.ts
+++ src/lxp/src/store/notifications/notifications.selectors.ts
@@ -20,9 +20,7 @@ export const getAllNotifications = createSelector(
       ).toISOString();
       const dateCreated = parseISO(ISOdateCreated.replace('Z', ''));
 
-      const isCtaOk =
-        ctasMapped.includes(notification.message.cta) ||
-        notification.message.cta;
+      const isCtaOk = ctasMapped.includes(notification.message.cta);
 
       if (!notification?.message?.expiryDate) {
         return dateCreated.getTime() <= currentDate.getTime() && isCtaOk;
