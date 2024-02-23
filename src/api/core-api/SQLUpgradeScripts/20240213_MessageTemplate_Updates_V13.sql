048a44f7c291f00e0cd5b853d825d566057b0e90
Tiaan Beyers <tiaan@ecdconnect.co.za>
2024-02-23 04:22:27 +0200

    fix(notifications): generic notifications fixes

--- src/api/core-api/SQLUpgradeScripts/20240213_MessageTemplate_Updates_V13.sql
+++ src/api/core-api/SQLUpgradeScripts/20240213_MessageTemplate_Updates_V13.sql
@@ -1,3 +1,5 @@
 UPDATE public."MessageLog"
 	SET "Action"='{"url":"/coach/practioner-remove","state":{"practitionerId":"[[PractitionerUserId]]"}}'
 	WHERE "MessageTemplateType"  in ('coach-remove-trainee');
+
+	ALTER TABLE public."MessageLog" ADD "RelatedToUserId" text NULL;
