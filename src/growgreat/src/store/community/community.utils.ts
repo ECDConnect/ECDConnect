9186b785ebd1d4b7635ff911629d1e74d3b48145
lubezerra <lucilene@ecdconnect.co.za>
2024-02-28 16:48:18 -0300

    chore(gg): remove trash

--- src/growgreat/src/store/community/community.utils.ts
+++
@@ -1,8 +0,0 @@
-import { ThunkStateStatus } from '../types';
-import { CommunityState } from './community.types';
-
-export const handleEarnPointsInfo = (
-  state: CommunityState & ThunkStateStatus,
-  tab: string,
-  locale: string
-) => {};
