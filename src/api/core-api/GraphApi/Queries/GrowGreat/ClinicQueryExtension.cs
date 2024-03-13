9ce42aa00955a523b4a6a557af6442e257b9fa73
Matthew de Kock <matthew@ecdconnect.co.za>
2024-03-13 14:29:47 +0000

    Merged in EC-2284_PregnantMotherVisitPoints (pull request #2974)
    EC-2284: Monthly job and logic for calculating up to date pregnant mother visits
    - Added job to trigger points calculation at the end of the month
    - Logic to check for no missed visits for maternal distress, malnutrition and alcohol abuse
    
    Approved-by: Liana Wiehahn

--- src/api/core-api/GraphApi/Queries/GrowGreat/ClinicQueryExtension.cs
+++ src/api/core-api/GraphApi/Queries/GrowGreat/ClinicQueryExtension.cs
@@ -16,8 +16,6 @@ using System;
 using System.Linq;
 using EcdLink.Api.CoreApi.GraphApi.Models;
 using System.Collections.Generic;
-using EcdLink.Api.CoreApi.GraphApi.Models.GrowGreat.Input;
-using EcdLink.Api.CoreApi.Services.PointsEngine.Interfaces;
 using EcdLink.Api.CoreApi.GraphApi.Models.GrowGreat.Portal;
 
 namespace EcdLink.Api.CoreApi.GraphApi.Queries.GrowGreat
