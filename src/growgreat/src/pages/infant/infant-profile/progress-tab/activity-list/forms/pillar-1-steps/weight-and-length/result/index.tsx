1e43a103478d2fc583092ab2c1b3e604930ef4f2
lubezerra <lucilene@ecdconnect.co.za>
2024-03-22 17:58:22 -0300

    chore(gg): remove snippet

--- src/growgreat/src/pages/infant/infant-profile/progress-tab/activity-list/forms/pillar-1-steps/weight-and-length/result/index.tsx
+++ src/growgreat/src/pages/infant/infant-profile/progress-tab/activity-list/forms/pillar-1-steps/weight-and-length/result/index.tsx
@@ -59,12 +59,10 @@ import {
   fillInMissingNumbers,
   findClosestWeight,
   findLastIndex,
-  getDataPerYear,
 } from './data/utils/utils';
 import { GrowthMonitoring } from '../..';
 import { Card } from './card';
 import { GrowthStatus, xAxisData } from './index.types';
-import { newData } from './visitGrowthDataDay';
 
 export const WeightAndLengthResultStep = ({
   infant,
@@ -72,53 +70,6 @@ export const WeightAndLengthResultStep = ({
   setGrowthMonitoring,
   setEnableButton,
 }: DynamicFormProps) => {
-  const girls = newData.VisitGrowthDataDay.filter(
-    (item) => item.Section === 'weight-for-age-girls'
-  );
-
-  const date = girls.map((item) => item.Day).sort((a, b) => a - b);
-  const median = girls.map((item) => item.Median).sort((a, b) => a - b);
-  const SD2 = girls.map((item) => item.SD2).sort((a, b) => a - b);
-  const SD2neg = girls.map((item) => item.SD2neg).sort((a, b) => a - b);
-  const SD3 = girls.map((item) => item.SD3).sort((a, b) => a - b);
-  const SD3neg = girls.map((item) => item.SD3neg).sort((a, b) => a - b);
-
-  const newObject = {
-    date,
-    median: {
-      label: 'median',
-      weight: getDataPerYear(median),
-    },
-    SD2: {
-      label: '2 SD',
-      weight: getDataPerYear(SD2),
-    },
-    SD3: {
-      label: '3 SD',
-      weight: getDataPerYear(SD3),
-    },
-    SD3neg: {
-      label: '-3 SD',
-      weight: getDataPerYear(SD3neg),
-    },
-    SD2neg: {
-      label: '-2 SD',
-      weight: getDataPerYear(SD2neg),
-    },
-  };
-
-  function downloadTextFile(text: any, name: string) {
-    const a = document.createElement('a');
-    const type = name.split('.').pop();
-    a.href = URL.createObjectURL(
-      new Blob([text], { type: `text/${type === 'txt' ? 'plain' : type}` })
-    );
-    a.download = name;
-    a.click();
-  }
-
-  // downloadTextFile(JSON.stringify(newObject), 'weight-for-age-girls.json')
-
   const [weightAxios, setWeightAxios] = useState<WeightOrHeightForAgeProps>();
   const [lengthAxios, setLengthAxios] = useState<WeightOrHeightForAgeProps>();
   const [weightResult, setWeightResult] = useState<(number | undefined)[]>([
