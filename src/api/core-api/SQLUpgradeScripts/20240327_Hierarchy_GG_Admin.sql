71f8360076b0a3ea266dd2c4888e8346d74e4dd7
Liana Wiehahn <liana@ecdconnect.co.za>
2024-03-27 09:51:12 +0200

    fix(hierarchy): add administrator hierarchy for gg

---
+++ src/api/core-api/SQLUpgradeScripts/20240327_Hierarchy_GG_Admin.sql
@@ -0,0 +1,3 @@
+INSERT INTO public."Hierarchy"
+("Id", "Type", "SystemType", "ParentId", "IsActive", "InsertedDate", "UpdatedDate", "UpdatedBy", "TenantId")
+VALUES('28527fc7-39a8-4a7d-b9f5-43570a070845', 'Administrator', 'ECDLink.DataAccessLayer.Entities.ApplicationUser', '00000000-0000-0000-0000-000000000000', true, current_timestamp, current_timestamp, null, '39077d0e-e443-4076-aaf2-978dc6805aa0');
