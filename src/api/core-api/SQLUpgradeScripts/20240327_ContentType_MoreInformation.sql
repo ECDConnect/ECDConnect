1b974b58aad53dca122362b981905739f0987fc6
Liana Wiehahn <liana@ecdconnect.co.za>
2024-03-27 13:45:19 +0200

    fix(more info): hide items from table and form

---
+++ src/api/core-api/SQLUpgradeScripts/20240327_ContentType_MoreInformation.sql
@@ -0,0 +1,3 @@
+update "ContentTypeField" set "DisplayMainTable" = false, "DisplayPage" = false where "ContentTypeId" =15 and "FieldName" ='type';
+update "ContentTypeField" set "DisplayPage" = false where "ContentTypeId" =15 and "FieldName" ='visit';
+update "ContentTypeField" set "DisplayPage" = false where "ContentTypeId" =15 and "FieldName" ='section';
