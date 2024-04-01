ebc9a4b213f9ef627c2c9b8962533976aa2f82ab
Ubiratan Rodrigues <ubiratanrodrigues@Ubiratans-MacBook-Pro.local>
2024-04-01 10:27:34 -0300

    fix(users): fix users date picker

--- src/admin-portal/src/app/components/date-picker/index.tsx
+++ src/admin-portal/src/app/components/date-picker/index.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import DateRangePickerCalendar from './DateRangePickerCalendar'; // Custom calendar component
-import { startOfMonth, endOfMonth, format, subDays } from 'date-fns';
+import { format } from 'date-fns';
 
 function CustomDateRangePicker(props: any) {
   const [showCalendar, setShowCalendar] = useState(false);
-
+  console.log({ props });
   const toggleCalendar = () => {
     setShowCalendar((prevShowCalendar) => !prevShowCalendar);
   };
@@ -19,18 +19,18 @@ function CustomDateRangePicker(props: any) {
         className="border-secondary hover:bg-secondary text-secondary flex flex-row rounded border-2 py-2 px-4 font-bold hover:text-white"
         onClick={toggleCalendar}
       >
-        {props.selectedRange?.length > 0 ? (
+        {props?.selectedRange?.length > 0 ? (
           <>
-            {formatDate(props.selectedRange[0])} -{' '}
-            {formatDate(props.selectedRange[1])}
+            {formatDate(props?.selectedRange?.[0])} -{' '}
+            {formatDate(props?.selectedRange?.[1])}
           </>
         ) : null}
       </button>
 
       {showCalendar && (
         <DateRangePickerCalendar
-          selectedRange={props.selectedRange}
-          onDateChange={props.handleDateChange}
+          selectedRange={props?.selectedRange}
+          onDateChange={props?.handleDateChange}
         />
       )}
     </div>
