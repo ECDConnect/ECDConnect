25675eded0a3a7e592c02b957e4209b32919c1d0
Liana Wiehahn <liana@ecdconnect.co.za>
2024-04-02 13:39:51 +0200

    fix(natal): hide more fields

---
+++ src/api/core-api/SQLUpgradeScripts/20240402_ContentType_Natat.sql
@@ -0,0 +1,19 @@
+update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =29 and "FieldName" ='title';
+update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =29 and "FieldName" ='section';
+update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =29 and "FieldName" ='type';
+
+update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =30 and "FieldName" ='title';
+update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =30 and "FieldName" ='section';
+update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =30 and "FieldName" ='type';
+
+update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =31 and "FieldName" ='title';
+update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =31 and "FieldName" ='section';
+update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =31 and "FieldName" ='type';
+
+update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =32 and "FieldName" ='title';
+update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =32 and "FieldName" ='section';
+update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =32 and "FieldName" ='type';
+
+update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =33 and "FieldName" ='title';
+update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =33 and "FieldName" ='section';
+update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =33 and "FieldName" ='type';
--- src/api/core-api/SQLUpgradeScripts/20240402_ContentType_Natatl.sql
+++
@@ -1,3 +0,0 @@
-update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =29 and "FieldName" ='title';
-update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =29 and "FieldName" ='section';
-update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =29 and "FieldName" ='type';
