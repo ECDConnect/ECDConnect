2a3061d2d9c9b36a6413efd7547eed7f1cb06b02
Liana Wiehahn <liana@ecdconnect.co.za>
2024-04-02 13:49:12 +0200

    fix(danger signs): hiding more items from display page

--- src/api/core-api/SQLUpgradeScripts/20240402_ContentType_Natal.sql
+++ src/api/core-api/SQLUpgradeScripts/20240402_ContentType_Natal.sql
@@ -17,3 +17,6 @@ update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "C
 update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =33 and "FieldName" ='title';
 update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =33 and "FieldName" ='section';
 update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =33 and "FieldName" ='type';
+
+update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =35 and "FieldName" ='section';
+update "ContentTypeField" set "DisplayPage" = false, "IsRequired"=false where "ContentTypeId" =35 and "FieldName" ='type';
