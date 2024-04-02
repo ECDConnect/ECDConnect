e52d42ce0c18803b3221a7aba3f0c61852f14ddf
Liana Wiehahn <liana@ecdconnect.co.za>
2024-04-02 13:30:00 +0200

    fix(natal): add items on form

---
+++ src/api/core-api/SQLUpgradeScripts/20240402_ContentType_Natatl.sql
@@ -0,0 +1,3 @@
+update "ContentTypeField" set "DisplayPage" = false where "ContentTypeId" =29 and "FieldName" ='title';
+update "ContentTypeField" set "DisplayPage" = false where "ContentTypeId" =29 and "FieldName" ='section';
+update "ContentTypeField" set "DisplayPage" = false where "ContentTypeId" =29 and "FieldName" ='type';
