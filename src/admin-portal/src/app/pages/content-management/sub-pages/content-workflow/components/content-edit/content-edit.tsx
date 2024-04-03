1f4dd783e2b3415c45808992e50430d957203edf
Ubiratan Rodrigues <ubiratanrodrigues@Ubiratans-MacBook-Pro.local>
2024-04-03 09:33:02 -0300

    chore(content): remove console.log

--- src/admin-portal/src/app/pages/content-management/sub-pages/content-workflow/components/content-edit/content-edit.tsx
+++ src/admin-portal/src/app/pages/content-management/sub-pages/content-workflow/components/content-edit/content-edit.tsx
@@ -453,7 +453,7 @@ export default function ContentEdit({
       setDangerSignsDisableInputs(true);
     }
   }, [defaultLanguageId, template?.fields, template?.title]);
-  console.log({ content });
+
   if (
     contentType &&
     contentValues &&
