9b3575385c1e065535524859498036781df76b97
Liana Wiehahn <liana@ecdconnect.co.za>
2024-05-30 16:29:05 +0200

    fix(w2): remove user id from update function

--- src/api/core-api/Security/Models/Requests/OAPractitionerModel.cs
+++ src/api/core-api/Security/Models/Requests/OAPractitionerModel.cs
@@ -4,7 +4,6 @@ namespace EcdLink.Api.CoreApi.Security.Models.Requests
     {
         // For Facebook save the id/number to Username
         // For Google save the email to Username
-        public string UserId { get; set; }
         public string Username { get; set; }
         public string PhoneNumber { get; set; }
         public string Password { get; set; }
