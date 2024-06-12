2a26e1a5abf32280daedd0719122da9bd7a72863
Derren <derren@meraki-it.co.za>
2024-06-12 10:14:25 +0200

    fix(??): iuserlockoutstore and iusertwofactorstore

--- src/api/Libraries/Modules/ECDLink.DataAccessLayer/Stores/ApplicationUserRoleStore.cs
+++ src/api/Libraries/Modules/ECDLink.DataAccessLayer/Stores/ApplicationUserRoleStore.cs
@@ -12,7 +12,9 @@ using System.Threading.Tasks;
 namespace ECDLink.DataAccessLayer.Stores
 {
     public class ApplicationUserRoleStore : IUserRoleStore<ApplicationUser>, IQueryableUserStore<ApplicationUser>, IUserPasswordStore<ApplicationUser>, IUserSecurityStampStore<ApplicationUser>,
-        IUserPhoneNumberStore<ApplicationUser>
+        IUserPhoneNumberStore<ApplicationUser>, IUserEmailStore<ApplicationUser>, IUserLockoutStore<ApplicationUser>, IUserTwoFactorStore<ApplicationUser>
+    // IUserClaimStore
+    // IUserLoginStore
     {
         private readonly AuthenticationDbContext _context;
         private readonly Guid _tenantId;
@@ -70,13 +72,13 @@ namespace ECDLink.DataAccessLayer.Stores
 
         public Task<ApplicationUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
         {
-            var user = _context.Users.Where(x => x.Id == Guid.Parse(userId)).SingleOrDefault();
+            var user = Users.Where(x => x.Id == Guid.Parse(userId)).SingleOrDefault();
             return Task.FromResult(user);
         }
 
         public Task<ApplicationUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
         {
-            var user = _context.Users.Where(x => x.NormalizedUserName == normalizedUserName || x.UserName == normalizedUserName).SingleOrDefault();
+            var user = Users.Where(x => x.NormalizedUserName == normalizedUserName || x.UserName == normalizedUserName).SingleOrDefault();
             return Task.FromResult(user);
         }
 
@@ -107,7 +109,7 @@ namespace ECDLink.DataAccessLayer.Stores
             var roleId = _context.Roles.Where(x => x.TenantId == _tenantId && (x.NormalizedName == roleName || x.Name == roleName)).Select(x => x.Id).SingleOrDefault();
             if (roleId == Guid.Empty) return Task.FromResult(new List<ApplicationUser>() as IList<ApplicationUser>);
             var userIds = _context.UserRoles.Where(x => x.RoleId == roleId).Select(x => x.UserId).ToList();
-            var users = _context.Users.Where(x => userIds.Contains(x.Id)).ToList();
+            var users = Users.Where(x => userIds.Contains(x.Id)).ToList();
             return Task.FromResult(users as IList<ApplicationUser>);
         }
 
@@ -230,5 +232,165 @@ namespace ECDLink.DataAccessLayer.Stores
             return Task.CompletedTask;
         }
 
+        // IUserEmailStore
+
+        public Task SetEmailAsync(ApplicationUser user, string email, CancellationToken cancellationToken)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            user.Email = email;
+            return Task.CompletedTask;
+        }
+
+        public Task<string> GetEmailAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            return Task.FromResult(user.Email);
+        }
+
+        public Task<bool> GetEmailConfirmedAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            return Task.FromResult(user.EmailConfirmed);
+        }
+
+        public Task SetEmailConfirmedAsync(ApplicationUser user, bool confirmed, CancellationToken cancellationToken)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            user.EmailConfirmed = confirmed;
+            return Task.CompletedTask;
+
+        }
+
+        public Task<ApplicationUser> FindByEmailAsync(string normalizedEmail, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrEmpty(normalizedEmail))
+            {
+                throw new ArgumentNullException(nameof(normalizedEmail));
+            }
+
+            var user = Users.Where(u => u.NormalizedEmail.Equals(normalizedEmail, StringComparison.OrdinalIgnoreCase)).SingleOrDefault();
+            return Task.FromResult(user);
+
+        }
+
+        public Task<string> GetNormalizedEmailAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            return Task.FromResult(user.NormalizedEmail);
+        }
+
+        public Task SetNormalizedEmailAsync(ApplicationUser user, string normalizedEmail, CancellationToken cancellationToken)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            user.NormalizedEmail = normalizedEmail;
+            return Task.CompletedTask;
+
+        }
+
+        // IUserLockoutStore
+        public Task<DateTimeOffset?> GetLockoutEndDateAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            return Task.FromResult(user.LockoutEnd);
+        }
+
+        public Task SetLockoutEndDateAsync(ApplicationUser user, DateTimeOffset? lockoutEnd, CancellationToken cancellationToken)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            user.LockoutEnd = lockoutEnd;
+            return Task.CompletedTask;
+        }
+
+        public Task<int> IncrementAccessFailedCountAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            user.AccessFailedCount++;
+            return Task.FromResult(user.AccessFailedCount);
+        }
+
+        public Task ResetAccessFailedCountAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            user.AccessFailedCount = 0;
+            return Task.CompletedTask;
+        }
+
+        public Task<int> GetAccessFailedCountAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            return Task.FromResult(user.AccessFailedCount);
+        }
+
+        public Task<bool> GetLockoutEnabledAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            return Task.FromResult(user.LockoutEnabled);
+        }
+
+        public Task SetLockoutEnabledAsync(ApplicationUser user, bool enabled, CancellationToken cancellationToken)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            user.LockoutEnabled = enabled;
+            return Task.CompletedTask;
+        }
+
+        // IUserTwoFactorStore
+        public Task SetTwoFactorEnabledAsync(ApplicationUser user, bool enabled, CancellationToken cancellationToken)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            user.TwoFactorEnabled = enabled;
+            return Task.CompletedTask;
+        }
+
+        public Task<bool> GetTwoFactorEnabledAsync(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            return Task.FromResult(user.TwoFactorEnabled);
+        }
     }
 }
