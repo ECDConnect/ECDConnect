56ec2933268166197356fe2555efaaac5f5a6807
Ubiratan Rodrigues <ubiratanrodrigues@Ubiratans-MacBook-Pro.local>
2024-06-14 14:57:41 -0300

    fix(registation): update landing page pic

--- src/lxp/src/pages/auth/oa-sign-up/oa-sign-up-or-login.tsx
+++ src/lxp/src/pages/auth/oa-sign-up/oa-sign-up-or-login.tsx
@@ -21,7 +21,7 @@ import { UserService } from '@/services/UserService';
 import { useTenant } from '@/hooks/useTenant';
 import { OAAgreements } from './components/oa-agreements/oa-agreements';
 import ROUTES from '@/routes/routes';
-import Banner1 from '../../../assets/banner-ss.jpg';
+import Banner1 from '../../../assets/banner-ss2.svg';
 import Banner3 from '../../../assets/banner2-ss-svg.svg';
 
 const token = new URLSearchParams(window.location.search).get('token');
