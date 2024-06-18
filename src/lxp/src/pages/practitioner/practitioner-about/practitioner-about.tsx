12e2e96ded792886b58a0d2015945539358d6084
Ubiratan Rodrigues <ubiratanrodrigues@Ubiratans-MacBook-Pro.local>
2024-06-18 14:54:17 -0300

    feat(profile): add profile emojis

--- src/lxp/src/pages/practitioner/practitioner-about/practitioner-about.tsx
+++ src/lxp/src/pages/practitioner/practitioner-about/practitioner-about.tsx
@@ -493,6 +493,7 @@ export const PractitionerAbout: React.FC = () => {
             onDelete={
               userProfilePicture || avatar ? deleteProfilePicture : undefined
             }
+            isProfileEmojis={true}
           ></PhotoPrompt>
         </div>
       </Dialog>
