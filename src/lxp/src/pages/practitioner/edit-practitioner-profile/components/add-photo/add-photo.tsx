a7a4c32452961b218f1ce0772bc7d7ccc69255c4
Ubiratan Rodrigues <ubiratanrodrigues@Ubiratans-MacBook-Pro.local>
2024-07-02 10:20:55 -0300

    fix(profile): fix error on fileUpload

--- src/lxp/src/pages/practitioner/edit-practitioner-profile/components/add-photo/add-photo.tsx
+++ src/lxp/src/pages/practitioner/edit-practitioner-profile/components/add-photo/add-photo.tsx
@@ -45,16 +45,16 @@ export const AddPhoto: React.FC<AddPhotoProps> = ({ onSubmit, isLoading }) => {
       appDispatch(userActions.updateUser(copy));
     }
 
-    if (!userProfilePicture) {
-      await createNewDocument({
-        data: imageBaseString,
-        userId: user?.id || '',
-        fileType: FileTypeEnum.ProfileImage,
-        fileName: `ProfilePicture_${user?.id}.png`,
-      });
-    } else {
-      updateDocument(userProfilePicture, imageBaseString);
-    }
+    // if (!userProfilePicture) {
+    //   await createNewDocument({
+    //     data: imageBaseString,
+    //     userId: user?.id || '',
+    //     fileType: FileTypeEnum.ProfileImage,
+    //     fileName: `ProfilePicture_${user?.id}.png`,
+    //   });
+    // } else {
+    //   updateDocument(userProfilePicture, imageBaseString);
+    // }
 
     // save details with request updateUser
     const userCopy = cloneDeep(user);
