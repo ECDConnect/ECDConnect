5dc8dfe827b6273733883ad2cd1c6df38b280117
Ubiratan Rodrigues <ubiratanrodrigues@Ubiratans-MacBook-Pro.local>
2024-07-05 08:23:02 -0300

    fix(profile): fix practitioner view classroom section

--- src/lxp/src/pages/dashboard/dashboard.tsx
+++ src/lxp/src/pages/dashboard/dashboard.tsx
@@ -996,11 +996,12 @@ export const Dashboard: React.FC = () => {
 
     history.push(profileRoute);
   };
-
+  console.log(classroom && !!classroom.id);
   const goToClassroom = () => {
     if (
-      (((classroom && !!classroom.id) ||
-        (classroomGroups && classroomGroups.length > 0)) &&
+      (classroom && !!classroom.id) ||
+      (classroomGroups &&
+        classroomGroups.length > 0 &&
         isRegistered &&
         isProgress &&
         isProgress > 0 &&
@@ -1008,6 +1009,7 @@ export const Dashboard: React.FC = () => {
         !missingProgramme) ||
       isTrialPeriod
     ) {
+      console.log('hihih');
       history.push(ROUTES.CLASSROOM.ROOT, {
         activeTabIndex: TabsItems.CLASSES,
       });
