60910038fe649dbc12b4a0402926c1a345d33cb5
Liana Wiehahn <liana@ecdconnect.co.za>
2024-07-10 12:20:24 +0200

    feat(w38): add more fields to activity
    EC-2635

--- src/api/core-api/SQLUpgradeScripts/20240710_ContentTypeField_ShareContent.sql
+++ src/api/core-api/SQLUpgradeScripts/20240710_ContentTypeField_ShareContent.sql
@@ -40,41 +40,45 @@ select nextval('public."ContentTypeField_Id_seq"'), 10, 'shareContent', 1, true,
 
 ---------------date----------------------
 insert into "ContentTypeField"
-select nextval('public."ContentTypeField_Id_seq"'), 6, 'updatedDate', 1, true, '', 1, current_date, current_date, null, null, 'Last updated', false, true, false; 
+select nextval('public."ContentTypeField_Id_seq"'), 6, 'updatedDate', 1, true, '', 1, current_date, current_date, null, null, 'Last updated', true, false, false; 
 
 insert into "ContentTypeField"
-select nextval('public."ContentTypeField_Id_seq"'), 12, 'updatedDate', 1, true, '', 2, current_date, current_date, null, null, 'Last updated', false, true, false; 
+select nextval('public."ContentTypeField_Id_seq"'), 12, 'updatedDate', 1, true, '', 2, current_date, current_date, null, null, 'Last updated', true, false, false; 
 
 insert into "ContentTypeField"
-select nextval('public."ContentTypeField_Id_seq"'), 7, 'updatedDate', 1, true, '', 3, current_date, current_date, null, null, 'Last updated', false, true, false; 
+select nextval('public."ContentTypeField_Id_seq"'), 7, 'updatedDate', 1, true, '', 3, current_date, current_date, null, null, 'Last updated', true, false, false; 
 
 insert into "ContentTypeField"
-select nextval('public."ContentTypeField_Id_seq"'), 7, 'updatedDate', 1, true, '', 4, current_date, current_date, null, null, 'Last updated', false, true, false; 
+select nextval('public."ContentTypeField_Id_seq"'), 7, 'updatedDate', 1, true, '', 4, current_date, current_date, null, null, 'Last updated', true, false, false; 
 
 insert into "ContentTypeField"
-select nextval('public."ContentTypeField_Id_seq"'), 7, 'updatedDate', 1, true, '', 5, current_date, current_date, null, null, 'Last updated', false, true, false; 
+select nextval('public."ContentTypeField_Id_seq"'), 7, 'updatedDate', 1, true, '', 5, current_date, current_date, null, null, 'Last updated', true, false, false; 
 
 insert into "ContentTypeField"
-select nextval('public."ContentTypeField_Id_seq"'), 7, 'updatedDate', 1, true, '', 6, current_date, current_date, null, null, 'Last updated', false, true, false; 
+select nextval('public."ContentTypeField_Id_seq"'), 7, 'updatedDate', 1, true, '', 6, current_date, current_date, null, null, 'Last updated', true, false, false; 
 
 insert into "ContentTypeField"
-select nextval('public."ContentTypeField_Id_seq"'), 5, 'updatedDate', 1, true, '', 7, current_date, current_date, null, null, 'Last updated', false, true, false; 
+select nextval('public."ContentTypeField_Id_seq"'), 5, 'updatedDate', 1, true, '', 7, current_date, current_date, null, null, 'Last updated', true, false, false; 
 
 insert into "ContentTypeField"
-select nextval('public."ContentTypeField_Id_seq"'), 6, 'updatedDate', 1, true, '', 8, current_date, current_date, null, null, 'Last updated', false, true, false; 
+select nextval('public."ContentTypeField_Id_seq"'), 6, 'updatedDate', 1, true, '', 8, current_date, current_date, null, null, 'Last updated', true, false, false; 
 
 insert into "ContentTypeField"
-select nextval('public."ContentTypeField_Id_seq"'), 8, 'updatedDate', 1, true, '', 9, current_date, current_date, null, null, 'Last updated', false, true, false; 
+select nextval('public."ContentTypeField_Id_seq"'), 8, 'updatedDate', 1, true, '', 9, current_date, current_date, null, null, 'Last updated', true, false, false; 
 
 insert into "ContentTypeField"
-select nextval('public."ContentTypeField_Id_seq"'), 10, 'updatedDate', 1, true, '', 10, current_date, current_date, null, null, 'Last updated', false, true, false; 
+select nextval('public."ContentTypeField_Id_seq"'), 10, 'updatedDate', 1, true, '', 10, current_date, current_date, null, null, 'Last updated', true, false, false; 
 
 insert into "ContentTypeField"
-select nextval('public."ContentTypeField_Id_seq"'), 6, 'updatedDate', 1, true, '', 11, current_date, current_date, null, null, 'Last updated', false, true, false; 
+select nextval('public."ContentTypeField_Id_seq"'), 6, 'updatedDate', 1, true, '', 11, current_date, current_date, null, null, 'Last updated', true, false, false; 
 
 insert into "ContentTypeField"
-select nextval('public."ContentTypeField_Id_seq"'), 4, 'updatedDate', 1, true, '', 12, current_date, current_date, null, null, 'Last updated', false, true, false; 
+select nextval('public."ContentTypeField_Id_seq"'), 4, 'updatedDate', 1, true, '', 12, current_date, current_date, null, null, 'Last updated', true, false, false; 
 
 insert into "ContentTypeField"
-select nextval('public."ContentTypeField_Id_seq"'), 11, 'updatedDate', 1, true, '', 13, current_date, current_date, null, null, 'Last updated', false, true, false; 
+select nextval('public."ContentTypeField_Id_seq"'), 11, 'updatedDate', 1, true, '', 13, current_date, current_date, null, null, 'Last updated', true, false, false; 
 
+-- activities and themes
+INSERT INTO public."ContentTypeField"
+("Id", "FieldOrder", "FieldName", "FieldTypeId", "IsActive", "DataLinkName", "ContentTypeId", "InsertedDate", "UpdatedDate", "UpdatedBy", "TenantId", "DisplayName", "DisplayMainTable", "DisplayPage", "IsRequired")
+VALUES(nextval('public."ContentTypeField_Id_seq"'), 12, 'themes', 4, true, 'Themes', 13, current_date, current_date, NULL, null, 'Themes', true, true, false);
