e5333c23e41721cb0a11eb91b507c3cf7fce1686
lubezerra <lucilene@ecdconnect.co.za>
2024-07-17 11:40:16 -0300

    fix(child): fix profile
    EC-2509

--- src/lxp/src/pages/child/child-profile/child-profile.tsx
+++ src/lxp/src/pages/child/child-profile/child-profile.tsx
@@ -29,7 +29,6 @@ import { getWeek, startOfISOWeekYear } from 'date-fns';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory, useLocation } from 'react-router';
-import { RemoveChildPrompt } from '../../../components/remove-child-prompt/remove-child-prompt';
 import OnlineOnlyModal from '../../../modals/offline-sync/online-only-modal';
 import { useOnlineStatus } from '@hooks/useOnlineStatus';
 import { useStaticData } from '@hooks/useStaticData';
@@ -81,6 +80,10 @@ import {
   TabsItems,
 } from '@/pages/classroom/class-dashboard/class-dashboard.types';
 import { ChildAttendanceReportState } from '../child-attendance-report/child-attendance-report.types';
+import { useUserPermissions } from '@/hooks/useUserPermissions';
+import { useIsTrialPeriod } from '@/hooks/useIsTrialPeriod';
+import { useThunkFetchCall } from '@/hooks/useThunkFetchCall';
+import { ChildrenActions } from '@/store/children/children.actions';
 
 const baseNotificationListItem: ListItemProps = {
   key: 'message-caregiver',
@@ -133,6 +136,13 @@ export const ChildProfile: React.FC = () => {
     (role) => role.systemName === RoleSystemNameEnum.Coach
   );
 
+  const isTrialPeriod = useIsTrialPeriod();
+
+  const { hasPermissionToManageChildren } = useUserPermissions();
+
+  const hasPermissionToEdit =
+    hasPermissionToManageChildren || practitioner?.isPrincipal || isTrialPeriod;
+
   const classProgrammes = classroomGroup?.classProgrammes?.filter(
     (x) => x?.isActive
   );
@@ -143,7 +153,6 @@ export const ChildProfile: React.FC = () => {
   const attendanceData = useSelector(attendanceSelectors.getAttendance);
   const authUser = useSelector(authSelectors.getAuthUser);
 
-  // TODO: this selector isn't working (show photo of child & allow user to add photo of child IF caregiver has consented)
   const childPhotoConsent = useSelector(
     userSelectors.getUserConsentByType(
       child?.userId,
@@ -171,8 +180,6 @@ export const ChildProfile: React.FC = () => {
     useState(false);
 
   const [childAge, setChildAge] = useState<Age>();
-  const [removeChildConfirmationVisible, setRemoveChildConfirmationVisible] =
-    useState<boolean>(false);
   const childPendingWorkflowStatusId = getWorkflowStatusIdByEnum(
     WorkflowStatusEnum.ChildPending
   );
@@ -182,6 +189,11 @@ export const ChildProfile: React.FC = () => {
   const [notifications, setNotifications] = useState<ListItemProps[]>([]);
   const [attendanceReport, setAttendanceReport] =
     useState<ChildAttendanceReportModel>();
+
+  const { isLoading } = useThunkFetchCall(
+    'children',
+    ChildrenActions.UPDATE_CHILD
+  );
 
   useEffect(() => {
     if (!isOnline) {
@@ -364,6 +376,7 @@ export const ChildProfile: React.FC = () => {
         buttonText: 'Add',
         buttonTextColor: 'secondary',
         buttonColor: 'secondaryAccent2',
+        withBorderRadius: false,
         onButtonClick: () => setCreateChildNoteVisible(true),
       };
     } else {
@@ -379,6 +392,7 @@ export const ChildProfile: React.FC = () => {
         showDivider: true,
         dividerType: 'dashed',
         withPaddingY: true,
+        withBorderRadius: false,
         onButtonClick: () => history.push(ROUTES.CHILD_NOTES, { childId }),
       };
     }
@@ -453,29 +467,31 @@ export const ChildProfile: React.FC = () => {
   };
 
   const picturePromptOnAction = async (imageBaseString: string) => {
-    const copy = Object.assign({}, child);
-    if (copy) {
-      copy.user!.profileImageUrl = imageBaseString;
-      appDispatch(childrenActions.updateChild(copy));
-      await appDispatch(
-        childrenThunkActions.updateChild({
-          id: copy.id as string,
-          child: copy,
-        })
-      );
-    }
+    const updatedChild = {
+      ...child,
+      user: { ...child?.user, profileImageUrl: imageBaseString },
+    };
+
+    appDispatch(childrenActions.updateChild(updatedChild));
+    await appDispatch(
+      childrenThunkActions.updateChild({
+        id: updatedChild.id as string,
+        child: updatedChild,
+      })
+    );
 
     if (profilePicture) {
       appDispatch(
         documentActions.updateDocument({
           ...profilePicture,
           file: imageBaseString,
+          fileType: FileTypeEnum.ProfileImage,
         })
       );
     } else {
       const fileName = `ProfilePicture_${child?.user?.id}.png`;
 
-      const statusId = await getWorkflowStatusIdByEnum(
+      const statusId = getWorkflowStatusIdByEnum(
         WorkflowStatusEnum.DocumentVerified
       );
 
@@ -525,6 +541,7 @@ export const ChildProfile: React.FC = () => {
         showSubTitleShape: true,
         withPaddingY: true,
         showDivider: true,
+        withBorderRadius: false,
         dividerType: 'dashed',
         onButtonClick: () => {
           if (isOnline) {
@@ -550,6 +567,7 @@ export const ChildProfile: React.FC = () => {
         buttonColor: 'secondaryAccent2',
         showButton: true,
         showDivider: true,
+        withBorderRadius: false,
         dividerType: 'dashed',
         withPaddingY: true,
         onButtonClick: () => {
@@ -571,6 +589,7 @@ export const ChildProfile: React.FC = () => {
         buttonColor: 'secondaryAccent2',
         showButton: true,
         showDivider: true,
+        withBorderRadius: false,
         dividerType: 'dashed',
         withPaddingY: true,
         onButtonClick: () => {
@@ -591,6 +610,7 @@ export const ChildProfile: React.FC = () => {
         buttonColor: 'quatenary',
         showButton: !practitionerIsOnLeave && isPrincipal,
         showDivider: true,
+        withBorderRadius: false,
         dividerType: 'dashed',
         withPaddingY: true,
         onButtonClick: () => {
@@ -663,7 +683,6 @@ export const ChildProfile: React.FC = () => {
             canChangeImage={!!childPhotoConsent}
             dataUrl={profilePicture?.file || child?.user?.profileImageUrl || ''}
             size={'header'}
-            // eslint-disable-next-line @typescript-eslint/no-empty-function
             onPressed={() => setEditProfilePictureVisible(true)}
           />
         </div>
@@ -721,7 +740,7 @@ export const ChildProfile: React.FC = () => {
             type="filled"
             onClick={contactCaregivers}
           />
-          {!practitionerIsOnLeave && (
+          {!practitionerIsOnLeave && hasPermissionToEdit && (
             <Button
               id="child_remove"
               icon="TrashIcon"
@@ -729,7 +748,7 @@ export const ChildProfile: React.FC = () => {
               color="quatenary"
               textColor="quatenary"
               type="outlined"
-              onClick={() => setRemoveChildConfirmationVisible(true)}
+              onClick={goToRemoveChild}
               text={`Remove ${child?.user?.firstName}`}
             />
           )}
@@ -751,24 +770,12 @@ export const ChildProfile: React.FC = () => {
         </div>
       </Dialog>
       <Dialog
-        className={'mb-16 px-4'}
-        stretch={true}
-        visible={removeChildConfirmationVisible}
-        position={DialogPosition.Bottom}
-      >
-        <RemoveChildPrompt
-          child={child}
-          childUser={child?.user}
-          onProceed={goToRemoveChild}
-          onClose={() => setRemoveChildConfirmationVisible(false)}
-        />
-      </Dialog>
-      <Dialog
         visible={editProfilePictureVisible}
         position={DialogPosition.Bottom}
       >
         <div className={'p-4'}>
           <PhotoPrompt
+            isLoading={isLoading}
             title="Profile Photo"
             onClose={() => setEditProfilePictureVisible(false)}
             onAction={picturePromptOnAction}
