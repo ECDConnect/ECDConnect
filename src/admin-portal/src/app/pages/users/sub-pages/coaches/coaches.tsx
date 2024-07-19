77627de7ca6d84e41a531d89c8d935ffec4f63ce
Liana Wiehahn <liana@ecdconnect.co.za>
2024-07-19 12:36:31 +0200

    fix(portal tables): fix filter for active and inactive selections on practitioner and coach

--- src/admin-portal/src/app/pages/users/sub-pages/coaches/coaches.tsx
+++ src/admin-portal/src/app/pages/users/sub-pages/coaches/coaches.tsx
@@ -566,6 +566,7 @@ export default function Coaches() {
       <div className="bg-adminPortalBg h-full rounded-2xl p-4 ">
         <div className="rounded-xl bg-white p-12">
           <Table
+            watchMode={true}
             ref={tableRef}
             rows={rows}
             columns={columns}
