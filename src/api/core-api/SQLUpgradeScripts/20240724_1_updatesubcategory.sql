e7038d66b3208dde637260911a2ed8f3a01fcc37
Matthew de Kock <matthew@ecdconnect.co.za>
2024-07-25 13:04:58 +0000

    Merged in EC-2811_SkillsFor0to5months (pull request #3828)
    EC-2811: Added skills for children 0 to 5 months old
    
    Approved-by: Amanda van Heerden

---
+++ src/api/core-api/SQLUpgradeScripts/20240724_1_updatesubcategory.sql
@@ -0,0 +1,4 @@
+update "ContentValue" cv 
+set "Value" = 'Communication: speaking & listening'
+where cv."ContentTypeFieldId" = 29 and cv."LocaleId" = '9688cd08-adef-408c-9d34-5d75ae5c44df'
+	and cv."Value" = 'Talking & understanding';
