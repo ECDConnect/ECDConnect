4f48dfdc0d88d14301152e3abaceb808e26f8304
Amanda van Heerden <amanda@meraki-it.co.za>
2024-08-04 13:54:49 +0200

    feat(ec-2630): theme changes

--- src/api/core-api/SQLUpgradeScripts/20240730_ContentTypeField.sql
+++ src/api/core-api/SQLUpgradeScripts/20240730_ContentTypeField.sql
@@ -2,3 +2,8 @@
 update "ContentTypeField" set "DisplayName" = 'Would you like to share this activity with other organisations' where "ContentTypeId" =13 and "FieldName" ='shareContent';
 update "ContentTypeField" set "DisplayName" = 'Steps (What do I do?)' where "ContentTypeId" =13 and "FieldName" ='description';
 update "ContentTypeField" set "DisplayName" = 'Notes' where "ContentTypeId" =13 and "FieldName" ='notes';
+
+
+update "ContentTypeField" set "DisplayName" = 'Title' where "ContentTypeId" =8 and "FieldName" ='name';
+update "ContentTypeField" set "DisplayName" = 'Theme icon' where "ContentTypeId"=8 and "FieldName" = 'imageUrl';
+update "ContentTypeField" set "DisplayName" = 'Would you like to share this theme with other organisations using other versions of ECD Connect? *' where "ContentTypeId" =8 and "FieldName" ='shareContent';
