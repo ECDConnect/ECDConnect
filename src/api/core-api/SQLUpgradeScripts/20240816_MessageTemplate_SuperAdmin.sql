881770d9d5f4a2dc6823e2876707243a3f2f1eaa
Liana Wiehahn <liana@ecdconnect.co.za>
2024-08-16 13:29:20 +0200

    feat(setup): super admin emails

---
+++ src/api/core-api/SQLUpgradeScripts/20240816_MessageTemplate_SuperAdmin.sql
@@ -0,0 +1,48 @@
+INSERT INTO "MessageTemplate" ("Id","IsActive","InsertedDate","UpdatedDate","UpdatedBy","Protocol","TemplateType","Message","TenantId","Subject","CTA","CTAText","TypeCode","NotificationColor","Ordering","Action") VALUES
+	 ('c32b8bf3-9d14-44b3-9b0e-4ccbf6b24698'::uuid,true,current_date,current_date,NULL,'email','welcome-email-to-new-super-admin','Dear [[FirstName]],<br><br>
+Hi! Welcome to the ECD Connect family! My name is Cebisa, the same virtual helper who you will meet in your app - and I just wanted to say hi! My team and I are so pleased to have you onboard and look forward to many happy insights together! <br><br>
+
+Working with ECD experts, behavioural scientists and tech-nerds, we created ECD Connect to support you to support your network of ECD heroes. We know you guys work really hard to support those incredible people out there providing critical services to our children. We are in awe!<br><br>
+
+And, we are also in-preparations! We will be in touch within one week with an update on your new platform.<br><br>
+
+Thank you for your patience while we get your baby up and running!<br><br>
+
+If you have any questions before then please respond to this address.<br><br>
+
+Kind regards,<br>
+Cebisa and the whole ECD Connect team<br><br>
+
+MORE ABOUT ECD CONNECT...<br>
+ECD Connect is an extensive and sophisticated licence-free, open-source, customisable digital platform. The platform includes web apps for principals, practitioners and field-support staff, that are designed for low-end smartphones, require minimal data, and can be used offline for up to 30 days. It also includes a head office admin portal for content, user and data management.<br><br>
+
+What are ECD Connect’s objectives?<br><br>
+<ol>
+<li>Reduce practitioners’ administrative and cognitive load</li>
+<li>Give practitioners meaningful feedback and a way to connect to their community of practice</li>
+<li>Equip organisations working in the ECD sector to monitor, support and respond to their programmes</li>
+</ol>
+
+What does ECD Connect offer to practitioners?<br>
+As a digital work companion, ECD Connect provides early educators with a range of tools to assist them in their jobs, including:<br><br>
+
+<ul>
+<li>Classroom management tools, such as multilingual educational resources, daily programmes, attendance trackers, assessment and other pedagogical tools.</li>
+<li>Business management tools, such as income and expense trackers, calendar and scheduling, venue and other checklists.</li>
+<li>In-app courses for ongoing professional development.</li>
+<li>Direct line of feedback and support.</li>
+<li>Connection to community of practice and incentives programmes.</li>
+</ul>
+
+The platform equips practitioners to be more effective in their roles and to develop both as individuals and as a collective. Research in the field has shown that by raising standards of professionalism and improving outcomes, practitioners receive greater recognition for their work and increased demand for their services, allowing them to generate more value for themselves and help to ensure that the critical work they do is financially sustainable.<br>
+If more competent practitioners remain in their line of work, the faster the sector will be able to close the gap to achieving universal access to quality ECD programmes.<br>
+What does ECD Connect offer to organisations?<br>
+The platform serves as a work management system providing direct information flows between ECD practitioners, supporting field staff, and management; thus enabling organisations to:<br>
+Improve programme monitoring.<br>
+Provide practitioners with information, in-service support, and peer interaction.<br>
+Perform quality assessments and provide feedback.<br>
+Enhance reporting with contextually relevant site-level data.<br><br>
+
+ECD Connect has been built with the support of the Global Innovation Fund, the DG Murray Trust and DSI/TIA as part of the 
+South African Government''s initiative to promote and support novel South African technologies. The platform is dedicated to fostering and enhancing the dedicated work of the 
+ECD sector, and will be accessible free of charge for non-commercial use.',NULL,'Welcome to ECD Connect',NULL,NULL,NULL,NULL,0,NULL);
