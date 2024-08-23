cb49e80643ff260ef91d7e3c6330336364fc5e49
Ubiratan Rodrigues <ubiratanrodrigues@192.168.1.5>
2024-08-23 08:39:48 -0300

    fix(users): fix add users modal height

--- src/admin-portal/src/app/pages/users/sub-pages/coaches/coaches.tsx
+++ src/admin-portal/src/app/pages/users/sub-pages/coaches/coaches.tsx
@@ -662,7 +662,7 @@ export default function Coaches() {
         </div>
       </div>
       <Dialog
-        className="absolute left-56 bottom-96 w-6/12"
+        className="absolute left-56 bottom-96 h-96 w-6/12"
         stretch
         visible={handleAddUser}
         position={DialogPosition.Middle}
