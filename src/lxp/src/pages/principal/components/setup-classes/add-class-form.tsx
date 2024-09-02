3b2b216c64e9f62a9d9fea711075261fead77b34
Ubiratan Rodrigues <ubiratanrodrigues@Ubiratans-MacBook-Pro.local>
2024-09-02 09:33:57 -0300

    fix(profile): fix add a class in the setup flow

--- src/lxp/src/pages/principal/components/setup-classes/add-class-form.tsx
+++ src/lxp/src/pages/principal/components/setup-classes/add-class-form.tsx
@@ -66,7 +66,7 @@ export const AddClassForm = ({ onSubmit }: { onSubmit: () => void }) => {
 
   const classroomGroup = useSelector(classroomsSelectors.getClassroomGroups);
 
-  const { name, meetEveryday, userId } = useWatch({
+  const { name, meetEveryday, practitionerId } = useWatch({
     control: classFormControl,
   });
 
@@ -124,7 +124,7 @@ export const AddClassForm = ({ onSubmit }: { onSubmit: () => void }) => {
 
   const isFormValid = () => {
     const meetingDays = getClassFormValues().meetingDays;
-    return meetingDays && meetingDays?.length > 1 && userId;
+    return meetingDays && meetingDays?.length > 1 && practitionerId;
   };
 
   const saveClassData = async () => {
