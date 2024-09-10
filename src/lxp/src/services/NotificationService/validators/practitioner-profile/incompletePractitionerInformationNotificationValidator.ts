4884352de2aacc74ea3f8a7bbef4906b44ef759d
Ubiratan Rodrigues <ubiratanrodrigues@Ubiratans-MacBook-Pro.local>
2024-09-10 14:05:26 -0300

    fix(notifications): fix practitioner progress 1 notification

--- src/lxp/src/services/NotificationService/validators/practitioner-profile/incompletePractitionerInformationNotificationValidator.ts
+++ src/lxp/src/services/NotificationService/validators/practitioner-profile/incompletePractitionerInformationNotificationValidator.ts
@@ -173,7 +173,7 @@ export class IncompletePractitionerInformationNotificationValidator
                 : 'Join or add a preschool!',
             message:
               practitionerState?.practitioner?.progress === 1.0
-                ? 'Ask your principal to sign up for AppName and add you to the preschool, or fill in your preschool code now.'
+                ? `Ask your principal to sign up for ${tenantState?.tenant?.applicationName} and add you to the preschool, or fill in your preschool code now.`
                 : practitionerState?.practitioner?.principalHierarchy
                 ? 'Connect with your principal & manage your classes.'
                 : 'Set up your preschool or connect with your principal.',
