33feeb7b9552b5cf6eb75b7bf4846a8c8b88d0af
Liana Wiehahn <liana@ecdconnect.co.za>
2024-09-16 11:27:30 +0200

    fix(w46): resources list table

--- src/api/core-api/GraphApi/Queries/Portal/ResourceQueryExtension.cs
+++ src/api/core-api/GraphApi/Queries/Portal/ResourceQueryExtension.cs
@@ -4,10 +4,12 @@ using ECDLink.Core.Services.Interfaces;
 using ECDLink.DataAccessLayer.Entities;
 using HotChocolate;
 using HotChocolate.Types;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using static iTextSharp.text.pdf.AcroFields;
 
 namespace EcdLink.Api.CoreApi.GraphApi.Queries.Portal
 {
@@ -37,63 +39,127 @@ namespace EcdLink.Api.CoreApi.GraphApi.Queries.Portal
 
             if (resourceData.Any())
             {
-                if (search == null && likesSearch == null && dataFreeSearch == null && startDate == null && endDate == null)
+                if (string.IsNullOrEmpty(search) && likesSearch.Count == 0 && dataFreeSearch.Count == 0 && startDate == null && endDate == null)
                 {
                     return resourceData;
                 } 
                 else
                 {
                     var allContentValuePairs = new List<object>();
-                    foreach (var resource in resourceData)
+
+                    if (!string.IsNullOrEmpty(search))
                     {
-                        var item = (IDictionary<string, object>)resource;
-                        item.TryGetValue("id", out var id);
-                        item.TryGetValue("resourceType", out var resourceTypeValue);
-                        item.TryGetValue("title", out var titleValue);
-                        item.TryGetValue("shortDescription", out var shortDescription);
-                        item.TryGetValue("link", out var link);
-                        item.TryGetValue("longDescription", out var longDescription);
-                        item.TryGetValue("dataFree", out var dataFree);
-                        item.TryGetValue("sectionType", out var sType);
-                        item.TryGetValue("numberLikes", out var numberLikes);
-                        item.TryGetValue("availableLanguages", out var availableLanguages);
-                        item.TryGetValue("updatedDate", out var updatedDate);
-                        item.TryGetValue("insertedDate", out var insertedDate);
-
-
-                        if (likesSearch != null)
+                        foreach (var resource in resourceData)
                         {
-
+                            var item = (IDictionary<string, object>)resource;
+                            item.TryGetValue("title", out var title);
+                            
+                            if (title.ToString().ToLower().Contains(search.ToLower())) {
+                                allContentValuePairs.Add(item);
+                            }
                         }
-                        if (dataFreeSearch != null)
-                        {
-                        }
-
-                        if (startDate != null)
-                        {
-                        }
-
-                        if (endDate != null)
-                        {
-                        }
-
-                        Dictionary<string, object> dataDict = new Dictionary<string, object>
-                        {
-                            { "id", Convert.ToString(id) },
-                            { "resourceType", Convert.ToString(resourceTypeValue) },
-                            { "title", Convert.ToString(titleValue) },
-                            { "shortDescription", Convert.ToString(shortDescription) },
-                            { "link", Convert.ToString(link)},
-                            { "longDescription", Convert.ToString(longDescription) },
-                            { "dataFree", Convert.ToString(dataFree) },
-                            { "sectionType", Convert.ToString(sType) },
-                            { "numberLikes", Convert.ToString(numberLikes) },
-                            { "availableLanguages", Convert.ToString(availableLanguages) },
-                            { "updatedDate", Convert.ToString(updatedDate) },
-                            { "insertedDate", Convert.ToString(insertedDate) },
-                        };
-                        allContentValuePairs.Add(dataDict);
                     }
+
+                    if (dataFreeSearch.Count != 0)
+                    {
+                        foreach (var resource in resourceData)
+                        {
+                            var item = (IDictionary<string, object>)resource;
+                            item.TryGetValue("dataFree", out var dataFree);
+
+                            if (dataFreeSearch.Contains(dataFree))
+                            {
+                                allContentValuePairs.Add(item);
+                            }
+                        }
+                    }
+
+                    if (likesSearch.Count != 0)
+                    {
+                        foreach (var resource in resourceData)
+                        {
+                            var item = (IDictionary<string, object>)resource;
+                            item.TryGetValue("numberLikes", out var numberLikes);
+
+                            if (numberLikes is null)
+                            {
+                                if (likesSearch.Contains(Constants.ResourceLikes.Zero))
+                                {
+                                    allContentValuePairs.Add(item);
+                                }
+                            }
+                            else
+                            {
+                                var itemLikes = int.TryParse(numberLikes.ToString(), out var likes);
+
+                                if (likesSearch.Contains(Constants.ResourceLikes.Zero))
+                                {
+                                    if (numberLikes.ToString() == "" || numberLikes.ToString() == "0")
+                                    {
+                                        allContentValuePairs.Add(item);
+                                    }
+                                }
+
+                                if (likesSearch.Contains(Constants.ResourceLikes.OneToTen))
+                                {
+                                    if (likes > 0 && likes < 11)
+                                    {
+                                        allContentValuePairs.Add(item);
+                                    }
+                                }
+                                else if (likesSearch.Contains(Constants.ResourceLikes.ElevenToFifty))
+                                {
+                                    if (likes > 10 && likes < 51)
+                                    {
+                                        allContentValuePairs.Add(item);
+                                    }
+                                }
+                                else if (likesSearch.Contains(Constants.ResourceLikes.FiftyOneToHundred))
+                                {
+                                    if (likes > 51 && likes < 101)
+                                    {
+                                        allContentValuePairs.Add(item);
+                                    }
+                                }
+                                else if (likesSearch.Contains(Constants.ResourceLikes.MoreThanHundred))
+                                {
+                                    if (likes > 100)
+                                    {
+                                        allContentValuePairs.Add(item);
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    if (startDate != null && endDate != null)
+                    {
+                        foreach (var resource in resourceData)
+                        {
+                            var item = (IDictionary<string, object>)resource;
+                            item.TryGetValue("updatedDate", out var updatedDate);
+                            item.TryGetValue("insertedDate", out var insertedDate);
+
+                            if (insertedDate is not null)
+                            {
+                                var insertDate = (DateTime)insertedDate;
+                                if (insertDate >= startDate && insertDate <= endDate)
+                                {
+                                    allContentValuePairs.Add(item);
+                                }
+                            } 
+                            else
+                            {
+                                var updateDate = (DateTime)updatedDate;
+                                if (updateDate >= startDate && updateDate <= endDate)
+                                {
+                                    allContentValuePairs.Add(item);
+                                }
+                            }
+
+                        }
+                    }
+
                     return allContentValuePairs;
                 }
             }
