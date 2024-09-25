c29017415acc361a86d92179dc96e39a9789ea78
Liana Wiehahn <liana@ecdconnect.co.za>
2024-09-25 14:58:06 +0200

    fix(resource): remove import

--- src/api/core-api/GraphApi/Queries/Portal/ResourceQueryExtension.cs
+++ src/api/core-api/GraphApi/Queries/Portal/ResourceQueryExtension.cs
@@ -16,7 +16,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
 
 namespace EcdLink.Api.CoreApi.GraphApi.Queries.Portal
 {
