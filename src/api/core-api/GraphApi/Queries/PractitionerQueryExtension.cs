9af1f2280a940466f3c5de26734859c9e9ac88b2
Liana Wiehahn <liana@ecdconnect.co.za>
2024-10-14 08:02:19 +0200

    fix(stats): adjust practitioner query for no income or expense

--- src/api/core-api/GraphApi/Queries/PractitionerQueryExtension.cs
+++ src/api/core-api/GraphApi/Queries/PractitionerQueryExtension.cs
@@ -587,6 +587,10 @@ namespace EcdLink.Api.CoreApi.GraphApi.Queries.SmartStart
                         .Select(e => start.AddMonths(e))
                         .TakeWhile(e => e <= end)
                         .Select(e => e.Month).Distinct().ToList();
+            var years = Enumerable.Range(0, Int32.MaxValue)
+                        .Select(e => start.AddMonths(e))
+                        .TakeWhile(e => e <= end)
+                        .Select(e => e.Year).Distinct().ToList();
 
             var monthReports = new List<MonthlyAttendanceReportModel>();
             var attendanceStart = startDate.Date;
@@ -634,9 +638,14 @@ namespace EcdLink.Api.CoreApi.GraphApi.Queries.SmartStart
             stats.TotalProgressReportsCompleted = totalProgressReportsCompleted;
             stats.TotalProgressReportsNotCompleted = totalProgressReportsNotCompleted;
 
-            var statementData = statementsRepo.GetAll().Where(x => x.IsActive == true && x.UserId == userId && x.InsertedDate.Date >= startDate.Date && x.InsertedDate.Date <= endDate.Value.Date).ToList();
+
+            var statementData = statementsRepo.GetAll().Where(x => x.IsActive == true
+                                                                   && x.UserId == userId
+                                                                   && years.Contains(x.Year)
+                                                                   && months.Contains(x.Month)).ToList();
+
             stats.TotalIncomeStatementsDownloaded = statementData.Where(x => x.Downloaded == true).Count();
-            stats.TotalIncomeStatementsWithNoItems = statementData.Where(x => x.IncomeItems.Count == 0 && x.ExpenseItems.Count == 0).Count();
+            stats.TotalIncomeStatementsWithNoItems = statementData.Where(x => x.IncomeItems.Count == 0 || x.ExpenseItems.Count == 0).Count();
 
             return stats;
         }
