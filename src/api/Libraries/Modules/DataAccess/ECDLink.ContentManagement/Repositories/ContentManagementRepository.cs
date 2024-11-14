13c1aed80650095629d764d67ba10cef98c8d083
Liana Wiehahn <liana@ecdconnect.co.za>
2024-11-14 08:43:38 +0200

    fix(w9): revert file type on cms file

--- src/api/Libraries/Modules/DataAccess/ECDLink.ContentManagement/Repositories/ContentManagementRepository.cs
+++ src/api/Libraries/Modules/DataAccess/ECDLink.ContentManagement/Repositories/ContentManagementRepository.cs
@@ -778,7 +778,7 @@ namespace ECDLink.ContentManagement.Repositories
             using MemoryStream fileStream = new MemoryStream(bytes);
 
             var fileName = DateTime.Now.Ticks + "_" + fieldName + getFileType(fileStr.Substring(0, fileStr.LastIndexOf(',')));
-            var fileUrl = Task.Run(() => _fileService.UploadFileStream(fileStream, fileName, FileTypeEnum.Unknown)).Result;
+            var fileUrl = Task.Run(() => _fileService.UploadFileStream(fileStream, fileName, FileTypeEnum.ContentImage)).Result;
             fileStream.Dispose();
 
             return fileUrl.ToString();
