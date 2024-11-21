95d15904b20e9456adb985d0e4ccc274cd8e7685
Kgaogelo Thorn <kgaogelo@swipeix.com>
2024-11-21 13:49:05 +0200

    feat(ec-3551): added coming soon in the journey tab

--- src/lxp/src/pages/practitioner/practitioner-profile/practitioner-journey/index.tsx
+++ src/lxp/src/pages/practitioner/practitioner-profile/practitioner-journey/index.tsx
@@ -66,6 +66,7 @@ export const PractitionerJourney = ({
 
   const user = useSelector(getUser);
   const userId = user?.id || '';
+  const isComingSoon = true;
 
   const timeline = useSelector(getPractitionerTimelineByIdSelector(userId));
   const currentPqaRating = useSelector(getCurrentPQaRatingByUserId(userId));
@@ -401,6 +402,14 @@ export const PractitionerJourney = ({
         backgroundColor="uiLight"
         className="tex pt-4"
       />
+    );
+  }
+
+  if (isComingSoon) {
+    return (
+      <div className="mt-2 flex flex-col p-4">
+        <Typography color="textDark" text={`Coming soon`} type={'h2'} />
+      </div>
     );
   }
 
