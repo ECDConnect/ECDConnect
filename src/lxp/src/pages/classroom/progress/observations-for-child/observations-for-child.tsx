fb66ca83a0fd98ffac1a08f02a596feedb130e43
Liana Wiehahn <liana@ecdconnect.co.za>
2024-11-28 07:47:49 +0200

    fix(w9): enable last button on observation page

--- src/lxp/src/pages/classroom/progress/observations-for-child/observations-for-child.tsx
+++ src/lxp/src/pages/classroom/progress/observations-for-child/observations-for-child.tsx
@@ -243,7 +243,6 @@ export const ObservationsForChild: React.FC = () => {
           icon="XIcon"
           text="Save & exit"
           textColor="quatenary"
-          disabled={currentStep === totalSkillsSteps + 2 ? !nextEnabled : false}
         />
       </div>
     </BannerWrapper>
