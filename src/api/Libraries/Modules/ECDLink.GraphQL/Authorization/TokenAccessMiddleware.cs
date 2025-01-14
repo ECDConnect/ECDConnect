a691ae0654f617f93e655b7d0fa25139fc51fbc2
djwaltswipeix <drikus@swipeix.com>
2025-01-14 12:19:58 +0200

    fix(bug): resolved more conflicts

--- src/api/Libraries/Modules/ECDLink.GraphQL/Authorization/TokenAccessMiddleware.cs
+++ src/api/Libraries/Modules/ECDLink.GraphQL/Authorization/TokenAccessMiddleware.cs
@@ -3,6 +3,7 @@ using ECDLink.Security.AccessModifiers.OpenAccess;
 using ECDLink.Security.Enums;
 using HotChocolate.Resolvers;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ECDLink.EGraphQL.Authorization
@@ -16,17 +17,26 @@ namespace ECDLink.EGraphQL.Authorization
             _next = next ?? throw new ArgumentNullException(nameof(next));
         }
 
-        public override async Task InvokeAsync(IDirectiveContext context)
+        public override async Task InvokeAsync(IMiddlewareContext context)
         {
-            var directive = context.Directive
-                .ToObject<TokenAccessDirective>();
+            // Since `IMiddlewareContext` does not support `Directive`, you need to pass the directive explicitly
+            var directive = context.Selection.SyntaxNode.Directives
+                .FirstOrDefault(d => d.Name.Value == "tokenAccess");
 
-            var validatorType = typeof(IOpenAccessValidator<>).MakeGenericType(directive.Validator);
+            if (directive == null)
+            {
+                await _next(context).ConfigureAwait(false);
+                return;
+            }
 
+            // Extract the validator type and other properties safely
+            var directiveArgs = directive.Arguments
+                .ToDictionary(arg => arg.Name.Value, arg => arg.Value.Value);
+
+            var validatorType = Type.GetType(directiveArgs["validator"].ToString());
             var validator = context.Services.GetService(validatorType);
 
             var token = context.ArgumentValue<string>(ArgumentConstants.Token);
-
             var method = validator.GetType().GetMethod("ValidateToken");
 
             var state = (AuthState)method.Invoke(validator, new object[] { token });
