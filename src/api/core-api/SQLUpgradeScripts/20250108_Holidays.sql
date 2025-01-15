5cb1a59aacd815a9c5152e567efce23d8c38c4d4
djwaltswipeix <drikus@swipeix.com>
2025-01-15 16:15:31 +0200

    fix(rework): rework of attendance percentage

--- src/api/core-api/SQLUpgradeScripts/20250108_Holidays.sql
+++ src/api/core-api/SQLUpgradeScripts/20250108_Holidays.sql
@@ -1,5 +1,6 @@
 CREATE TABLE public."Holidays" (
     Id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     Day DATE NOT NULL,
-    Locale VARCHAR(10) NOT NULL
+    Locale VARCHAR(10) NOT NULL,
+    CheckedDate DATE NOT NULL,
 );
