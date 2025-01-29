7e256bd7a5a3b404aaa917f922d6894bea2e78c5
Kgaogelo Thorn <kgaogelo@swipeix.com>
2025-01-29 14:41:07 +0200

    fix(ec-2897): fixed activity report modal

--- src/lxp/src/pages/classroom/programme-planning/programme-dashboard/programme-dashboard.tsx
+++ src/lxp/src/pages/classroom/programme-planning/programme-dashboard/programme-dashboard.tsx
@@ -43,6 +43,7 @@ import { useIsTrialPeriod } from '@/hooks/useIsTrialPeriod';
 import { useUserPermissions } from '@/hooks/useUserPermissions';
 import { practitionerSelectors } from '@/store/practitioner';
 import { replaceSkillText } from '@/utils/child/child-progress-report.utils';
+import { useProgressForChildren } from '@/hooks/useProgressForChildren';
 
 const { usePDF } = require('react-to-pdf');
 
@@ -52,6 +53,8 @@ export interface iSkills {
 }
 
 export const ProgrammeDashboard: React.FC = () => {
+  const { percentageReportsCompleted, percentageObservationsCompleted } =
+    useProgressForChildren();
   const [showInitialWalkthrough, setShowInitialWalkthrough] = useState(false);
 
   const { isLoading: isLoadingActivities } = useThunkFetchCall(
@@ -530,10 +533,18 @@ export const ProgrammeDashboard: React.FC = () => {
   ]);
 
   useEffect(() => {
-    if (lastProgressReportPeriodHasPassed && baseReports?.length > 0) {
+    if (
+      (percentageObservationsCompleted === 100 ||
+        percentageReportsCompleted === 100) &&
+      lastProgressReportPeriodHasPassed
+    ) {
       showProgressReportEndedDialog();
     }
-  }, [lastProgressReportPeriodHasPassed, baseReports?.length]);
+  }, [
+    percentageReportsCompleted,
+    percentageObservationsCompleted,
+    lastProgressReportPeriodHasPassed,
+  ]);
 
   return (
     <BannerWrapper
