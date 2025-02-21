6df7a82767c3c29812c70950768948abf18af383
Liana Wiehahn <liana@ecdconnect.co.za>
2025-02-21 13:41:24 +0200

    fix(ec-3799): populate holidays on cron job

--- src/api/Libraries/Modules/ECDLink.Development/Holidays/HolidayServiceOverride.cs
+++ src/api/Libraries/Modules/ECDLink.Development/Holidays/HolidayServiceOverride.cs
@@ -1,13 +1,10 @@
 using ECDLink.Abstractrions.Services;
 using ECDLink.Core.Models;
-using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
 using System.Threading.Tasks;
 using ECDLink.DataAccessLayer.Context;
-using ECDLink.DataAccessLayer.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECDLink.Development.Holidays
@@ -20,7 +17,6 @@ namespace ECDLink.Development.Holidays
     public class HolidayServiceOverride : IHolidayService<Holiday>
     {
         private readonly AuthenticationDbContext _context;
-
         private readonly Dictionary<(DateTime startMonth, DateTime endMonth, string locale), IEnumerable<Holiday>> _holidayMonthCache = new Dictionary<(DateTime, DateTime, string), IEnumerable<Holiday>>();
         private readonly Dictionary<(int year, string locale), IEnumerable<Holiday>> _holidayYearCache = new Dictionary<(int, string), IEnumerable<Holiday>>();
 
@@ -57,53 +53,11 @@ namespace ECDLink.Development.Holidays
 
         public async Task<IEnumerable<Holiday>> GetHolidaysAsync(int year, string locale = "ZA")
         {
-            var currentMonth = DateTime.Now.Month;
-            var holidaysToUpdate = await _context.Holidays
-                .Where(x => x.CheckedDate.Month != currentMonth)
-                .ToListAsync();
-
-            if (!await _context.Holidays.AnyAsync() || holidaysToUpdate.Any())
-            {
-                await FetchAndUpdateHolidaysAsync();
-            }
-
             return await _context.Holidays
                 .Where(x => x.Day.Year == year && x.Locale == locale)
+                .Distinct()
                 .ToListAsync();
         }
 
-        public async Task FetchAndUpdateHolidaysAsync()
-        {
-            var currentYear = DateTime.UtcNow.Year;
-
-            // Remove holidays from previous years
-            await _context.Holidays
-                .Where(h => h.Day.Year < currentYear)
-                .ExecuteDeleteAsync();
-
-            var endpoint = $"https://date.nager.at/api/v3/PublicHolidays/{currentYear}/ZA";
-
-            var client = new RestClient(endpoint);
-            var request = new RestRequest();
-            request.AddHeader("Content-Type", "application/json");
-            request.AddHeader("Accept", "application/json");
-            var response = await client.ExecuteAsync(request);
-
-            if (response.ResponseStatus == ResponseStatus.Completed)
-            {
-                var holidays = JsonSerializer.Deserialize<IEnumerable<DaysOff>>(response.Content);
-
-                var newHolidays = holidays
-                    .Select(h => new Holiday
-                    {
-                        Day = DateTime.Parse(h.date),
-                        CheckedDate = DateTime.Now,
-                        Locale = "ZA"
-                    });
-
-                await _context.Holidays.AddRangeAsync(newHolidays);
-                await _context.SaveChangesAsync();
-            }
-        }
     }
 }
