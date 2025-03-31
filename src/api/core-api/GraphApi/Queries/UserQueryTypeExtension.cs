e35b17c84bc76049916fc41f361c8bf20cc69a9d
Liana Wiehahn <liana@ecdconnect.co.za>
2025-03-31 13:30:12 +0300

    fix(ec-3857): exclude external users from query

--- src/api/core-api/GraphApi/Queries/UserQueryTypeExtension.cs
+++ src/api/core-api/GraphApi/Queries/UserQueryTypeExtension.cs
@@ -18,6 +18,7 @@ using HotChocolate.Data;
 using HotChocolate.Types;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Math.EC.Rfc7748;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -52,7 +53,7 @@ namespace EcdLink.Api.CoreApi.GraphApi.Queries
             var userIsAdmin = await userManager.IsInRoleAsync(currentUser, Roles.ADMINISTRATOR) || await userManager.IsInRoleAsync(currentUser, Roles.SUPER_ADMINISTRATOR);
 
             var usersQuery = userManager.Users
-                .Where(u => u.TenantId == tenantId)
+                .Where(u => u.TenantId == tenantId && !u.UserName.StartsWith("External_Edit"))
                 .AsNoTracking();
 
 
