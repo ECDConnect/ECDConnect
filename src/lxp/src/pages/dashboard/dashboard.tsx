c5066effd0e9fc1ec618904bddb8ce86e886b48e
Liana Wiehahn <liana@ecdconnect.co.za>
2025-06-02 08:34:36 +0300

    fix(ec-3328): revert fetch of classes

--- src/lxp/src/pages/dashboard/dashboard.tsx
+++ src/lxp/src/pages/dashboard/dashboard.tsx
@@ -145,9 +145,6 @@ export const Dashboard: React.FC = () => {
     notificationsSelectors.getDashboardNotification
   );
 
-  const [fetchClassroomGroups, setFetchClassroomGroups] = useState(
-    !!classroomGroups.length
-  );
   const pointsSummaryData = useSelector(pointsSelectors.getPointsSummary);
   const [pointsScoreProps, setPointsScoreProps] = useState<ScoreCardProps>();
   const pointsToDo = useSelector(pointsSelectors.getPointsToDo);
@@ -177,15 +174,6 @@ export const Dashboard: React.FC = () => {
   useEffect(() => {
     if (isOnline && practitioner) {
       getPointsToDoItems();
-    }
-  }, []);
-
-  useEffect(() => {
-    if (isOnline && !fetchClassroomGroups) {
-      appDispatch(
-        classroomsThunkActions.getClassroomGroups({ overrideCache: true })
-      );
-      setFetchClassroomGroups(true);
     }
   }, []);
 
