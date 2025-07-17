51b96b0cd4269b7426c15507e00bbaae9a1cc341
Amanda van Heerden <amanda@meraki-it.co.za>
2025-07-17 14:31:09 +0200

    fix(ec-3362): fix coach notification

--- src/lxp/src/pages/dashboard/components/dashboard-items/dashboard-items.tsx
+++ src/lxp/src/pages/dashboard/components/dashboard-items/dashboard-items.tsx
@@ -85,6 +85,9 @@ export const DashboardItems: React.FC<DashboardItemsProps> = ({
       ) ||
       notification.message?.cta?.includes(
         notificationTagConfig?.SeeClasses?.cta ?? ''
+      ) ||
+      notification.message?.cta?.includes(
+        notificationTagConfig?.SeePractitioners?.cta ?? ''
       )
     ) {
       appDispatch(notificationActions.removeNotification(notification!));
