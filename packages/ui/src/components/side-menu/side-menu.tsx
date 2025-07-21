dfe7a2bc6a10dfac7987a2b85594b77dfbd145b6
Liana Wiehahn <liana@ecdconnect.co.za>
2025-07-21 09:27:12 +0200

    fix(ec-3991): add responsive props for logo image

--- packages/ui/src/components/side-menu/side-menu.tsx
+++ packages/ui/src/components/side-menu/side-menu.tsx
@@ -71,11 +71,18 @@ export const SideMenu: React.FC<SideMenuProps> = ({
                 </div>
               </Transition.Child>
 
-              <div className="px-18 aspect-image w-3/4">
-                <img src={logoUrl} className="h-full w-full object-cover" />
+              <div className="flex justify-center px-4 py-0 md:justify-start">
+                <div className="aspect-[2/1] relative w-28 flex-shrink-0 md:w-40 lg:w-48">
+                  <img
+                    src={logoUrl}
+                    alt="Company Logo"
+                    className="h-full w-full object-contain"
+                    loading="lazy"
+                  />
+                </div>
               </div>
 
-              <div className="mt-5 flex h-0 flex-1 flex-col justify-between overflow-y-auto">
+              <div className="mt-2 flex h-0 flex-1 flex-col justify-between overflow-y-auto">
                 <nav className="px-18 space-y-1.5">
                   {navigation.map((item) =>
                     item.nestedChildren ? (
