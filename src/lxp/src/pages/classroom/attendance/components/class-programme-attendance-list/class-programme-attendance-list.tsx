5c6167a51c926ac7fbf6f32b20277ff04edd010a
Amanda van Heerden <amanda@meraki-it.co.za>
2025-07-31 15:29:15 +0200

    fix(ec-3999): fix new child in class

--- src/lxp/src/pages/classroom/attendance/components/class-programme-attendance-list/class-programme-attendance-list.tsx
+++ src/lxp/src/pages/classroom/attendance/components/class-programme-attendance-list/class-programme-attendance-list.tsx
@@ -38,8 +38,6 @@ export const ClassProgrammeAttendanceList: React.FC<
     const classroomGroupLearners = classroomGroup.learners.filter((x) => {
       const startedAttendance = new Date(x.startedAttendance);
       startedAttendance.setHours(0, 0, 0, 0);
-      const child = children?.find((c) => c.userId === x.childUserId);
-
       return (
         (!x.stoppedAttendance ||
           new Date(x.stoppedAttendance).getTime() > attendanceDate.getTime()) &&
@@ -69,7 +67,7 @@ export const ClassProgrammeAttendanceList: React.FC<
         !!child.user &&
         !!child.user.firstName &&
         !!child.user.surname &&
-        !!child.caregiverId
+        !!child.caregiver
       ) {
         filteredLearners.push(learner);
       }
