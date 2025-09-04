bc48c0d533db524343e64d8142bb9c8dc0437b63
Amanda van Heerden <amanda@meraki-it.co.za>
2025-09-04 08:44:50 +0200

    feat(4009): add other languages

--- src/api/core-api/GraphApi/Models/Users/AddChildTokenModel.cs
+++ src/api/core-api/GraphApi/Models/Users/AddChildTokenModel.cs
@@ -39,6 +39,7 @@ namespace EcdLink.Api.CoreApi.GraphApi.Models.Users
         public Guid? WorkflowStatusId { get; set; }
 
         public string InsertedBy { get; set; }
+        public string OtherLanguages { get; set; }
     }
 
     public class AddChildSiteAddressTokenModel
