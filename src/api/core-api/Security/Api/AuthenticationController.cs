e25b932e50960802fb7617c5b2dfb5c0bffbaf82
Derren <derren@meraki-it.co.za>
2025-11-11 09:35:18 +0200

    feat(ec-4063): use enum not string

--- src/api/core-api/Security/Api/AuthenticationController.cs
+++ src/api/core-api/Security/Api/AuthenticationController.cs
@@ -699,7 +699,7 @@ namespace ECDLink.Security.Api
                 });
             }
 
-            bool isSSOAccount = user.RegisterType != "username";
+            bool isSSOAccount = user.RegisterType != RegisterTypeConstants.USERNAME;
             if (!isSSOAccount)
             {
                 if (string.IsNullOrEmpty(input.Password))
