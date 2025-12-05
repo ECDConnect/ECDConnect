a29289a3b4d96c61c8436d2e98ec249f1eac33c5
Amanda van Heerden <amanda@meraki-it.co.za>
2025-12-05 14:16:00 +0200

    fix(ec-3484): set number of users + fix validation

--- src/admin-portal/src/app/pages/users/components/user-panel-create/user-panel-create.tsx
+++ src/admin-portal/src/app/pages/users/components/user-panel-create/user-panel-create.tsx
@@ -65,7 +65,7 @@ export default function UserPanelCreate(props: UserPanelCreateProps) {
 
   // FORMS
   // USER FORM DETAILS
-  const { register, formState, handleSubmit, watch } = useForm({
+  const { register, formState, handleSubmit, watch, setValue } = useForm({
     resolver: yupResolver(userSchema),
     defaultValues: initialUserDetailsValues,
     mode: 'onChange',
@@ -175,100 +175,102 @@ export default function UserPanelCreate(props: UserPanelCreateProps) {
 
   const getComponent = () => {
     return (
-      <>
-        <div className="">
-          {isDirty && (
-            <div className="focus:outline-none focus:ring-primary absolute right-5 -top-20 z-10 mt-6 flex h-7 items-center rounded-md bg-white text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-offset-2">
-              <button
-                className="focus:outline-none focus:ring-primary rounded-md bg-white text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-offset-2"
-                onClick={() => setDisplayFormIsDirty(true)}
-              >
-                <span className="sr-only">Close panel</span>
-                <XIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-            </div>
-          )}
-          <div className="border-b border-dashed border-gray-500 px-2">
-            <h1 className="py-4 text-xl font-medium leading-6 text-black">
-              Administrator Details
-            </h1>
-            <p className="text-md pb-2 text-gray-500">Step 1 of 1</p>
+      <div className="">
+        {isDirty && (
+          <div className="focus:outline-none focus:ring-primary absolute right-5 -top-20 z-10 mt-6 flex h-7 items-center rounded-md bg-white text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-offset-2">
+            <button
+              className="focus:outline-none focus:ring-primary rounded-md bg-white text-gray-400 hover:text-gray-500 focus:ring-2 focus:ring-offset-2"
+              onClick={() => setDisplayFormIsDirty(true)}
+            >
+              <span className="sr-only">Close panel</span>
+              <XIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
           </div>
-          <form className="space-y-8 divide-y divide-gray-200">
-            <div className="space-y-0">
-              <div className="grid grid-cols-1 ">
-                <div className="my-4 sm:col-span-3">
-                  <FormField
-                    label={'First name *'}
-                    nameProp={'firstName'}
-                    register={register}
-                    error={errors.firstName?.message}
-                    placeholder="First name"
-                  />
-                </div>
-                <div className="my-4 sm:col-span-3">
-                  <FormField
-                    label={'Surname *'}
-                    nameProp={'surname'}
-                    register={register}
-                    error={errors.surname?.message}
-                    placeholder="Surname/family name"
-                  />
-                </div>
-                <div className="my-4 sm:col-span-3">
-                  <FormField
-                    label={'Work email address *'}
-                    nameProp={'email'}
-                    register={register}
-                    error={
-                      isEmailInUse
-                        ? 'A user already exists with this email.'
-                        : errors.email?.message
-                    }
-                    placeholder="e.g name@email.com"
-                  />
-                </div>
+        )}
+        <div className="border-b border-dashed border-gray-500 px-2">
+          <h1 className="py-4 text-xl font-medium leading-6 text-black">
+            Administrator Details
+          </h1>
+          <p className="text-md pb-2 text-gray-500">Step 1 of 1</p>
+        </div>
+        <form className="space-y-8 divide-y divide-gray-200">
+          <div className="space-y-0">
+            <div className="grid grid-cols-1 ">
+              <div className="my-4 sm:col-span-3">
+                <FormField
+                  label={'First name *'}
+                  nameProp={'firstName'}
+                  register={register}
+                  error={errors.firstName?.message}
+                  placeholder="First name"
+                />
+              </div>
+              <div className="my-4 sm:col-span-3">
+                <FormField
+                  label={'Surname *'}
+                  nameProp={'surname'}
+                  register={register}
+                  error={errors.surname?.message}
+                  placeholder="Surname/family name"
+                />
+              </div>
+              <div className="my-4 sm:col-span-3">
+                <FormField
+                  label={'Work email address *'}
+                  nameProp={'email'}
+                  register={register}
+                  error={
+                    isEmailInUse
+                      ? 'A user already exists with this email.'
+                      : errors.email?.message
+                  }
+                  onChange={async (event) => {
+                    const newEmail = event.target.value;
+                    setValue('email', newEmail, {
+                      shouldValidate: true,
+                    });
+                  }}
+                  placeholder="e.g name@email.com"
+                />
               </div>
             </div>
-          </form>
-          <Dialog
-            className={'mb-16 px-4'}
-            stretch
-            visible={displayFormIsDirty}
-            position={DialogPosition.Middle}
-          >
-            <ActionModal
-              icon={'InformationCircleIcon'}
-              iconColor="alertMain"
-              iconBorderColor="alertBg"
-              importantText={`Discard unsaved changes?`}
-              detailText={
-                'If you leave now, you will lose all of your changes.'
-              }
-              actionButtons={[
-                {
-                  text: 'Keep editing',
-                  textColour: 'secondary',
-                  colour: 'secondary',
-                  type: 'outlined',
-                  onClick: () => setDisplayFormIsDirty(false),
-                  leadingIcon: 'PencilIcon',
+          </div>
+        </form>
+        <Dialog
+          className={'mb-16 px-4'}
+          stretch
+          visible={displayFormIsDirty}
+          position={DialogPosition.Middle}
+        >
+          <ActionModal
+            icon={'InformationCircleIcon'}
+            iconColor="alertMain"
+            iconBorderColor="alertBg"
+            importantText={`Discard unsaved changes?`}
+            detailText={'If you leave now, you will lose all of your changes.'}
+            actionButtons={[
+              {
+                text: 'Keep editing',
+                textColour: 'secondary',
+                colour: 'secondary',
+                type: 'outlined',
+                onClick: () => setDisplayFormIsDirty(false),
+                leadingIcon: 'PencilIcon',
+              },
+              {
+                text: 'Discard changes',
+                textColour: 'white',
+                colour: 'secondary',
+                type: 'filled',
+                onClick: () => {
+                  emitCloseDialog(false);
                 },
-                {
-                  text: 'Discard changes',
-                  textColour: 'white',
-                  colour: 'secondary',
-                  type: 'filled',
-                  onClick: () => {
-                    emitCloseDialog(false);
-                  },
-                  leadingIcon: 'TrashIcon',
-                },
-              ]}
-            />
-          </Dialog>
-        </div>
-      </>
+                leadingIcon: 'TrashIcon',
+              },
+            ]}
+          />
+        </Dialog>
+      </div>
     );
   };
 
