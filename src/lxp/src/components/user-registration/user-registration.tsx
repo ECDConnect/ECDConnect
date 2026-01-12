32fa54d92414aa9734741cc0e21a334adabe5a85
Derren <derren@meraki-it.co.za>
2026-01-12 22:54:52 +0200

    feat(ec-4063): new pop-ups for sso

--- src/lxp/src/components/user-registration/user-registration.tsx
+++ src/lxp/src/components/user-registration/user-registration.tsx
@@ -17,10 +17,14 @@ import jwtDecode from 'jwt-decode';
 import { useTenant } from '@/hooks/useTenant';
 import {
   createUser,
+  CreateUserInfo,
   CreateUserResult,
 } from '@/utils/user/user-registration.utils';
 import { NOTIFICATION, useNotifications } from '@ecdlink/core';
 import ROUTES from '@/routes/routes';
+import { Login as FacebookLogin } from 'react-facebook';
+import { LoginResponse } from '@/types/facebook';
+
 interface UserRegistrationProps {}
 export interface UserRegistrationRouteState {
   userId?: string;
@@ -46,37 +50,27 @@ export const UserRegistration: React.FC<UserRegistrationProps> = ({}) => {
     undefined
   );
 
-  const onGoogleLoginSuccess = async (response: CredentialResponse) => {
-    const credential = response.credential || '';
-    const decoded = jwtDecode<any>(credential);
-    const email = decoded.email;
-    setGoogleEmail(email);
-    setGoogleCredential(credential);
+  const isSslEnabled = window.location.protocol === 'https:';
 
-    const createUserResult = await createUser({
-      userId: userId as any as string,
-      username: email,
-      password: '',
-      phoneNumber: phoneNumber ?? '',
-      registerType: 'google',
-      shareInfoPartners,
-      token,
-      googleToken: credential,
-      tenant,
-    });
+  const onLoginSuccess = async (createUserInfo: CreateUserInfo) => {
+    const createUserResult = await createUser(createUserInfo);
     switch (createUserResult) {
+      case CreateUserResult.UsernameExistsLogin:
       case CreateUserResult.SuccessLogin:
         history.push(ROUTES.LOGIN, {
-          username: email,
+          username: createUserInfo.username,
           password: '',
-          loginType: 'google',
-          googleToken: credential,
+          loginType: createUserInfo.registerType,
+          googleToken: createUserInfo.googleToken,
+          facebookToken: createUserInfo.facebookToken,
         });
         setMessageError('');
-        setNotification({
-          title: ` Successfully registered!`,
-          variant: NOTIFICATION.SUCCESS,
-        });
+        if (createUserResult === CreateUserResult.SuccessLogin) {
+          setNotification({
+            title: ` Successfully registered!`,
+            variant: NOTIFICATION.SUCCESS,
+          });
+        }
         break;
       case CreateUserResult.UsernameExists:
         setMessageError(
@@ -98,8 +92,50 @@ export const UserRegistration: React.FC<UserRegistrationProps> = ({}) => {
     }
   };
 
+  const onGoogleLoginSuccess = async (response: CredentialResponse) => {
+    const credential = response.credential || '';
+    const decoded = jwtDecode<any>(credential);
+    const email = decoded.email;
+    setGoogleEmail(email);
+    setGoogleCredential(credential);
+
+    await onLoginSuccess({
+      userId: userId as any as string,
+      username: email,
+      password: '',
+      phoneNumber: phoneNumber ?? '',
+      registerType: 'google',
+      shareInfoPartners,
+      token,
+      googleToken: credential,
+      facebookToken: undefined,
+      tenant,
+    });
+  };
+
   const onGoogleLoginError = () => {
     console.log('Google Login failed.');
+  };
+
+  const onFacebookLoginSuccess = async (response: LoginResponse) => {
+    const { userID, accessToken } = response.authResponse;
+
+    await onLoginSuccess({
+      userId: userId as any as string,
+      username: userID,
+      password: '',
+      phoneNumber: phoneNumber ?? '',
+      registerType: 'facebook',
+      shareInfoPartners,
+      token,
+      googleToken: undefined,
+      facebookToken: accessToken,
+      tenant,
+    });
+  };
+
+  const onFacebookLoginError = (error: Error) => {
+    console.log('Facebook login failed', error);
   };
 
   return (
@@ -118,32 +154,59 @@ export const UserRegistration: React.FC<UserRegistrationProps> = ({}) => {
           className={'text-sm font-normal'}
           color={'textDark'}
         />
-        {isOnline && !!process.env.REACT_APP_GOOGLE_CLIENT_ID && (
-          <div className="mb-6 mt-6">
-            <div className="mb-6 flex justify-center">
-              <GoogleLogin
-                onSuccess={(credentialResponse) =>
-                  onGoogleLoginSuccess(credentialResponse)
-                }
-                onError={() => onGoogleLoginError()}
-                type="standard"
-                text="signup_with"
-                logo_alignment="center"
-                size="medium"
-                width={300}
-              />
+        {isOnline &&
+          (!!process.env.REACT_APP_GOOGLE_CLIENT_ID ||
+            (!!process.env.REACT_APP_FACEBOOK_APP_ID && isSslEnabled)) && (
+            <div className="mb-6 mt-6">
+              {!!process.env.REACT_APP_GOOGLE_CLIENT_ID && (
+                <div className="mb-6 flex justify-center">
+                  <GoogleLogin
+                    onSuccess={(credentialResponse) =>
+                      onGoogleLoginSuccess(credentialResponse)
+                    }
+                    onError={() => onGoogleLoginError()}
+                    type="standard"
+                    text="signup_with"
+                    logo_alignment="center"
+                    size="large"
+                    width={250}
+                  />
+                </div>
+              )}
+              {!!process.env.REACT_APP_FACEBOOK_APP_ID && isSslEnabled && (
+                <div className="mb-6 flex justify-center">
+                  <FacebookLogin
+                    onSuccess={(response: any) =>
+                      onFacebookLoginSuccess(response)
+                    }
+                    onError={(error: Error) => onFacebookLoginError(error)}
+                    fields={['id', 'name', 'picture']}
+                    scope={['public_profile']}
+                    //className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700"
+                    className="flex h-10 items-center space-x-2 rounded-lg bg-blue-600 px-6 py-3 text-white transition-colors hover:bg-blue-700"
+                  >
+                    <svg
+                      className="h-5 w-5"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"></path>
+                    </svg>
+                    <span>Sign up with Facebook</span>
+                  </FacebookLogin>
+                </div>
+              )}
+              {messageError && (
+                <Typography
+                  type={'help'}
+                  text={messageError}
+                  className={'mt-1 mb-6 text-sm font-normal'}
+                  color={'errorMain'}
+                />
+              )}
+              <Divider title="OR" />
             </div>
-            {messageError && (
-              <Typography
-                type={'help'}
-                text={messageError}
-                className={'mt-1 mb-6 text-sm font-normal'}
-                color={'errorMain'}
-              />
-            )}
-            <Divider title="OR" />
-          </div>
-        )}
+          )}
         <Button
           className={'mt-2 w-full rounded-xl'}
           type="filled"
