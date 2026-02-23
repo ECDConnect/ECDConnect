7ed1f5ac8612fab00515e809c4be3a86583fdab7
Liana Wiehahn <liana@ecdconnect.co.za>
2026-02-23 12:22:26 +0200

    fix(ec-4311): decrease banner images

--- src/lxp/tailwind.config.js
+++ src/lxp/tailwind.config.js
@@ -1,9 +1,10 @@
 const plugin = require('tailwindcss/plugin');
 
 module.exports = {
-  purge: {
-    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  },
+  // purge: {
+  //   enabled: true, // force it even if you want to test
+  //   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  // },
   important: true,
   mode: 'aot',
   darkMode: false, // or 'media' or 'class'
