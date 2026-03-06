58dc3e1b7f42774c488dc7bb1cb785ad26cab764
Amanda van Heerden <amanda@meraki-it.co.za>
2026-03-06 15:56:01 +0200

    fix(ec-4335): fix leave edit / delete + remove BE calls + invites in redux

---
+++ src/lxp/src/store/invites/invites.actions.ts
@@ -0,0 +1,42 @@
+import { InviteDto } from '@ecdlink/core';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { InviteService } from '@/services/InviteService';
+import { RootState, ThunkApiType } from '../types';
+
+export const getInvitesByPrincipalId = createAsyncThunk<
+  InviteDto[],
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  {},
+  ThunkApiType<RootState>
+>('getInvitesByPrincipalId', async (_, { getState, rejectWithValue }) => {
+  const {
+    auth: { userAuth },
+    invitesData: { invites: invitesCache },
+  } = getState();
+
+  if (!invitesCache) {
+    try {
+      let invites: InviteDto[] | undefined;
+
+      if (userAuth?.auth_token) {
+        const response = await new InviteService(
+          userAuth?.auth_token
+        ).getInvitesByPrincipalId(userAuth.id);
+
+        invites = response.filter((invite) => invite.isAccepted !== true);
+      } else {
+        return rejectWithValue('no access token, profile check required');
+      }
+
+      if (!invites) {
+        return rejectWithValue('Error getting invites');
+      }
+
+      return invites;
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  } else {
+    return invitesCache;
+  }
+});
