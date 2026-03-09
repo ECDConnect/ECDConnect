b3ec374f51789d4b232f3e4e7925f58623e2d98c
Amanda van Heerden <amanda@meraki-it.co.za>
2026-03-09 12:40:26 +0200

    fix(ec-4337): cater for upgraded packages

--- src/admin-portal/craco.config.js
+++ src/admin-portal/craco.config.js
@@ -7,4 +7,20 @@ module.exports = {
         ],
       },
     },
+ webpack: {
+    configure: (webpackConfig) => {
+      const babelLoaderRule = webpackConfig.module.rules
+        .find(rule => Array.isArray(rule.oneOf))
+        ?.oneOf?.find(r => r.loader?.includes('babel-loader'));
+
+      if (babelLoaderRule) {
+        babelLoaderRule.include = [
+          babelLoaderRule.include,
+          /node_modules[\\\/]graphql/
+        ];
+      }
+
+      return webpackConfig;
+    }
+  },
   }
