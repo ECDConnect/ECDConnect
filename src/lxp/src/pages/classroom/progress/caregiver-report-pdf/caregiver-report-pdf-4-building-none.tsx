684c476d301533d5da59cf0eab99fd1b96ac98f2
Amanda van Heerden <amanda@meraki-it.co.za>
2026-03-10 16:25:24 +0200

    fix(ec-3431): fix image on progress report

--- src/lxp/src/pages/classroom/progress/caregiver-report-pdf/caregiver-report-pdf-4-building-none.tsx
+++ src/lxp/src/pages/classroom/progress/caregiver-report-pdf/caregiver-report-pdf-4-building-none.tsx
@@ -73,7 +73,13 @@ export const ProgressCaregiverReportBuildingNonePage: React.FC<
       <img
         src={backgroundImg}
         className=""
-        style={{ position: 'absolute', bottom: 0, left: 0 }}
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          height: '200px',
+          width: '100%',
+        }}
       />
     </div>
   );
