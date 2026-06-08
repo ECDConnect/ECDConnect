cd5213d1e03b444ca628d5cb5a1c216a1a1ae907
Amanda van Heerden <amanda@meraki-it.co.za>
2026-06-08 10:37:48 +0200

    fix(ec-4534): fix redirect

--- src/lxp/src/pages/practitioner/practitioner-profile/practitioner-journey/published-forms/index.tsx
+++ src/lxp/src/pages/practitioner/practitioner-profile/practitioner-journey/published-forms/index.tsx
@@ -95,8 +95,8 @@ export const PublishedFormsList = ({ onBack }: FormProps) => {
             : 'Health and safety check'
         } completed!`,
       });
-      onBack();
     }
+    onBack();
   };
 
   const [completedVisitId, setCompletedVisitId] = useState('');
