2f4de98cbedeaebbfc9138dc2e3723d732df80a0
Liana Wiehahn <liana@ecdconnect.co.za>
2026-06-26 08:28:29 +0200

    fix(ec-4552): add new data to logging errors

--- src/lxp/src/store/queryErrors/queryErrors.types.ts
+++ src/lxp/src/store/queryErrors/queryErrors.types.ts
@@ -1,11 +1,13 @@
 export interface AppError {
-  id: string;
+  id?: string;
   type: 'network' | 'graphql' | 'api';
   message: string;
-  stack?: string;
-  timestamp: number;
   payload?: any;
   eventDate: number;
+  clientUrl?: string; // the url of the client when the error occurred
+  isOnline?: boolean; // was the user online or not
+  requestPayload?: any; // this includes the graph query Id and arguments
+  userAgent?: string; // browser and OS info
 }
 
 export interface ErrorState {
