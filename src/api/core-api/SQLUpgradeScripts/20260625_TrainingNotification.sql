e0c219075db933ed82250391130e56c8db1a1e68
Derren Page <derren@meraki-it.co.za>
2026-08-04 21:28:59 +0200

    Merge/ecdconnect database 20260803 (#6)

--- src/api/core-api/SQLUpgradeScripts/20260625_TrainingNotification.sql
+++
@@ -1,6 +0,0 @@
--- notification
-INSERT INTO "MessageTemplate" ("Id","IsActive","InsertedDate","UpdatedDate","UpdatedBy","Protocol","TemplateType","Message","TenantId","Subject","CTA","CTAText","TypeCode","NotificationColor","Ordering","Action") VALUES
-	 (uuid_generate_v4(),true,current_date,current_date,NULL,'hub','course-completed','Well done on completing a course! You can download your certificate.',null,'Download your certificate','[[CertificateDownload]]','Get certificate',NULL,'green',26,'{"url":"/practitioner/profile","state":{"activeTabIndex":"1"}}');
-
-INSERT INTO "MessageTemplate" ("Id","IsActive","InsertedDate","UpdatedDate","UpdatedBy","Protocol","TemplateType","Message","TenantId","Subject","CTA","CTAText","TypeCode","NotificationColor","Ordering","Action") VALUES
-	 (uuid_generate_v4(),true,current_date,current_date,NULL,'push','course-completed','Well done on completing a course! You can download your certificate.',null,'Download your certificate','[[CertificateDownload]]','Get certificate',NULL,'green',26,'{"url":"/practitioner/profile","state":{"activeTabIndex":"1"}}');
